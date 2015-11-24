0d9a04ce9d24d026bded077abc084b25b6e60d73
Sylvester Keil <sylvester@keil.or.at>
2015-11-24 23:32:36 +0100

    Add Electron Connect client to main window

--- src/browser/main.js
+++ src/browser/main.js
@@ -2,6 +2,7 @@
 
 const { app, BrowserWindow } = require('electron');
 const { join } = require('path');
+const { client } = require('electron-connect');
 
 const ROOT = join(__dirname, '../..');
 
@@ -16,6 +17,9 @@ app
     win = new BrowserWindow({});
 
     win.loadURL(`file://${ROOT}/static/index.html`);
+
+    // Enable live-reloading in dev environment
+    client.create(win);
 
     win.once('closed', () => { win = undefined; });
   });
