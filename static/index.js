fda3ab8dc13cc14ba6e4e2787567ad432c380967
Sylvester Keil <sylvester@keil.or.at>
2015-11-25 19:01:45 +0100

    Print something useful to the screen for testing

--- static/index.js
+++ static/index.js
@@ -1,6 +1,10 @@
 'use strict';
 
 {
+  const pkg = require('../package');
+
   window.onload = () => {
+    document.documentElement.innerHTML =
+      `${pkg.name} ${pkg.version} ${process.env.NODE_ENV}`;
   };
 }
