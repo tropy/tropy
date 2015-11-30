18d82537688f50d0b3c21d039bab234093034760
Sylvester Keil <sylvester@keil.or.at>
2015-11-30 19:33:25 +0100

    Add CLI mode switch

--- src/browser/args.js
+++ src/browser/args.js
@@ -1,10 +1,34 @@
 'use strict';
 
+const { basename } = require('path');
 const pkg = require('../../package');
 
+const exe = basename(process.argv[0]);
+const NAME = pkg.name.toUpperCase();
+
+
 module.exports = require('yargs')
-  .usage('Usage: $0')
+  .usage(`Usage: ${exe} [options]`)
+  .strict()
   .wrap(78)
 
+  .option('mode', {
+    alias: 'm',
+    type: 'string',
+    describe: 'Set mode',
+    choices: ['debug', 'dev', 'test', 'normal']
+  })
+  .default('mode', defaultmode, '"normal"')
+
   .help('help')
-  .version(pkg.version);
+  .version(pkg.version)
+
+  .epilogue([
+    'Environment Variables:',
+    `  ${NAME}_MODE  Set default mode`
+  ].join('\n'));
+
+
+function defaultmode() {
+  return process.env[`${NAME}_MODE`] || 'normal';
+}
