dde5f19614e8f82a93bd692b31350db3266077c6
Sylvester Keil <sylvester@keil.or.at>
2015-12-14 14:42:40 +0100

    Include previous coverage results in report

--- test/support/coverage.js
+++ test/support/coverage.js
@@ -1,12 +1,25 @@
 'use strict';
 
+const path = require('path');
+const exists = require('fs').existsSync;
+const read = require('fs').readFileSync;
+
 const istanbul = require('istanbul');
+
+const dir = path.resolve(__dirname, '..', '..', 'coverage');
+let prev;
+
+// Add results from previous browser/renderer tests
+if (exists(path.join(dir, 'coverage-final.json'))) {
+  prev = JSON.parse(read('coverage-final.json', 'utf8'));
+}
 
 module.exports = function (runner) {
   runner.on('end', function () {
-    let reporter = new istanbul.Reporter();
+    let reporter = new istanbul.Reporter(null, dir);
     let collector = new istanbul.Collector();
 
+    if (prev) collector.add(prev);
     collector.add(__coverage__);
 
     reporter.add('text-summary');
