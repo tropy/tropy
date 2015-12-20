5528f662edd53f63331a04d6c67072c27d0505d5
Sylvester Keil <sylvester@keil.or.at>
2015-12-20 21:45:37 +0100

    Remove semicolons

--- static/index.js
+++ static/index.js
@@ -1,15 +1,15 @@
-'use strict';
+'use strict'
 
 {
-  const pkg = require('../package');
-  const dom = require('../lib/dom');
+  const pkg = require('../package')
+  const dom = require('../lib/dom')
 
   window.onload = () => {
     document.body.innerHTML =
-      `${pkg.name} ${pkg.version} ${process.env.NODE_ENV}`;
-  };
+      `${pkg.name} ${pkg.version} ${process.env.NODE_ENV}`
+  }
 
   dom.append(
     dom.css(`@import '../lib/stylesheets/${process.platform}.css';`),
-    document.head);
+    document.head)
 }
