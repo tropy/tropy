7eccf44aec4055ff1988efae0f390c475c6171cf
Sylvester Keil <sylvester@keil.or.at>
2015-12-21 19:21:10 +0100

    Adjust userData path depending on environment

--- src/browser/main.js
+++ src/browser/main.js
@@ -6,9 +6,17 @@ const opts = args.parse(process.argv.slice(1))
 process.env.NODE_ENV = opts.environment
 
 const { app } = require('electron')
+const { join } = require('path')
+
+if (opts.environment !== 'production') {
+  app.setPath('userData', join(process.cwd(), 'tmp', opts.environment))
+}
+
 const tropy = new (require('./tropy'))(opts)
 
-if (app.makeSingleInstance(() => tropy.open())) app.exit(0)
+if (opts.environment !== 'test') {
+  if (app.makeSingleInstance(() => tropy.open())) app.exit(0)
+}
 
 app
   .on('window-all-closed', () => {
