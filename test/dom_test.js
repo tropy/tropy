7a3c545a95747e4af9a5396789d764cb68d4281a
Sylvester Keil <sylvester@keil.or.at>
2016-01-05 20:37:12 +0100

    Add more dom tests

--- test/dom_test.js
+++ test/dom_test.js
@@ -8,4 +8,27 @@ describe('dom', () => {
       expect(dom.css()).to.be.instanceof(HTMLStyleElement)
     })
   })
+
+  describe('.stylesheet', () => {
+    it('creates a link node', function () {
+      let link = dom.stylesheet('theme.css')
+
+      expect(link).to.be.instanceof(HTMLLinkElement)
+      expect(link.getAttribute('href')).to.eql('theme.css')
+    })
+  })
+
+  describe('.append', () => {
+    it('adds a node to another', () => {
+      let n1 = document.createElement('div')
+      let n2 = document.createElement('div')
+
+      dom.append(n1, n2)
+
+      expect(n1.children).to.be.empty
+
+      expect(n2.children).to.have.length(1)
+      expect(n2.children.item(0)).to.equal(n1)
+    })
+  })
 })
