6e36d7987ca3250df0721e01237aef6a71c64212
Sylvester Keil <sylvester@keil.or.at>
2016-01-12 15:33:54 +0100

    Test window url generation

--- test/browser/window_test.js
+++ test/browser/window_test.js
@@ -7,4 +7,33 @@ describe('Window', () => {
   it('extends BrowserWindow', () => {
     expect(new Window()).to.be.instanceof(BrowserWindow)
   })
+
+  describe('#open', () => {
+    let win
+
+    beforeEach(() => {
+      win = new Window()
+      win.webContents.removeAllListeners('dom-ready')
+      sinon.spy(win, 'loadURL')
+    })
+
+    afterEach(() => {
+      win.loadURL.restore()
+      win.destroy()
+    })
+
+    it('loads static page', () => {
+      win.open('index.html')
+
+      expect(win.loadURL)
+        .to.have.been.calledWithMatch('static/index.html')
+    })
+
+    it('loads url with encoded data', () => {
+      win.open('index.html', { foo: 'bar' })
+
+      expect(win.loadURL)
+        .to.have.been.calledWithMatch('#%7B%22foo%22%3A%22bar%22%7D')
+    })
+  })
 })
