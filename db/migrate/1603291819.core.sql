bc1459ea79367d24f6180b81f3f291eda0e6c46a
Sylvester Keil <sylvester@keil.or.at>
2016-03-29 22:36:06 +0200

    Use explicit id names
    WIP core schema

--- db/migrate/1603291819.core.sql
+++ db/migrate/1603291819.core.sql
@@ -1,20 +1,20 @@
 
 CREATE TABLE archive (
-  id TEXT PRIMARY KEY,
+  archive_id TEXT PRIMARY KEY,
   name TEXT NOT NULL,
   created_at TIMESTAMP NOT NULL DEFAULT current_timestamp,
   opened_at TIMESTAMP NOT NULL DEFAULT current_timestamp
 );
 
 CREATE TABLE items (
-  id INTEGER PRIMARY KEY,
+  item_id INTEGER PRIMARY KEY,
   created_at TIMESTAMP NOT NULL DEFAULT current_timestamp,
   modified_at TIMESTAMP NOT NULL DEFAULT current_timestamp
 );
 
 CREATE TABLE photos (
-  id INTEGER PRIMARY KEY,
-  item_id INTEGER NOT NULL REFERENCES items(id),
+  photo_id INTEGER PRIMARY KEY,
+  item_id INTEGER NOT NULL REFERENCES items(item_id),
   path TEXT NOT NULL,
   mimetype TEXT,
   checksum TEXT,
@@ -24,8 +24,8 @@ CREATE TABLE photos (
 );
 
 CREATE TABLE selections (
-  id INTEGER PRIMARY KEY,
-  photo_id INTEGER NOT NULL REFERENCES photos(id),
+  selection_id INTEGER PRIMARY KEY,
+  photo_id INTEGER NOT NULL REFERENCES photos(photo_id),
   position INTEGER NOT NULL DEFAULT 0,
   top INTEGER NOT NULL DEFAULT 0,
   left INTEGER NOT NULL DEFAULT 0,
@@ -38,22 +38,22 @@ CREATE TABLE selections (
 );
 
 CREATE TABLE notes (
-  id INTEGER PRIMARY KEY,
+  note_id INTEGER PRIMARY KEY,
   created_at TIMESTAMP NOT NULL DEFAULT current_timestamp,
   modified_at TIMESTAMP NOT NULL DEFAULT current_timestamp
 );
 
 CREATE TABLE lists (
-  id INTEGER PRIMARY KEY,
+  list_id INTEGER PRIMARY KEY,
   name TEXT NOT NULL,
-  parent_id INTEGER REFERENCES lists(id),
+  parent_list_id INTEGER REFERENCES lists(list_id),
   created_at TIMESTAMP NOT NULL DEFAULT current_timestamp,
   modified_at TIMESTAMP NOT NULL DEFAULT current_timestamp
 );
 
 CREATE TABLE list_items (
-  item_id INTEGER REFERENCES items(id),
-  list_id INTEGER REFERENCES lists(id),
+  item_id INTEGER REFERENCES items(item_id),
+  list_id INTEGER REFERENCES lists(list_id),
   position INTEGER NOT NULL DEFAULT 0,
 
   PRIMARY KEY(item_id, list_id),
@@ -61,7 +61,7 @@ CREATE TABLE list_items (
 );
 
 CREATE TABLE tags (
-  id INTEGER PRIMARY KEY,
+  tag_id INTEGER PRIMARY KEY,
   name TEXT UNIQUE NOT NULL COLLATE NOCASE,
   color INTEGER,
   created_at TIMESTAMP NOT NULL DEFAULT current_timestamp,
@@ -69,15 +69,13 @@ CREATE TABLE tags (
 );
 
 CREATE TABLE item_tags (
-  item_id INTEGER REFERENCES items(id),
-  tag_id INTEGER REFERENCES tags(id),
+  item_id INTEGER REFERENCES items(item_id),
+  tag_id INTEGER REFERENCES tags(tag_id),
 
   PRIMARY KEY(item_id, tag_id)
 );
 
 CREATE TABLE deleted_items (
-  item_id INTEGER NOT NULL REFERENCES items(id),
-  deleted_at TIMESTAMP NOT NULL DEFAULT current_timestamp,
-
-  PRIMARY KEY(item_id, deleted_at)
+  item_id INTEGER PRIMARY KEY REFERENCES items(item_id),
+  deleted_at TIMESTAMP NOT NULL DEFAULT current_timestamp
 );
--- db/schema.sql
+++ db/schema.sql
@@ -12,19 +12,19 @@ PRAGMA user_version=1603291819;
 PRAGMA foreign_keys=OFF;
 BEGIN TRANSACTION;
 CREATE TABLE archive (
-  id TEXT PRIMARY KEY,
+  archive_id TEXT PRIMARY KEY,
   name TEXT NOT NULL,
   created_at TIMESTAMP NOT NULL DEFAULT current_timestamp,
   opened_at TIMESTAMP NOT NULL DEFAULT current_timestamp
 );
 CREATE TABLE items (
-  id INTEGER PRIMARY KEY,
+  item_id INTEGER PRIMARY KEY,
   created_at TIMESTAMP NOT NULL DEFAULT current_timestamp,
   modified_at TIMESTAMP NOT NULL DEFAULT current_timestamp
 );
 CREATE TABLE photos (
-  id INTEGER PRIMARY KEY,
-  item_id INTEGER NOT NULL REFERENCES items(id),
+  photo_id INTEGER PRIMARY KEY,
+  item_id INTEGER NOT NULL REFERENCES items(item_id),
   path TEXT NOT NULL,
   mimetype TEXT,
   checksum TEXT,
@@ -33,8 +33,8 @@ CREATE TABLE photos (
   modified_at TIMESTAMP NOT NULL DEFAULT current_timestamp
 );
 CREATE TABLE selections (
-  id INTEGER PRIMARY KEY,
-  photo_id INTEGER NOT NULL REFERENCES photos(id),
+  selection_id INTEGER PRIMARY KEY,
+  photo_id INTEGER NOT NULL REFERENCES photos(photo_id),
   position INTEGER NOT NULL DEFAULT 0,
   top INTEGER NOT NULL DEFAULT 0,
   left INTEGER NOT NULL DEFAULT 0,
@@ -46,43 +46,41 @@ CREATE TABLE selections (
   UNIQUE(photo_id, position)
 );
 CREATE TABLE notes (
-  id INTEGER PRIMARY KEY,
+  note_id INTEGER PRIMARY KEY,
   created_at TIMESTAMP NOT NULL DEFAULT current_timestamp,
   modified_at TIMESTAMP NOT NULL DEFAULT current_timestamp
 );
 CREATE TABLE lists (
-  id INTEGER PRIMARY KEY,
+  list_id INTEGER PRIMARY KEY,
   name TEXT NOT NULL,
-  parent_id INTEGER REFERENCES lists(id),
+  parent_list_id INTEGER REFERENCES lists(list_id),
   created_at TIMESTAMP NOT NULL DEFAULT current_timestamp,
   modified_at TIMESTAMP NOT NULL DEFAULT current_timestamp
 );
 CREATE TABLE list_items (
-  item_id INTEGER REFERENCES items(id),
-  list_id INTEGER REFERENCES lists(id),
+  item_id INTEGER REFERENCES items(item_id),
+  list_id INTEGER REFERENCES lists(list_id),
   position INTEGER NOT NULL DEFAULT 0,
 
   PRIMARY KEY(item_id, list_id),
   UNIQUE(item_id, list_id, position)
 );
 CREATE TABLE tags (
-  id INTEGER PRIMARY KEY,
+  tag_id INTEGER PRIMARY KEY,
   name TEXT UNIQUE NOT NULL COLLATE NOCASE,
   color INTEGER,
   created_at TIMESTAMP NOT NULL DEFAULT current_timestamp,
   modified_at TIMESTAMP NOT NULL DEFAULT current_timestamp
 );
 CREATE TABLE item_tags (
-  item_id INTEGER REFERENCES items(id),
-  tag_id INTEGER REFERENCES tags(id),
+  item_id INTEGER REFERENCES items(item_id),
+  tag_id INTEGER REFERENCES tags(tag_id),
 
   PRIMARY KEY(item_id, tag_id)
 );
 CREATE TABLE deleted_items (
-  item_id INTEGER NOT NULL REFERENCES items(id),
-  deleted_at TIMESTAMP NOT NULL DEFAULT current_timestamp,
-
-  PRIMARY KEY(item_id, deleted_at)
+  item_id INTEGER PRIMARY KEY REFERENCES items(item_id),
+  deleted_at TIMESTAMP NOT NULL DEFAULT current_timestamp
 );
 COMMIT;
 PRAGMA foreign_keys=ON;
