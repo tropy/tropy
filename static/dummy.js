6a268a7b07e2d9ac7a5006912968c75b8a921fef
Sylvester Keil <sylvester@keil.or.at>
2016-06-07 23:42:42 +0200

    Hook-up dummy hotspots

--- static/dummy.js
+++ static/dummy.js
@@ -1,12 +1,28 @@
 'use strict'
 
 {
-  const { ready, $, on, toggle } = require('../lib/dom')
+  const { ready, $, attr, on, toggle } = require('../lib/dom')
 
   ready(() => {
-    on($('.container'), 'click', function () {
-      toggle(this, 'item')
-    })
-  })
 
+    const C = $('.container')
+    const P = $('#p')
+    const I = $('#i')
+
+    const hotspots = $('.hotspots').children
+
+    function activate(node) {
+      P.src = `assets/images/dummy/p-${attr(node, 'data-p')}.png`
+      I.src = `assets/images/dummy/i-${attr(node, 'data-i')}.png`
+
+      toggle(C, 'item', attr(node, 'data-view') === 'item')
+    }
+
+    for (let hs of hotspots) {
+      on(hs, 'click', function () { activate(this) })
+    }
+
+    activate(hotspots[0])
+
+  })
 }
