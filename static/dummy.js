0209cf3ca53d1eaa85d146db8f96e32f465de00c
Sylvester Keil <sylvester@keil.or.at>
2016-06-07 23:54:38 +0200

    Set hotspot active class

--- static/dummy.js
+++ static/dummy.js
@@ -12,8 +12,13 @@
     const hotspots = $('.hotspots').children
 
     function activate(node) {
+
       P.src = `assets/images/dummy/p-${attr(node, 'data-p')}.png`
       I.src = `assets/images/dummy/i-${attr(node, 'data-i')}.png`
+
+      for (let hs of hotspots) {
+        toggle(hs, 'active', hs === node)
+      }
 
       toggle(C, 'item', attr(node, 'data-view') === 'i')
     }
