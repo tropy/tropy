7eeaf1a3db277c6b5ecffbcf8282fd006491d0b8
Sylvester Keil <sylvester@keil.or.at>
2016-06-15 19:32:14 +0200

    Load localized strings in project container

--- src/containers/project.js
+++ src/containers/project.js
@@ -1,13 +1,42 @@
 'use strict'
 
 const React = require('react')
+
 const { IntlProvider } = require('react-intl')
 const { Project } = require('../components/project')
+const { Strings } = require('../common/res')
 
-const ProjectContainer = () => (
-  <IntlProvider locale={ARGS.locale}>
-    <Project/>
-  </IntlProvider>
-)
+
+class ProjectContainer extends React.Component {
+  constructor() {
+    super()
+
+    this.state = {
+      locale: ARGS.locale,
+      messages: null,
+      defaultLocale: 'en'
+    }
+  }
+
+  componentWillMount() {
+    Strings
+      .open(this.state.locale)
+
+      .catch({ code: 'ENOENT' }, () =>
+          Strings.open(this.state.defaultLocale))
+
+      .then(strings => {
+        this.setState({ messages: strings })
+      })
+  }
+
+  render() {
+    return (
+      <IntlProvider {...this.state} key={this.state.locale}>
+        <Project/>
+      </IntlProvider>
+    )
+  }
+}
 
 module.exports = ProjectContainer
