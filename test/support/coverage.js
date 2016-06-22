98d2272e7941650c9fe6dc1133953dfca6f14957
Sylvester Keil <sylvester@keil.or.at>
2016-06-22 17:03:51 +0200

    Use a sync file matcher for coverage

--- test/support/coverage.js
+++ test/support/coverage.js
@@ -1,31 +1,38 @@
 'use strict'
 
-const path = require('path')
 const istanbul = require('istanbul')
+const glob = require('glob')
+
+const { resolve } = require('path')
 
 // Override __src to point to instrumented sources!
-global.__src = path.resolve(__dirname, '..', '..', 'lib')
+global.__src = resolve(__dirname, '..', '..', 'lib')
 
+const root = resolve(__dirname, '..', '..')
 const reporter = new istanbul.Reporter()
 reporter.addAll(['text-summary', 'json'])
 
-istanbul.matcherFor({
-  root: path.resolve(__dirname, '..', '..'),
-  includes: ['lib/**/*.js']
-}, (err, match) => {
+function matcher(pattern) {
+  const map = {}
+  const fn = function (file) { return map[file] }
 
-  let instrumenter = new istanbul.Instrumenter()
-  let transformer = instrumenter.instrumentSync.bind(instrumenter)
+  fn.files = glob.sync(pattern, { root, realpath: true })
+  for (let file of fn.files) map[file] = true
 
-  istanbul.hook.hookRequire(match, transformer, {})
+  return fn
+}
 
-  global.__coverage__ = {}
-})
+const instrumenter = new istanbul.Instrumenter()
+const transformer = instrumenter.instrumentSync.bind(instrumenter)
+
+istanbul.hook.hookRequire(matcher('lib/**/*.js'), transformer, {})
+
+global.__coverage__ = {}
 
 function done() {
   // Check for global variable
   // Touch uncovered files
-  let collector = new istanbul.Collector()
+  const collector = new istanbul.Collector()
 
   collector.add(__coverage__)
 
@@ -35,5 +42,9 @@ function done() {
 if (process.type === 'browser') {
   process.on('exit', done)
 } else {
-  window.addEventListener('mocha-done', done)
+  console.log('register unload handler')
+  window.addEventListener('beforeunload', done)
+  window.addEventListener('beforeunload', () => {
+    console.log('unloading...')
+  })
 }
