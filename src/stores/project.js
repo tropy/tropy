3204a3ed90e0920198f60cfe8b0fd35972dbe69b
Sylvester Keil <sylvester@keil.or.at>
2016-07-20 21:54:34 +0200

    Enable redux devtools in dev and debug mode

--- src/stores/project.js
+++ src/stores/project.js
@@ -1,21 +1,30 @@
 'use strict'
 
-const { createStore, applyMiddleware, combineReducers } = require('redux')
+const {
+  createStore, applyMiddleware, combineReducers, compose
+} = require('redux')
+
 const { default: thunk } = require('redux-thunk')
 const { project } = require('../reducers/project')
 const { intl } = require('../reducers/intl')
 
+const dev = (ARGS.environment === 'development' || ARGS.debug)
+
 module.exports = {
   create(init = {}) {
-    return createStore(
-      combineReducers({
-        project,
-        intl
-      }),
-      init,
-      applyMiddleware(
-        thunk
-      )
+
+    const reducer = combineReducers({
+      project,
+      intl
+    })
+
+    const middleware = applyMiddleware(
+      thunk
     )
+    const enhancer = (dev && window.devToolsExtension) ?
+      compose(middleware, window.devToolsExtension()) :
+      middleware
+
+    return createStore(reducer, init, enhancer)
   }
 }
