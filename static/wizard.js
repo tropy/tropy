1b0640d3a7424416ec604508e5d833876b3dbb57
Sylvester Keil <sylvester@keil.or.at>
2016-07-26 14:55:42 +0200

    Add wizard boilerplate code

--- static/wizard.js
+++ static/wizard.js
@@ -1,5 +1,26 @@
 'use strict'
 
 {
+  const { createElement: element } = require('react')
+  const { render } = require('react-dom')
+  const { all } = require('bluebird')
+  const { ready, $ } = require('../lib/dom')
+  const { create } = require('../lib/stores/wizard')
+  const { Main } = require('../lib/containers/main')
+  const { Wizard } = require('../lib/components/wizard')
+  const { getMessages } = require('../lib/actions/intl')
+
+  const store = create()
+
+  all([
+    store.dispatch(getMessages()),
+    ready
+  ])
+    .then(() => {
+      render(
+        element(Main, { store }, element(Wizard)),
+        $('main')
+      )
+    })
 
 }
