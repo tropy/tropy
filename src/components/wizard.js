773a49412744ea581b19f53f0fac995f9f5836ff
Sylvester Keil <sylvester@keil.or.at>
2016-07-27 17:34:07 +0200

    WIP wizard container

--- src/components/wizard.js
+++ src/components/wizard.js
@@ -1,26 +1,58 @@
 'use strict'
 
 const React = require('react')
-const { FormattedMessage } = require('react-intl')
+const { PropTypes } = React
+const { connect } = require('react-redux')
+const { FormattedMessage, injectIntl, intlShape } = require('react-intl')
 const { Steps, Step } = require('./steps')
 
-const Wizard = () => (
+let Wizard = ({ submit, update, project, intl }) => (
   <div id="wizard">
     <Steps>
       <Step>
 
         <h1><FormattedMessage id="wizard.project.title"/></h1>
 
-        <label>
-          <FormattedMessage id="wizard.project.name"/>
-        </label>
-        <input ref="name"/>
+        <input
+          type="text"
+          value={project.name}
+          onChange={update()}
+          placeholder={
+            intl.formatMessage({ id: 'wizard.project.name' })
+          }/>
+
+        <button onClick={submit()} disabled={!project.name}>
+          <FormattedMessage id="wizard.project.submit"/>
+        </button>
 
       </Step>
     </Steps>
   </div>
 )
 
-module.exports = {
-  Wizard
+Wizard.propTypes = {
+  intl: intlShape.isRequired,
+
+  submit: PropTypes.function,
+  update: PropTypes.function,
+
+  project: PropTypes.shape({
+    name: PropTypes.string
+  })
 }
+
+module.exports.Component = Wizard
+
+Wizard = connect(
+  state => ({
+    project: state.project || {}
+  }),
+  () => ({
+    update: () => {},
+    submit: () => {}
+  })
+)(Wizard)
+
+Wizard = injectIntl(Wizard)
+
+module.exports.Wizard = Wizard
--- test/components/wizard_test.js
+++ test/components/wizard_test.js
@@ -2,12 +2,28 @@
 
 const React = require('react')
 const { shallow } = require('enzyme')
+const { intl } = require('../support/intl')
 
 describe('Wizard', () => {
-  const { Wizard } = __require('components/wizard')
+  const { Component: Wizard } = __require('components/wizard')
+
+  let update
+  let submit
+
+  beforeEach(() => {
+    update = sinon.spy()
+    submit = sinon.spy()
+  })
 
   it('has id wizard', () => {
-    expect(shallow(<Wizard/>)).to.have.id('wizard')
+    expect(shallow(
+      <Wizard
+        update={update}
+        submit={submit}
+        project={{ name: 'tropy' }}
+        intl={intl}/>
+
+    )).to.have.id('wizard')
   })
 
 })
