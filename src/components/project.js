28719d80793e20a3f28f9004ac8a900411134f5d
Johannes Krtek <johannes.krtek@flachware.com>
2016-08-04 11:06:08 +0200

    Fix error.

--- src/components/project.js
+++ src/components/project.js
@@ -3,7 +3,7 @@
 const React = require('react')
 
 const { ProjectSidebar } = require('../containers/sidebar')
-const { Items, Item } = require('./items')
+const { Items } = require('./items')
 
 const Project = () => (
   <div id="project">
