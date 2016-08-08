18d24a9f387c2b1c0862639c72ae3a37452c7e79
Johannes Krtek <johannes.krtek@flachware.com>
2016-08-09 00:04:38 +0200

    Add item-list scaffold.

--- src/components/items.js
+++ src/components/items.js
@@ -8,11 +8,30 @@ const { Viewer } = require('./viewer')
 
 
 const Items = () => (
-  <section id="items">
+  <section id="items" className="list-view">
     <header>
       <Toolbar/>
     </header>
-    Items
+    <ul className="item-list-header">
+      <li className="item-list-header-row">
+        <div className="title">Title</div>
+        <div className="type">Type</div>
+        <div className="date">Date</div>
+        <div className="box">Box</div>
+        <div className="photos"></div>
+      </li>
+    </ul>
+    <div className="item-list-container">
+      <ul className="item-list">
+        <li className="item">
+          <div className="title">Title</div>
+          <div className="type">Type</div>
+          <div className="date">Date</div>
+          <div className="box">Box</div>
+          <div className="photos"></div>
+        </li>
+      </ul>
+    </div>
     <Item/>
   </section>
 )
