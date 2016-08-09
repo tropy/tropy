20e7971fa8f59cbcf607aec903c93c2498ab6722
Johannes Krtek <johannes.krtek@flachware.com>
2016-08-09 18:41:42 +0200

    Add responsive dev thumbnails.

--- src/components/items.js
+++ src/components/items.js
@@ -31,42 +31,66 @@ const Items = () => (
     <div className="item-list-container">
       <ul className="item-list">
         <li className="item">
-          <div className="metadata" style={col1}>Application Norman Bailey</div>
+          <div className="metadata" style={col1}>
+            <img src={'dev/dummy-24.jpg'} srcSet={'dev/dummy-24-2x.jpg 2x'}
+              width={24} height={24}/>
+            Application Norman Bailey
+          </div>
           <div className="metadata" style={col2}>Application Form</div>
           <div className="metadata" style={col3}>1897-07-26</div>
           <div className="metadata number" style={col4}>17</div>
           <div className="metadata number" style={col5}>2</div>
         </li>
         <li className="item">
-          <div className="metadata">Norman Bailey</div>
+          <div className="metadata">
+            <img src={'dev/dummy-24.jpg'} srcSet={'dev/dummy-24-2x.jpg 2x'}
+              width={24} height={24}/>
+            Norman Bailey
+          </div>
           <div className="metadata">Portrait</div>
           <div className="metadata">1844</div>
           <div className="metadata number">17</div>
           <div className="metadata number">1</div>
         </li>
         <li className="item">
-          <div className="metadata">Application H. F. Cary</div>
+          <div className="metadata">
+            <img src={'dev/dummy-24.jpg'} srcSet={'dev/dummy-24-2x.jpg 2x'}
+              width={24} height={24}/>
+            Application H. F. Cary
+          </div>
           <div className="metadata">Application Form</div>
           <div className="metadata">1899-10-24</div>
           <div className="metadata number">17</div>
           <div className="metadata number">2</div>
         </li>
         <li className="item">
-          <div className="metadata">Denver International Communication</div>
+          <div className="metadata">
+            <img src={'dev/dummy-24.jpg'} srcSet={'dev/dummy-24-2x.jpg 2x'}
+              width={24} height={24}/>
+            Denver International Communication
+          </div>
           <div className="metadata">Correspondence</div>
           <div className="metadata">1899-12-16</div>
           <div className="metadata number">17</div>
           <div className="metadata number">1</div>
         </li>
         <li className="item">
-          <div className="metadata">Frank Cary</div>
+          <div className="metadata">
+            <img src={'dev/dummy-24.jpg'} srcSet={'dev/dummy-24-2x.jpg 2x'}
+              width={24} height={24}/>
+            Frank Cary
+          </div>
           <div className="metadata">Portrait</div>
           <div className="metadata">1868</div>
           <div className="metadata number">17</div>
           <div className="metadata number">1</div>
         </li>
         <li className="item active">
-          <div className="metadata">Denver to Chicago</div>
+          <div className="metadata">
+            <img src={'dev/dummy-24.jpg'} srcSet={'dev/dummy-24-2x.jpg 2x'}
+              width={24} height={24}/>
+            Denver to Chicago
+          </div>
           <div className="metadata">Correspondence</div>
           <div className="metadata">1899-12-24</div>
           <div className="metadata number">27</div>
