03691404597727bd6d3ca483be986151ed074519
Johannes Krtek <johannes.krtek@flachware.com>
2016-08-09 14:25:22 +0200

    Add some dummy content.

--- src/components/items.js
+++ src/components/items.js
@@ -18,17 +18,45 @@ const Items = () => (
         <div className="type">Type</div>
         <div className="date">Date</div>
         <div className="box">Box</div>
-        <div className="photos"></div>
+        <div className="photos">Photos</div>
       </li>
     </ul>
     <div className="item-list-container">
       <ul className="item-list">
         <li className="item">
-          <div className="title">Title</div>
-          <div className="type">Type</div>
-          <div className="date">Date</div>
-          <div className="box">Box</div>
-          <div className="photos"></div>
+          <div className="title">Application Norman Bailey</div>
+          <div className="type">Application Form</div>
+          <div className="date">1897-07-26</div>
+          <div className="box">17</div>
+          <div className="photos">2</div>
+        </li>
+        <li className="item">
+          <div className="title">Norman Bailey</div>
+          <div className="type">Portrait</div>
+          <div className="date">1844</div>
+          <div className="box">17</div>
+          <div className="photos">1</div>
+        </li>
+        <li className="item">
+          <div className="title">Application H. F. Cary</div>
+          <div className="type">Application Form</div>
+          <div className="date">1899-10-24</div>
+          <div className="box">17</div>
+          <div className="photos">2</div>
+        </li>
+        <li className="item">
+          <div className="title">Denver International Communication</div>
+          <div className="type">Correspondence</div>
+          <div className="date">1899-12-16</div>
+          <div className="box">17</div>
+          <div className="photos">1</div>
+        </li>
+        <li className="item">
+          <div className="title">Frank Cary</div>
+          <div className="type">Portrait</div>
+          <div className="date">1868</div>
+          <div className="box">17</div>
+          <div className="photos">1</div>
         </li>
       </ul>
     </div>
