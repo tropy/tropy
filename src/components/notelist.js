3fb3c98edfa8412d382805ab56ec32fc609d244c
Johannes Krtek <johannes.krtek@flachware.com>
2016-08-11 11:45:52 +0200

    Add CSS multiline truncation to note-list.

--- src/components/notelist.js
+++ src/components/notelist.js
@@ -5,13 +5,27 @@ const React = require('react')
 const NoteList = () => (
   <ul className="note-list">
     <li className="note active">
-      Denver, Colorado Dec. 24th, 1899.<br/>Mr. Wm. A. Pinkerton, …
+      <div className="css-multiline-truncate">
+        Denver, Colorado Dec. 24th, 1899.<br/>
+        Mr. Wm. A. Pinkerton,<br/>
+        Chicago, Illinois
+      </div>
     </li>
     <li className="note">
-      Name in full: H. Frank Clark<br/>Age: 31 …
+      <div className="css-multiline-truncate">
+        Name in full: H. Frank Clark<br/>
+        Age: 31<br/>
+        Height: 5-11 3/4
+      </div>
     </li>
     <li className="note">
-      Dear Sir: -<br/>I am in receipt of yours of the 16th …
+      <div className="css-multiline-truncate">
+        I am in receipt of yours of the 16th inst. enclosing Application and
+        History of H. Frank Cary, which I have read over very carefully and note
+        his references have been run down very carefully in Chicago and that
+        every person seen gives him a first class character and seem to be very
+        honest about it.
+      </div>
     </li>
   </ul>
 )
