00fd85159f96844671535b5ca5c02f85a6e0c24b
Sylvester Keil <sylvester@keil.or.at>
2016-09-02 11:37:15 +0200

    Add action filter to ipc forwarder

--- src/sagas/ipc.js
+++ src/sagas/ipc.js
@@ -4,23 +4,15 @@ const { takeEvery: every, eventChannel } = require('redux-saga')
 const { call, fork, put, take } = require('redux-saga/effects')
 const { ipcRenderer: ipc } = require('electron')
 const { warn, debug } = require('../common/log')
-
-function channel(name) {
-  return eventChannel(emitter => {
-    const listener = (_, action) => {
-      emitter(action)
-    }
-
-    ipc.on(name, listener)
-
-    return () => ipc.removeListener(name, listener)
-  })
-}
+const { id } = require('../common/util')
+const { TICK } = require('../constants/history')
 
 module.exports = {
 
   forward({ type, payload, meta }) {
-    if (meta && meta.ipc) ipc.send(type, payload)
+    if (meta && meta.ipc) {
+      ipc.send(type, (ACTION_FILTER[type] || id)(payload))
+    }
   },
 
   *receive() {
@@ -43,4 +35,23 @@ module.exports = {
     yield fork(module.exports.receive)
   }
 
+}
+
+const ACTION_FILTER = {
+  [TICK]: payload => ({
+    past: payload.past.length,
+    future: payload.future.length
+  })
+}
+
+function channel(name) {
+  return eventChannel(emitter => {
+    const listener = (_, action) => {
+      emitter(action)
+    }
+
+    ipc.on(name, listener)
+
+    return () => ipc.removeListener(name, listener)
+  })
 }
