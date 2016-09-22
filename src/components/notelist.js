d4c64f023ec77f2662c084aeddbf9e60bdc1e626
Johannes Krtek <johannes.krtek@flachware.com>
2016-09-22 21:59:16 +0200

    Fix css multiline truncation.

--- src/components/notelist.js
+++ src/components/notelist.js
@@ -6,16 +6,24 @@ const NoteList = () => (
   <ul className="note-list">
     <li className="note active">
       <div className="css-multiline-truncate">
-        Denver, Colorado Dec. 24th, 1899.<br/>
-        Mr. Wm. A. Pinkerton,<br/>
+        Denver, Colorado Dec. 24th, 1899.
+        Mr. Wm. A. Pinkerton,
         Chicago, Illinois
+        Dear Sir: --
+        Replying to Mr. R.A. Pinkerton's letter to you dated the 20th inst. I
+        note he asks four questions:
       </div>
     </li>
     <li className="note">
       <div className="css-multiline-truncate">
-        Name in full: H. Frank Clark<br/>
-        Age: 31<br/>
+        Name in full: H. Frank Clark
+        Age: 31
         Height: 5-11 3/4
+        Weight: 170
+        Build: Medium
+        Nationality: American
+        Place of Birth: Oswego NY
+        Married of Single: Married
       </div>
     </li>
     <li className="note">
