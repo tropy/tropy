c0c77edc7f2dd4e00738e143937ed9c6db21d9d0
Sylvester Keil <sylvester@keil.or.at>
2016-09-24 12:50:19 +0200

    Fix seq middleware babel bug

--- src/middleware/seq.js
+++ src/middleware/seq.js
@@ -1,11 +1,12 @@
 'use strict'
 
-module.exports = {
-  seq(k = 1) {
-    function inc() {
-      return (k = Number.isSafeInteger(k) ? ++k : -k), k
-    }
+const sequence = (k = 1) => () => (
+  (k = Number.isSafeInteger(k) ? ++k : -k), k
+)
 
+module.exports = {
+  seq() {
+    const inc = sequence()
     return next => action => {
       action.meta = { ...action.meta, seq: inc() }
       return next(action)
