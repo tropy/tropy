8e3b54994b196b48f910a4773d280a33330b84ac
Sylvester Keil <sylvester@keil.or.at>
2016-11-08 13:38:58 +0100

    Add photo constants

--- src/constants/index.js
+++ src/constants/index.js
@@ -4,6 +4,7 @@ module.exports = {
   ITEM: require('./item'),
   LIST: require('./list'),
   METADATA: require('./metadata'),
+  PHOTO: require('./photo'),
   PROJECT: require('./project'),
   TAG: require('./tag'),
   UI: require('./ui'),
---
+++ src/constants/photo.js
@@ -0,0 +1,13 @@
+'use strict'
+
+module.exports = {
+  CREATE: 'photo.create',
+
+  INSERT: 'photo.insert',
+  REMOVE: 'photo.remove',
+
+  SAVE: 'photo.save',
+  UPDATE: 'photo.update',
+
+  LOAD: 'photo.load'
+}
