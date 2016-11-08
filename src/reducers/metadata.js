77f5a2fdf1bf0f424abdbf926d26e5e761454d38
Sylvester Keil <sylvester@keil.or.at>
2016-11-08 15:10:59 +0100

    Remove metadata on item/photo remove

--- src/reducers/metadata.js
+++ src/reducers/metadata.js
@@ -1,12 +1,18 @@
 'use strict'
 
 const { LOAD, UPDATE } = require('../constants/metadata')
+const { ITEM, PHOTO } = require('../constants')
+const { omit } = require('../common/util')
 
 module.exports = {
   metadata(state = {}, { type, payload, meta, error }) {
     switch (type) {
       case LOAD:
         return (meta.done && !error) ? { ...state, ...payload } : state
+
+      case ITEM.REMOVE:
+      case PHOTO.REMOVE:
+        return omit(state, payload)
 
       case UPDATE: {
         const [id, data] = payload
