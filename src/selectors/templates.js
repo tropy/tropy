d0b96acd3f28b4698477c2b2e6d55d598cd4621a
Sylvester Keil <sylvester@keil.or.at>
2016-11-24 18:38:19 +0100

    Add template fixture

---
+++ src/selectors/templates.js
@@ -0,0 +1,31 @@
+'use strict'
+
+const { DC } = require('../constants/properties')
+
+const T = {
+  core: [
+    DC.TYPE,
+    DC.TITLE,
+    DC.DESCRIPTION,
+    DC.DATE,
+    DC.CREATOR,
+    DC.PUBLISHER,
+    DC.SOURCE,
+    DC.RIGHTS
+  ],
+
+  photo: [
+    DC.TITLE,
+    DC.DESCRIPTION,
+    DC.DATE
+  ]
+}
+
+const getTemplate = (state, props) =>
+  T[props.template]
+    .map(property => ({ property: state.properties[property] }))
+
+
+module.exports = {
+  getTemplate
+}
