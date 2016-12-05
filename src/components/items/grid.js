94a7742fa92176bd691e896b2a319121be34a1f0
Johannes Krtek <johannes.krtek@flachware.com>
2016-12-05 12:34:34 +0100

    Add more dummy tiles.

--- src/components/items/grid.js
+++ src/components/items/grid.js
@@ -38,6 +38,8 @@ class ItemGrid extends Component {
           )}
 
           {tile}{tile}{tile}{tile}{tile}{tile}{tile}{tile}{tile}{tile}
+          {tile}{tile}{tile}{tile}{tile}{tile}{tile}{tile}{tile}{tile}
+          {tile}{tile}{tile}{tile}{tile}{tile}{tile}{tile}{tile}{tile}
         </ul>
       </div>
     )
