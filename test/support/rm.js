29f228f3fb03fa2dd2860c49b0c7e8b97c6208e0
Sylvester Keil <sylvester@keil.or.at>
2016-12-19 21:10:03 +0100

    Increase max retry for AppVeyor

--- test/support/rm.js
+++ test/support/rm.js
@@ -3,7 +3,7 @@
 const { promisify } = require('bluebird')
 const rimraf = promisify(require('rimraf'))
 
-const MAX_RETRY = (process.env.APPVEYOR) ? 10 : 3
+const MAX_RETRY = (process.env.APPVEYOR) ? 20 : 3
 
 module.exports = {
   rm(file) {
