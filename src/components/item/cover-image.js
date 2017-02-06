57b5acbcc82e4a9ea8c1cb12908b8559cbe6aa12
Sylvester Keil <sylvester@keil.or.at>
2017-02-06 21:44:42 +0100

    Use Thumbnail in CoverImage

--- src/components/item/cover-image.js
+++ src/components/item/cover-image.js
@@ -1,82 +1,32 @@
 'use strict'
 
 const React = require('react')
-const { Component, PropTypes } = React
-const { IconItem } = require('../icons')
-const { imageURL } = require('../../common/cache')
-const { createClickHandler } = require('../util')
-const { noop } = require('../../common/util')
+const { PropTypes } = React
+const { Thumbnail } = require('../photo')
+const { get } = require('../../common/util')
 const cn = require('classnames')
 
-class CoverImage extends Component {
+const { shape, number, arrayOf } = PropTypes
 
-  get cardinality() {
-    const { item } = this.props
-    return item && item.photos ? item.photos.length : 0
-  }
+const card = (item) =>
+  get(item, 'photos.length', 0)
 
-  get src() {
-    const { item: { cover, photos }, cache, size } = this.props
-    const csize = size > 48 ? 512 : 48
+const cover = (item) =>
+  item.cover || get(item, ['photos', 0])
 
-    switch (true) {
-      case !!(cover):
-        return imageURL(cache, cover, csize)
-      case !!(photos.length):
-        return imageURL(cache, photos[0], csize)
-    }
-  }
 
-  get style() {
-    return {
-      width: `${this.props.size}px`,
-      height: `${this.props.size}px`
-    }
-  }
+const CoverImage = ({ item, ...props }) => (
+  <div className={cn({ 'cover-image': true, 'stack': card(item) > 1 })}>
+    <Thumbnail {...props} id={cover(item)}/>
+  </div>
+)
 
-  handleClick = createClickHandler({
-    onClick: event => this.props.onClick(event),
-    onDoubleClick: event => this.props.onDoubleClick(event)
-  })
-
-  render() {
-    const { onClickInside } = this.props
-    const { style, cardinality, handleClick } = this
-
-    return (
-      <figure
-        className={cn({ 'cover-image': true, 'stack': cardinality > 1 })}
-        style={style}
-        onClick={onClickInside}
-        onMouseDown={handleClick}>
-        {cardinality === 0
-          ? <IconItem/>
-          : <img srcSet={`${encodeURI(this.src)} 2x`}/>
-        }
-      </figure>
-    )
-  }
-
-  static propTypes = {
-    item: PropTypes.shape({
-      id: PropTypes.number.isRequired,
-      photos: PropTypes.arrayOf(PropTypes.number),
-      cover: PropTypes.number
-    }).isRequired,
-
-    size: PropTypes.number.isRequired,
-    cache: PropTypes.string.isRequired,
-
-    onClickInside: PropTypes.func,
-    onClick: PropTypes.func,
-    onDoubleClick: PropTypes.func
-  }
-
-  static defaultProps = {
-    onClick: noop,
-    onClickInside: noop,
-    onDoubleClick: noop
-  }
+CoverImage.propTypes = {
+  item: shape({
+    id: number.isRequired,
+    photos: arrayOf(number),
+    cover: number
+  }).isRequired,
 }
 
 module.exports = {
