a00d95b93e00c5a81b600d63bae037dfb5353bae
Sylvester Keil <sylvester@keil.or.at>
2017-02-08 21:26:26 +0100

    Add cornell-obama template

--- src/selectors/templates.js
+++ src/selectors/templates.js
@@ -8,7 +8,7 @@ const { entries } = Object
 const { DC } = require('../constants/properties')
 
 const CORE = 'https://schema.tropy.org/v1/templates/core'
-const FULL = 'https://schema.tropy.org/v1/templates/full'
+const CORNELL = 'https://schema.tropy.org/v1/templates/cornell-obama'
 const PHOTO = 'https://schema.tropy.org/v1/templates/photo'
 
 const T = {
@@ -17,26 +17,33 @@ const T = {
     name: 'Core Item',
     type: 'item',
     fields: [
-      DC.TYPE,
-      DC.TITLE,
-      DC.DATE,
-      DC.DESCRIPTION
+      { property: DC.TITLE },
+      { property: DC.TYPE },
+      { property: DC.DATE },
+      { property: DC.CREATOR },
+      { property: DC.DESCRIPTION }
     ],
   },
 
-  [FULL]: {
-    uri: FULL,
-    name: 'Expanded Item',
+  [CORNELL]: {
+    uri: CORNELL,
+    name: 'Cornell Obama',
     type: 'item',
     fields: [
-      DC.TYPE,
-      DC.TITLE,
-      DC.DESCRIPTION,
-      DC.DATE,
-      DC.CREATOR,
-      DC.PUBLISHER,
-      DC.SOURCE,
-      DC.RIGHTS
+      { property: DC.TITLE },
+      { property: DC.DATE, hint: 'Date of creation of object' },
+      { property: DC.COVERAGE, hint: 'Use controlled vocabulary from Getty' },
+      { property: DC.DESCRIPTION },
+      {
+        property: DC.PUBLISHER,
+        constant: 'Division of Rare and Manuscript Collections, Cornell Library'
+      },
+      { property: DC.SOURCE },
+      { property: DC.IDENTIFIER },
+      {
+        property: DC.RIGHTS,
+        constant: "The Cornell University Library has made a reasonable effort to identify all possible rights holders in this image, but in this case, the current rights holders remain unknown.  The Library has elected to place the item online as an exercise of fair use for non-commercial educational use. If would like to learn more about this item and to hear from individuals or institutions that have any additional information as to rights holders: contact rareref@cornell.edu. Responsibility for making an independent legal assessment of an item and securing any necessary permissions ultimately rests with persons desiring to use the item. This image has been selected and made available by a user using Artstor's software tools. Artstor has not screened or selected this image or cleared any rights to it and is acting as an online service provider pursuant to 17 U.S.C. §512. Artstor disclaims any liability associated with the use of this image. Should you have any legal objection to the use of this image, please visit http://www.artstor.org/copyright for contact information and instructions on how to proceed."
+      }
     ],
   },
 
@@ -45,8 +52,8 @@ const T = {
     name: 'Tropy Photo',
     type: 'photo',
     fields: [
-      DC.TITLE,
-      DC.DATE
+      { property: DC.TITLE },
+      { property: DC.DATE }
     ]
   }
 }
@@ -60,7 +67,10 @@ const getTemplates = memo(
       .reduce((tpl, [k, v]) => {
         tpl[k] = {
           ...v,
-          fields: v.fields.map(p => ({ property: properties[p] }))
+          fields: v.fields.map(field => ({
+            ...field,
+            property: properties[field.property]
+          }))
         }
 
         return tpl
