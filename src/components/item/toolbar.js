5fdf82a14552c3ddd577c10225cad347e60fc3d4
Sylvester Keil <sylvester@keil.or.at>
2017-02-28 00:32:58 +0100

    Extract item toolbar component

---
+++ src/components/item/toolbar.js
@@ -0,0 +1,42 @@
+'use strict'
+
+const React = require('react')
+const { PureComponent, PropTypes } = React
+const { Toolbar, ToolGroup } = require('../toolbar')
+const { IconChevron16 } = require('../icons')
+const { IconButton } = require('../button')
+const { MODE } = require('../../constants/project')
+const { bool, func } = PropTypes
+
+
+class ItemToolbar extends PureComponent {
+
+  handleModeChange = () => {
+    this.props.onModeChange(MODE.PROJECT)
+  }
+
+  render() {
+    return (
+      <Toolbar onDoubleClick={ARGS.frameless ? this.props.onMaximize : null}>
+        <div className="toolbar-left">
+          <ToolGroup>
+            {this.props.isItemOpen &&
+              <IconButton
+                icon={<IconChevron16/>}
+                onClick={this.handleModeChange}/>}
+          </ToolGroup>
+        </div>
+      </Toolbar>
+    )
+  }
+
+  static propTypes = {
+    isItemOpen: bool.isRequired,
+    onMaximize: func.isRequired,
+    onModeChange: func.isRequired
+  }
+}
+
+module.exports = {
+  ItemToolbar
+}
