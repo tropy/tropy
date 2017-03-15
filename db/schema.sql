ccf0679aba1e7f8cd5a0d53dbc96908f919d826f
Sylvester Keil <sylvester@keil.or.at>
2017-03-15 21:19:43 +0100

    Add doc to notes schema

--- db/schema.sql
+++ db/schema.sql
@@ -90,6 +90,7 @@ CREATE TABLE notes (
   id           INTEGER  REFERENCES subjects ON DELETE CASCADE,
   position     INTEGER,
   text         TEXT     NOT NULL DEFAULT '',
+  doc,
   language     TEXT     NOT NULL DEFAULT 'en',
   created      NUMERIC  NOT NULL DEFAULT CURRENT_TIMESTAMP,
   modified     NUMERIC  NOT NULL DEFAULT CURRENT_TIMESTAMP,
