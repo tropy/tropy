2eb85e4391b46d73d06eb52f5fc1274f9793f319
Johannes Krtek <johannes.krtek@flachware.com>
2017-03-20 16:35:32 +0100

    Add context toolbars to editor toolbar.

--- src/components/editor/toolbar.js
+++ src/components/editor/toolbar.js
@@ -42,71 +42,81 @@ class EditorToolbar extends PureComponent {
   render() {
     return (
       <Toolbar isDraggable={false}>
-        <div className="toolbar-left">
-          {this.props.hasMarkTools &&
-            <ToolGroup>
-              <IconButton
-                canHaveFocus={false}
-                icon={<IconB/>}
-                isActive={this.props.isBoldActive}
-                title="editor.commands.bold"
-                onMouseDown={this.bold}/>
-              <IconButton
-                canHaveFocus={false}
-                icon={<IconI/>}
-                isActive={this.props.isItalicActive}
-                title="editor.commands.italic"
-                onMouseDown={this.italic}/>
-              <IconButton
-                canHaveFocus={false}
-                icon={<IconU/>}
-                isActive={this.props.isUnderlineActive}
-                title="editor.commands.underline"
-                onMouseDown={this.underline}/>
-              <IconButton
-                canHaveFocus={false}
-                icon={<IconS/>}
-                isActive={this.props.isStrikeThroughActive}
-                title="editor.commands.strikethrough"
-                onMouseDown={this.strikethrough}/>
-              <IconButton
-                canHaveFocus={false}
-                icon={<IconQ/>}
-                title="editor.commands.blockquote"
-                onMouseDown={this.blockquote}/>
-            </ToolGroup>
-          }
-          {this.props.hasListTools &&
-            <ToolGroup>
-              <IconButton
-                canHaveFocus={false}
-                icon={<IconBulletList/>}
-                title="editor.commands.ul"
-                onMouseDown={this.ul}/>
-              <IconButton
-                canHaveFocus={false}
-                icon={<IconNumberedList/>}
-                title="editor.commands.ol"
-                onMouseDown={this.ol}/>
-              <IconButton
-                canHaveFocus={false}
-                icon={<IconLift/>}
-                onMouseDown={this.liftListItem}/>
-              <IconButton
-                canHaveFocus={false}
-                icon={<IconSink/>}
-                onMouseDown={this.sinkListItem}/>
-            </ToolGroup>
-          }
-          {this.props.hasLinkTools &&
-            <ToolGroup>
-              <IconButton
-                isDisabled
-                canHaveFocus={false}
-                title="editor.commands.link"
-                icon={<IconLink/>}/>
-            </ToolGroup>
-          }
+        <div className="toolbar-context active">
+          <div className="toolbar-left">
+            {this.props.hasMarkTools &&
+              <ToolGroup>
+                <IconButton
+                  canHaveFocus={false}
+                  icon={<IconB/>}
+                  isActive={this.props.isBoldActive}
+                  title="editor.commands.bold"
+                  onMouseDown={this.bold}/>
+                <IconButton
+                  canHaveFocus={false}
+                  icon={<IconI/>}
+                  isActive={this.props.isItalicActive}
+                  title="editor.commands.italic"
+                  onMouseDown={this.italic}/>
+                <IconButton
+                  canHaveFocus={false}
+                  icon={<IconU/>}
+                  isActive={this.props.isUnderlineActive}
+                  title="editor.commands.underline"
+                  onMouseDown={this.underline}/>
+                <IconButton
+                  canHaveFocus={false}
+                  icon={<IconS/>}
+                  isActive={this.props.isStrikeThroughActive}
+                  title="editor.commands.strikethrough"
+                  onMouseDown={this.strikethrough}/>
+                <IconButton
+                  canHaveFocus={false}
+                  icon={<IconQ/>}
+                  title="editor.commands.blockquote"
+                  onMouseDown={this.blockquote}/>
+              </ToolGroup>
+            }
+            {this.props.hasListTools &&
+              <ToolGroup>
+                <IconButton
+                  canHaveFocus={false}
+                  icon={<IconBulletList/>}
+                  title="editor.commands.ul"
+                  onMouseDown={this.ul}/>
+                <IconButton
+                  canHaveFocus={false}
+                  icon={<IconNumberedList/>}
+                  title="editor.commands.ol"
+                  onMouseDown={this.ol}/>
+                <IconButton
+                  canHaveFocus={false}
+                  icon={<IconLift/>}
+                  onMouseDown={this.liftListItem}/>
+                <IconButton
+                  canHaveFocus={false}
+                  icon={<IconSink/>}
+                  onMouseDown={this.sinkListItem}/>
+              </ToolGroup>
+            }
+            {this.props.hasLinkTools &&
+              <ToolGroup>
+                <IconButton
+                  isDisabled
+                  canHaveFocus={false}
+                  title="editor.commands.link"
+                  icon={<IconLink/>}/>
+              </ToolGroup>
+            }
+          </div>
+        </div>
+        <div className="toolbar-context">
+          <span className="toolbar-left form-inline">
+            <input type="text" className="form-control"
+              placeholder="Link target"/>
+
+            <span className="btn btn-primary">OK</span>
+          </span>
         </div>
       </Toolbar>
     )
