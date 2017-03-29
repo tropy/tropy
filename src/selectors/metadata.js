118a263539cce486144e538b6fa9d03416862414
Sylvester Keil <sylvester@keil.or.at>
2017-03-29 20:52:39 +0200

    Add bulk data selector

--- src/selectors/metadata.js
+++ src/selectors/metadata.js
@@ -2,11 +2,39 @@
 
 const { createSelector: memo } = require('reselect')
 const { pluck } = require('./util')
+const {
+  cat, compose, filter, map, transduce, transformer
+} = require('transducers.js')
+
+
+const collect = transformer((acc, [uri, value]) => {
+  if (acc.data.hasOwnProperty(uri)) {
+    if (acc.data[uri].text === value.text) {
+      acc.stats[uri]++
+    }
+
+  } else {
+    acc.data[uri] = value
+    acc.stats[uri] = 1
+  }
+
+  return acc
+})
+
+const skipId = filter(kv => kv[0] !== 'id')
 
 const getMetadata = ({ metadata }) => metadata
 
 const getSelectedMetadata = memo(
-  getMetadata, ({ nav }) => (nav.items), pluck
+  getMetadata,
+  ({ nav }) => (nav.items),
+
+  (metadata, ids) =>
+    transduce(
+      ids,
+      compose(map(id => metadata[id]), cat, skipId),
+      collect,
+      { data: {}, stats: {} })
 )
 
 const getVisibleMetadata = memo(
---
+++ test/selectors/metadata_test.js
@@ -0,0 +1,31 @@
+'use strict'
+
+const { text } = __require('value')
+
+describe('selectors', () => {
+  const selectors = __require('selectors/metadata')
+
+  const metadata = {
+    1: { id: 1, title: text('foo'), type: text('test') },
+    2: { id: 2, title: text('bar'), type: text('test') }
+  }
+
+  const state = (items = []) => ({ nav: { items }, metadata })
+
+  describe('getSelectedMetadata', () => {
+    const { getSelectedMetadata } = selectors
+
+    it('returns combined metadata in bulk', () => {
+      expect(getSelectedMetadata(state([1, 2])))
+        .to.have.property('data')
+          .and.have.deep.property('title.text', 'foo')
+    })
+
+    it('collects stats', () => {
+      expect(getSelectedMetadata(state([1, 2])))
+        .to.have.property('stats')
+          .and.eql({ title: 1, type: 2 })
+    })
+  })
+
+})
