07aa3830d4f2ec4c783b0cb1870c22fd4c229d0c
Sylvester Keil <sylvester@keil.or.at>
2017-03-29 12:17:57 +0200

    Make project toolbar pure component

--- src/components/project/toolbar.js
+++ src/components/project/toolbar.js
@@ -1,60 +1,77 @@
 'use strict'
 
 const React = require('react')
+const { PureComponent, PropTypes } = React
+const { bool, func, number } = PropTypes
 const { Toolbar } = require('../toolbar')
 const { IconPlus, IconList, IconGrid } = require('../icons')
 const { Slider } = require('../slider')
 const { SearchField } = require('../search')
 const { IconButton } = require('../button')
-const { bool, func, number } = React.PropTypes
 
-const ProjectToolbar = ({
-  canCreateItems,
-  isDisabled,
-  isEmpty,
-  zoom,
-  maxZoom,
-  onZoomChange,
-  onItemCreate,
-  ...props
-}) => (
-  <Toolbar {...props}>
-    <div className="toolbar-left">
-      <div className="tool-group">
-        <Slider
-          value={zoom}
-          max={maxZoom}
-          isDisabled={isEmpty || isDisabled}
-          onChange={onZoomChange}
-          minIcon={<IconList/>}
-          maxIcon={<IconGrid/>}/>
-      </div>
-      <div className="tool-group">
-        {canCreateItems && !isDisabled &&
-          <IconButton icon={<IconPlus/>} onClick={onItemCreate}/>}
-      </div>
-    </div>
-    <div className="toolbar-right">
-      <SearchField isDisabled={isDisabled}/>
-    </div>
-  </Toolbar>
-)
 
-ProjectToolbar.propTypes = {
-  canCreateItems: bool,
-  isDraggable: bool,
-  isDisabled: bool,
-  isEmpty: bool,
-  maxZoom: number.isRequired,
-  zoom: number.isRequired,
-  onDoubleClick: func,
-  onItemCreate: func.isRequired,
-  onZoomChange: func.isRequired
+class ProjectToolbar extends PureComponent {
+
+  renderItemCreateButton() {
+    if (this.props.isDisabled) return
+    if (!this.props.canCreateItems) return
+
+    return (
+      <IconButton icon={<IconPlus/>} onClick={this.props.onItemCreate}/>
+    )
+  }
+
+  render() {
+    const {
+      isDisabled,
+      isDraggable,
+      isEmpty,
+      zoom,
+      maxZoom,
+      onDoubleClick,
+      onZoomChange,
+    } = this.props
+
+    return (
+      <Toolbar isDraggable={isDraggable} onDoubleClick={onDoubleClick}>
+        <div className="toolbar-left">
+          <div className="tool-group">
+            <Slider
+              value={zoom}
+              max={maxZoom}
+              isDisabled={isEmpty || isDisabled}
+              onChange={onZoomChange}
+              minIcon={<IconList/>}
+              maxIcon={<IconGrid/>}/>
+          </div>
+          <div className="tool-group">
+            {this.renderItemCreateButton()}
+          </div>
+        </div>
+        <div className="toolbar-right">
+          <SearchField isDisabled={isDisabled}/>
+        </div>
+      </Toolbar>
+    )
+  }
+
+  static propTypes = {
+    canCreateItems: bool,
+    isDraggable: bool,
+    isDisabled: bool,
+    isEmpty: bool,
+    maxZoom: number.isRequired,
+    zoom: number.isRequired,
+    onDoubleClick: func,
+    onItemCreate: func.isRequired,
+    onZoomChange: func.isRequired
+  }
+
+  static defaultProps = {
+    isDraggable: ARGS.frameless
+  }
 }
 
-ProjectToolbar.defaultProps = {
-  isDraggable: ARGS.frameless
-}
 
 module.exports = {
   ProjectToolbar
