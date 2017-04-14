9fe038975c06ab677a9fbde95be279c7b4e268b7
Sylvester Keil <sylvester@keil.or.at>
2017-04-14 23:18:55 +0200

    Add FTS examples

---
+++ db/test/search.sql
@@ -0,0 +1,29 @@
+PRAGMA foreign_keys = on;
+
+SELECT item_id AS item,
+    photos.id AS photo,
+    fts_notes.rowid AS note
+  FROM fts_notes JOIN photos USING (id)
+  WHERE fts_notes MATCH 'query';
+
+SELECT coalesce(items.id, item_id) AS item,
+    photos.id AS photo
+  FROM metadata
+    JOIN fts_metadata ON (value_id = fts_metadata.rowid)
+    LEFT OUTER JOIN items USING (id)
+    LEFT OUTER JOIN photos USING (id)
+  WHERE fts_metadata MATCH 'query';
+
+
+SELECT item_id AS id
+  FROM fts_notes JOIN photos USING (id)
+  WHERE fts_notes MATCH 'query'
+
+INTERSECT
+
+SELECT coalesce(items.id, item_id) AS id
+  FROM metadata
+    JOIN fts_metadata ON (value_id = fts_metadata.rowid)
+    LEFT OUTER JOIN items USING (id)
+    LEFT OUTER JOIN photos USING (id)
+  WHERE fts_metadata MATCH 'query';
