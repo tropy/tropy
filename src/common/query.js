f1b8783b0df53d2d9eb1b8a009c7aa30607bca95
Sylvester Keil <sylvester@keil.or.at>
2017-04-19 21:50:11 +0200

    Extend query builder

--- src/common/query.js
+++ src/common/query.js
@@ -1,6 +1,8 @@
 'use strict'
 
 const { entries } = Object
+const { isArray } = Array
+const { pluck, list } = require('./util')
 
 class Query {
   get hasParams() {
@@ -28,54 +30,133 @@ class Select extends Query {
   constructor(...args) {
     super()
 
+    this.isNegated = false
+    this.isDistinct = false
+
+    this.col = {}
     this.src = []
+    this.ord = []
+    this.con = []
+
     this.select(...args)
   }
 
-  distinct() {
+  get distinct() {
     this.isDistinct = true
     return this
   }
 
-  select(...args) {
-    for (let columns of args) {
-      if (typeof columns === 'string') {
-        this.col = { ...this.col, [columns]: columns }
+  get all() {
+    this.isDistinct = false
+    return this
+  }
+
+  get not() {
+    this.isNegated = true
+    return this
+  }
+
+  select(...columns) {
+    for (let cols of columns) {
+      if (typeof cols === 'string') {
+        this.col = { ...this.col, [cols]: cols }
         continue
       }
 
-      this.col = { ...this.col, ...columns }
+      this.col = { ...this.col, ...cols }
     }
 
     return this
   }
 
-  from() {
+  from(...sources) {
+    this.src = [...this.src, ...sources]
     return this
   }
 
-  where() {
+  where(conditions) {
+    try {
+      if (typeof conditions === 'string') {
+        this.con.push(conditions)
+
+      } else {
+        for (let lhs in conditions) {
+          let rhs = conditions[lhs]
+          let cmp
+
+          switch (true) {
+            case (rhs == null):
+              rhs = 'NULL'
+              cmp = this.isNegated ? 'IS NOT' : 'IS'
+              break
+
+            case (isArray(rhs)):
+              rhs = `(${rhs.join(', ')})`
+              cmp = this.isNegated ? 'NOT IN' : 'IN'
+              break
+
+            default:
+              this.params[`$${lhs}`] = rhs
+              rhs = `$${lhs}`
+              cmp = this.isNegated ? '!=' : '='
+          }
+
+          this.con.push(`${lhs} ${cmp} ${rhs}`)
+        }
+      }
+
+      return this
+
+    } finally {
+      this.isNegated = false
+    }
+  }
+
+  unless(...args) {
+    return this.not.where(...args)
+  }
+
+  order(by, dir) {
+    this.ord = [...this.ord, `${by}${dir ? ` ${dir}` : ''}`]
     return this
   }
 
-  order() {
+  with(head, body) {
+    this.cte = { head, body }
     return this
   }
 
   get query() {
-    const { SELECT, columns } = this
+    return pluck(this, ['WITH', 'SELECT', 'FROM', 'WHERE', 'ORDER']).join(' ')
+  }
 
-    return `${SELECT} ${columns}`
+  get WITH() {
+    return this.cte != null ?
+      `WITH ${this.cte.head} AS (${this.cte.body})` : undefined
   }
 
   get SELECT() {
-    return this.isDistinct ? 'SELECT DISTINCT' : 'SELECT'
+    return (
+      `SELECT${this.isDistinct ? ' DISTINCT' : ''} ${
+        entries(this.col)
+          .map(([n, a]) => n === a ? n : `${n} AS ${a}`)
+          .join(', ') || '*'
+      }`
+    )
   }
 
-  get columns() {
-    return entries(this.col)
-      .map(([name, alias]) => name === alias ? name : `${name} AS ${alias}`)
-      .join(', ')
+  get FROM() {
+    return `FROM ${this.src.join(', ')}`
+  }
+
+  get WHERE() {
+    return (this.con.length === 0) ?
+      undefined : `WHERE ${this.con.join(' AND ')}`
+  }
+
+  get ORDER() {
+    return (this.ord.length === 0) ?
+      undefined : `ORDER BY ${this.ord.join(', ')}`
   }
 }
 
--- test/common/query_test.js
+++ test/common/query_test.js
@@ -1,26 +1,57 @@
 'use strict'
 
-describe('Query Generator', () => {
+describe('Query Builder', () => {
   const query = __require('common/query')
 
   describe('Select', () => {
     const { select } = query
 
-    it('simple', () => {
-      expect(select('a').query).to.eql('SELECT a')
-    })
+    it('empty', () =>
+      expect(select().SELECT).to.eql('SELECT *'))
 
-    it('multiple', () => {
-      expect(select('a').select('b').query).to.eql('SELECT a, b')
-    })
+    it('simple', () =>
+      expect(select('a').SELECT).to.eql('SELECT a'))
+
+    it('multiple', () =>
+      expect(select('a').select('b').query).to.match(/^SELECT a, b/))
 
     it('alias', () => {
-      expect(select({ a: 'a', foo: 'b' }).query).to.eql('SELECT a, foo AS b')
-      expect(select('a', { b: 'c' }, 'd').query).to.eql('SELECT a, b AS c, d')
+      expect(select({ a: 'a', foo: 'b' }).query)
+        .to.match(/SELECT a, foo AS b/)
+      expect(select('a', { b: 'c' }, 'd').query)
+        .to.match(/SELECT a, b AS c, d/)
     })
 
-    it('distinct', () => {
-      expect(select('a').distinct().query).to.eql('SELECT DISTINCT a')
+    it('distinct', () =>
+      expect(select('a').distinct.query).to.match(/SELECT DISTINCT a/))
+
+    it('scoped', () => {
+      expect(select('t.a', { 't.b': 'c' }, 't.*').query)
+        .to.match(/^SELECT t\.a, t\.b AS c, t\.\*/)
     })
+
+    it('sources', () =>
+      expect(select('a').from('b').query).to.match(/SELECT a FROM b/))
+
+    it('order', () => {
+      expect(
+        select('a', 'b')
+          .from('b')
+          .order('b')
+          .order('a', 'DESC')
+          .query
+      ).to.match(/SELECT a, b FROM b ORDER BY b, a DESC/)
+    })
+
+    it('params', () => {
+      let q = select()
+
+      q.where({ a: null })
+      expect(select().where({ a: null }).WHERE).to.eql('WHERE a IS NULL')
+
+      q.not.where({ a: null })
+      expect(q.WHERE).to.eql('WHERE a IS NULL AND a IS NOT NULL')
+    })
+
   })
 })
