9fe29b837d049542d245ac603aa8e5ca709705dc
Sylvester Keil <sylvester@keil.or.at>
2017-04-21 15:42:54 +0200

    Handle Squirrel Events on Windows
    See #6

--- src/browser/main.js
+++ src/browser/main.js
@@ -6,76 +6,77 @@ if (process.env.TROPY_RUN_UNIT_TESTS === 'true') {
   require('electron-mocha')
 
 } else {
-  let READY = undefined
+  if (!require('./squirrel')()) {
+    let READY = undefined
 
-  const args = require('./args')
-  const opts = args.parse(process.argv.slice(1))
+    const args = require('./args')
+    const opts = args.parse(process.argv.slice(1))
 
-  process.env.NODE_ENV = opts.environment
-  global.ARGS = opts
+    process.env.NODE_ENV = opts.environment
+    global.ARGS = opts
 
-  require('./path')(opts.dir)
+    require('./path')(opts.dir)
 
-  const { app }  = require('electron')
-  const { all }  = require('bluebird')
-  const { once } = require('../common/util')
-  const { extname } = require('path')
-  const { info, verbose } =
-    require('../common/log')(app.getPath('userData'))
+    const { app }  = require('electron')
+    const { all }  = require('bluebird')
+    const { once } = require('../common/util')
+    const { extname } = require('path')
+    const { info, verbose } =
+      require('../common/log')(app.getPath('userData'))
 
-  if (process.env.NODE_ENV !== 'test') {
-    if (app.makeSingleInstance(() => tropy.open(...opts._))) {
-      verbose('other instance detected, exiting...')
-      app.exit(0)
-    }
-  }
-
-  if (opts.scale) {
-    app.commandLine.appendSwitch('force-device-scale-factor', opts.scale)
-  }
-
-  verbose(`started in ${opts.e} mode`)
-  verbose(`using ${app.getPath('userData')}`)
-
-  const tropy = new (require('./tropy'))()
-
-  tropy.listen()
-  tropy.restore()
-
-  if (process.platform === 'darwin') {
-    app.on('open-file', (event, file) => {
-      switch (extname(file)) {
-        case '.tpy':
-          event.preventDefault()
-          if (!READY) opts._ = [file]
-          else tropy.open(file)
-          break
-
-        case '.jpg':
-        case '.jpeg':
-          if (READY && tropy.win) {
-            event.preventDefault()
-            tropy.import([file])
-          }
-          break
+    if (process.env.NODE_ENV !== 'test') {
+      if (app.makeSingleInstance(() => tropy.open(...opts._))) {
+        verbose('other instance detected, exiting...')
+        app.exit(0)
       }
+    }
+
+    if (opts.scale) {
+      app.commandLine.appendSwitch('force-device-scale-factor', opts.scale)
+    }
+
+    verbose(`started in ${opts.e} mode`)
+    verbose(`using ${app.getPath('userData')}`)
+
+    const tropy = new (require('./tropy'))()
+
+    tropy.listen()
+    tropy.restore()
+
+    if (process.platform === 'darwin') {
+      app.on('open-file', (event, file) => {
+        switch (extname(file)) {
+          case '.tpy':
+            event.preventDefault()
+            if (!READY) opts._ = [file]
+            else tropy.open(file)
+            break
+
+          case '.jpg':
+          case '.jpeg':
+            if (READY && tropy.win) {
+              event.preventDefault()
+              tropy.import([file])
+            }
+            break
+        }
+      })
+    }
+
+    all([
+      once(app, 'ready'),
+      once(tropy, 'app:restored')
+
+    ]).then(() => {
+      READY = Date.now()
+      info('ready after %sms', READY - START)
+
+      tropy.open(...opts._)
     })
+
+    app
+      .on('quit', (_, code) => {
+        verbose(`quit with exit code ${code}`)
+      })
   }
-
-  all([
-    once(app, 'ready'),
-    once(tropy, 'app:restored')
-
-  ]).then(() => {
-    READY = Date.now()
-    info('ready after %sms', READY - START)
-
-    tropy.open(...opts._)
-  })
-
-  app
-    .on('quit', (_, code) => {
-      verbose(`quit with exit code ${code}`)
-    })
-
 }
---
+++ src/browser/squirrel.js
@@ -0,0 +1,48 @@
+'use strict'
+
+function handleSquirrelEvent() {
+  if (process.platform !== 'win32') return false
+  if (process.argv.length === 1) return false
+
+  const ChildProcess = require('child_process')
+  const { basename, resolve, join } = require('path')
+
+  const root = resolve(process.execPath, '..', '..')
+  const update = join(root, 'Update.exe')
+  const exe = basename(process.execPath)
+
+  function spawn(cmd, ...args) {
+    try {
+      return ChildProcess.spawn(cmd, args, { detached: true })
+
+    } catch (error) {
+      app.quit(1)
+    }
+  }
+
+  function delay(fn, by = 1000) {
+    return setTimeout(fn, by)
+  }
+
+  switch (process.argv[1]) {
+    case '--squirrel-install':
+    case '--squirrel-updated':
+      spawn(update, '--createShortcut', exe)
+      delay(app.quit)
+      break
+    case '--squirrel-uninstall':
+      spawn(update, '--removeShortcut', exe)
+      delay(app.quit)
+      break
+    case '--squirrel-obsolete':
+      app.quit()
+      break
+
+    default:
+      return false
+  }
+
+  return true
+}
+
+module.exports = handleSquirrelEvent
