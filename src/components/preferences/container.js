db5c9a17b7746a2045226c6e585518d3f408bc9e
Sylvester Keil <sylvester@keil.or.at>
2017-04-27 21:10:58 +0200

    Show TitleBar for frameless preference window

--- src/components/preferences/container.js
+++ src/components/preferences/container.js
@@ -3,13 +3,32 @@
 const React = require('react')
 const { PureComponent } = React
 const { connect } = require('react-redux')
+const { bool } = require('prop-types')
+const { TitleBar } = require('../titlebar')
 
 
 class PrefsContainer extends PureComponent {
+
+  renderTitleBar() {
+    return (!this.props.isFrameless) ? null : (
+      <TitleBar title="preferences.title"/>
+    )
+  }
+
   render() {
     return (
-      <div className="preferences"/>
+      <div className="preferences">
+        {this.renderTitleBar()}
+      </div>
     )
+  }
+
+  static propTypes = {
+    isFrameless: bool,
+  }
+
+  static defaultProps = {
+    isFrameless: ARGS.frameless
   }
 }
 
---
+++ src/components/titlebar.js
@@ -0,0 +1,24 @@
+'use strict'
+
+const React = require('react')
+const { PureComponent } = React
+const { FormattedMessage } = require('react-intl')
+const { string } = require('prop-types')
+
+class TitleBar extends PureComponent {
+  render() {
+    return (
+      <div className="titlebar">
+        <FormattedMessage id={this.props.title}/>
+      </div>
+    )
+  }
+
+  static propTypes = {
+    title: string.isRequired
+  }
+}
+
+module.exports = {
+  TitleBar
+}
