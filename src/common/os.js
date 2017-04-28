9f2774cc1f48268e281babdaa19c83092f2adcc7
Sylvester Keil <sylvester@keil.or.at>
2017-04-28 23:26:01 +0200

    Move .desktop builder to common

--- src/common/os.js
+++ src/common/os.js
@@ -2,9 +2,9 @@
 
 const { release } = require('os')
 const { platform } = process
+const { qualified } = require('./release')
 
 module.exports = {
-
   get EL_CAPITAN() {
     return platform === 'darwin' && release() > '15'
   },
@@ -21,8 +21,20 @@ module.exports = {
     return platform === 'win32'
   },
 
-
   meta: platform === 'darwin' ?
     (event) => event.metaKey :
-    (event) => event.ctrlKey
+    (event) => event.ctrlKey,
+
+  desktop(exec = qualified.name) {
+    return `#!/usr/bin/env xdg-open
+[Desktop Entry]
+Version=1.0
+Terminal=false
+Type=Application
+Name=${qualified.product}
+Exec=${exec} %f
+Icon=${qualified.name}
+Categories=GTK;Graphics;2DGraphics;Viewer;Development;
+MimeType=image/jpeg;application/vnd.tpy;`
+  }
 }
