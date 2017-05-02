24a0bbac906a777b6663f81f70c049ec4f4d8e98
Johannes Krtek <johannes.krtek@flachware.com>
2017-05-02 12:45:39 +0200

    Add preferences stylesheets for all platforms.

--- src/components/titlebar.js
+++ src/components/titlebar.js
@@ -8,7 +8,7 @@ const { string } = require('prop-types')
 class TitleBar extends PureComponent {
   render() {
     return (
-      <div className="titlebar">
+      <div className="titlebar draggable">
         <FormattedMessage id={this.props.title}/>
       </div>
     )
