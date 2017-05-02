06cb0ce26604a6da009ebe5c28c613db254fd3a6
Johannes Krtek <johannes.krtek@flachware.com>
2017-05-02 21:33:02 +0200

    Add property list to template editor.

--- src/components/template/editor.js
+++ src/components/template/editor.js
@@ -7,7 +7,26 @@ const { PureComponent } = React
 class TemplateEditor extends PureComponent {
   render() {
     return (
-      <div className="template-editor"/>
+      <div className="template editor">
+        <ul className="properties">
+          <li className="property">
+            <div className="form-group">
+              <label className="col-3">Property</label>
+              <div className="col-9">
+                <select className="form-control">
+                  <option>http://purl.org/dc/elements/1.1/title</option>
+                </select>
+              </div>
+            </div>
+            <div className="form-group">
+              <label className="col-3">Label</label>
+              <div className="col-9">
+                <input className="form-control" type="text" value="Title"/>
+              </div>
+            </div>
+          </li>
+        </ul>
+      </div>
     )
   }
 }
