a6a0fb36bb37e0d11433d1ef4275283195453f18
Johannes Krtek <johannes.krtek@flachware.com>
2017-05-03 12:49:16 +0200

    Tweak alignment.

--- src/components/template/editor.js
+++ src/components/template/editor.js
@@ -23,38 +23,42 @@ class TemplateEditor extends PureComponent {
   render() {
     return (
       <div className="template editor form-horizontal">
-        <div className="form-group select-template">
-          <label className="control-label col-3" htmlFor="">Template</label>
-          <div className="col-9 flex-row center">
-            <TemplateSelect
-              templates={this.props.templates}
-              selected={get(this.props.template, ['id'])}
-              onChange={this.onTemplateChange}/>
-            <div className="btn-group">
-              <IconButton icon={<IconNew/>}/>
-              <IconButton icon={<IconCopy/>}/>
-              <IconButton icon={<IconTrash/>}/>
-              <IconButton icon={<IconImport/>}/>
-              <IconButton icon={<IconExport/>}/>
+        <header className="template-header">
+          <div className="form-group select-template">
+            <label className="control-label col-3" htmlFor="">Template</label>
+            <div className="col-9 flex-row center">
+              <TemplateSelect
+                templates={this.props.templates}
+                selected={get(this.props.template, ['id'])}
+                onChange={this.onTemplateChange}/>
+              <div className="btn-group">
+                <IconButton icon={<IconNew/>}/>
+                <IconButton icon={<IconCopy/>}/>
+                <IconButton icon={<IconTrash/>}/>
+                <IconButton icon={<IconImport/>}/>
+                <IconButton icon={<IconExport/>}/>
+              </div>
             </div>
           </div>
-        </div>
-        <div className="form-group compact">
-          <label className="control-label col-3" htmlFor="">Name</label>
-          <div className="col-9">
-            <input className="form-control" type="text" value="Core Item"/>
+          <div className="form-group compact">
+            <label className="control-label col-3" htmlFor="">Name</label>
+            <div className="col-9">
+              <input className="form-control" type="text" value="Core Item"/>
+            </div>
           </div>
-        </div>
-        <div className="form-group">
-          <label className="control-label col-3" htmlFor="">ID</label>
-          <div className="col-9">
-            <input className="form-control" type="text"
-              value="https://schema.tropy.org/core-item"/>
+          <div className="form-group">
+            <label className="control-label col-3" htmlFor="">ID</label>
+            <div className="col-9">
+              <input className="form-control" type="text"
+                value="https://schema.tropy.org/core-item"/>
+            </div>
           </div>
-        </div>
-        <div className="form-group align-right">
-          <button className="btn btn-primary min-width">Save</button>
-        </div>
+          <div className="form-group">
+            <div className="col-12 text-right">
+              <button className="btn btn-primary min-width">Save</button>
+            </div>
+          </div>
+        </header>
 
         <ul className="properties">
           <li className="property">
