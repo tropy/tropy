584359f587420755f201e50199d8e7def5376c66
Sylvester Keil <sylvester@keil.or.at>
2017-05-21 21:09:39 +0200

    Fix photo template name

--- src/selectors/templates.js
+++ src/selectors/templates.js
@@ -6,7 +6,7 @@ const { entries, values } = Object
 const { DC, TR, S } = require('../constants/properties')
 
 const CORE = 'https://tropy.org/schema/v1/templates/item'
-const PHOTO = 'https://tropy.org/schmea/v1/templates/photo'
+const PHOTO = 'https://tropy.org/schema/v1/templates/photo'
 
 const CORNELL = 'https://schema.tropy.org/v1/templates/cornell-obama'
 const PINKERTON = 'https://schema.tropy.org/v1/templates/pinkerton'
