d78ec942591bdab2af92b4aa4ff6b1320960a74e
Sylvester Keil <sylvester@keil.or.at>
2017-05-22 22:31:49 +0200

    Improve script utilities

--- scripts/util.js
+++ scripts/util.js
@@ -3,7 +3,14 @@
 const colors = require('colors/safe')
 const ms = require('ms')
 const pad = require('string.prototype.padstart')
-const COLOR = { warn: 'yellow', error: 'red', default: 'blue' }
+
+const COLOR = {
+  warn: 'yellow',
+  error: 'red',
+  red: 'red',
+  green: 'green',
+  log: 'blue'
+}
 
 
 function colorize(level, text) {
@@ -27,40 +34,61 @@ function time({ value }, padding = 8) {
   return colors.gray(pad(`+${ms(value)}`, padding, ' '))
 }
 
+function format(msg, level, pre) {
+  return (pre) ?
+    `${time(seq.next())} ${colorize(level, pre)} ${msg}` :
+    `  ${msg}`
+}
+
+function log(...args) {
+  print('log', ...args)
+}
+
+function error(...args) {
+  print('error', ...args)
+}
+
+function warn(...args) {
+  print('warn', ...args)
+}
+
+function check(predicate, pre, msg = 'assertion failed', ...args) {
+  if (!predicate) bail(pre, msg, ...args)
+}
+
+function bail(...args) {
+  error(...args)
+  exit(1)
+}
+
+function print(level = 'log', pre, msg, ...args) {
+  console[level](format(msg, level, pre), ...args)
+}
+
+function red(pre, msg, ...args) {
+  console.log(format(msg, 'red', pre), ...args)
+}
+
+function green(pre, msg, ...args) {
+  console.log(format(msg, 'green', pre), ...args)
+}
+
+
+
 module.exports = function (name) {
-  let fmt = (msg, level = 'default') =>
-    `${time(seq.next())} ${colorize(level, name)} ${msg}`
-
-  let util = {
-    check(predicate, msg = 'assertion failed', ...args) {
-      if (!predicate) util.bail(msg, ...args)
-    },
-
-    print(...args) {
-      console.log(...args)
+  return {
+    check(predicate, ...args) {
+      check(predicate, name, ...args)
     },
 
     rules(target) {
-      for (let rule in target) util.print(`  -> ${rule}`)
+      for (let rule in target) log(false, `-> ${rule}`)
     },
 
-    say(msg, ...args) {
-      console.log(fmt(msg), ...args)
-    },
-
-    warn(msg, ...args) {
-      console.warn(fmt(msg, 'warn'), ...args)
-    },
-
-    error(msg, ...args) {
-      console.error(fmt(msg, 'error'), ...args)
-    },
-
-    bail(...args) {
-      util.error(...args)
-      exit(1)
-    }
+    say(...args) { log(name, ...args) },
+    warn(...args) { warn(name, ...args) },
+    error(...args) { error(name, ...args) },
+    red(...args) { red(name, ...args) },
+    green(...args) { green(name, ...args) }
   }
-
-  return util
 }
