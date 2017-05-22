6e493e2997cc023c498af073003cd49403bbdbb1
Sylvester Keil <sylvester@keil.or.at>
2017-05-22 23:25:04 +0200

    Update watch scripts

--- scripts/watch.js
+++ scripts/watch.js
@@ -3,32 +3,23 @@
 require('shelljs/make')
 
 const chokidar = require('chokidar')
-const colors = require('colors/safe')
 const { relative, extname, basename } = require('path')
 
 const make = require('./make')
-const log = require('./log')
+const sass = require('./sass')
+const babel = require('./babel')
+const { error, green, red, say } = require('./util')('watch')
 const cwd = process.cwd()
 
-const COLOR = { change: 'blue', unlink: 'red', add: 'green' }
-
-function colorize(event, text) {
-  return colors[COLOR[event] || 'white'](text)
-}
-
-function shorten(file) {
-  return relative(cwd, file)
-}
-
-function mocha(spec, tag) {
+function mocha(spec) {
   const args = (/browser|common/).test(spec) ?
     [spec] : ['--renderer', spec]
 
   make.mocha(args, true, (code, stdout) => {
     if (code === 0) {
-      log.info(colors.green(spec), { tag })
+      green(spec)
     } else {
-      log.error(colors.red(spec), { tag })
+      error(spec)
       process.stderr.write(stdout)
     }
   })
@@ -41,42 +32,45 @@ target.all = () => {
 }
 
 target.src = () => {
-  const tag = 'watch:src'
-
   process.env.TROPY_RUN_UNIT_TESTS = 'true'
 
   chokidar
     .watch('src/**/*.{js,jsx,scss,sass}', {
       persistent: true
     })
-
     .on('all', (event, file) => {
-      file = shorten(file)
+      file = relative(cwd, file)
 
-      if (event === 'error') {
-        return log.error(file, { tag })
-      }
-
-      log.info(colorize(event, file), { tag })
-
-      if (event === 'unlink') {
-        return rm(file.replace(/^src/, 'lib'))
+      switch (event) {
+        case 'change':
+          say(file)
+          break
+        case 'add':
+          green(file)
+          break
+        case 'unlink':
+          red(file)
+          rm(file.replace(/^src/, 'lib'))
+          return
+        case 'error':
+          error(file)
+          return
       }
 
       if ((/^\.jsx?$/).test(extname(file))) {
-        make['compile:js'](file)
+        babel.transform(file)
 
         if (event === 'change') {
           const spec = file
             .replace(/^src/, 'test')
             .replace(/\.jsx?$/, '_test.js')
 
-          if (test('-f', spec)) mocha(spec, tag)
+          if (test('-f', spec)) mocha(spec)
         }
 
       } else {
-
-        make['compile:css']((basename(file)[0] === '_') ? undefined : file)
+        if ((basename(file)[0] === '_')) sass.compile()
+        else sass.compile(file)
       }
     })
 }
@@ -87,6 +81,6 @@ target.test = () => {
       persistent: true
     })
     .on('change', (file) => {
-      mocha(shorten(file), 'watch:test')
+      mocha(relative(cwd, file))
     })
 }
