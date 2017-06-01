f3807dc81bda92579f7f4a4a8f3279e9f5628bc7
Sylvester Keil <sylvester@keil.or.at>
2017-06-01 10:16:17 +0200

    Disable short option group argument parsing

--- src/browser/main.js
+++ src/browser/main.js
@@ -11,9 +11,6 @@ if (process.env.TROPY_RUN_UNIT_TESTS === 'true') {
   const args = require('./args')
   const opts = args.parse(process.argv.slice(1))
 
-  // See https://github.com/yargs/yargs/issues/878
-  if (opts._.length > 0) opts._ = opts._.filter(f => f !== true)
-
   process.env.NODE_ENV = opts.environment
   global.ARGS = opts
 
