f569bb9e41a3f18f24f9bc8895c757885d0b9c10
Sylvester Keil <sylvester@keil.or.at>
2017-06-04 13:59:15 +0200

    Use reducer utils

--- src/reducers/photos.js
+++ src/reducers/photos.js
@@ -1,54 +1,35 @@
 'use strict'
 
 const { PHOTO, PROJECT } = require('../constants')
-const { into, map } = require('transducers.js')
-const { nested } = require('./util')
-
-const init = {}
+const { bulk, insert, load, nested, update } = require('./util')
 
 module.exports = {
   // eslint-disable-next-line complexity
-  photos(state = init, { type, payload, error, meta }) {
+  photos(state = {}, { type, payload, error, meta }) {
     switch (type) {
       case PROJECT.OPEN:
-        return { ...init }
+        return {}
 
       case PHOTO.LOAD:
-        return (meta.done && !error) ? { ...state, ...payload } : state
+        return load(state, payload, meta, error)
 
       case PHOTO.CREATE:
-        return (meta.done && !error) ?
-          { ...state, [payload.id]: payload } :
-          state
+        return (error || !meta.done) ?
+          state :
+          insert(state, payload)
 
       case PHOTO.INSERT:
-        return { ...state, [payload.id]: payload }
-
+        return insert(state, payload, meta)
       case PHOTO.UPDATE:
-        return {
-          ...state,
-          [payload.id]: {
-            ...state[payload.id],
-            ...payload
-          }
-        }
+        return update(state, payload, meta)
 
       case PHOTO.NOTE.ADD:
         return nested.add('notes', state, payload, meta)
       case PHOTO.NOTE.REMOVE:
         return nested.remove('notes', state, payload, meta)
 
-
-      case PHOTO.BULK.UPDATE: {
-        const [ids, data] = payload
-
-        return into(
-          { ...state },
-          map(id => ({ [id]: { ...state[id], ...data } })),
-          ids
-        )
-      }
-
+      case PHOTO.BULK.UPDATE:
+        return bulk.update(state, payload, meta)
 
       default:
         return state
