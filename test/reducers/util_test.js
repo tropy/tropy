e5ca8a5e99c5e4efd654413cdae1de660885a2e5
Sylvester Keil <sylvester@keil.or.at>
2017-06-12 10:13:26 +0200

    Update chai.js

--- test/reducers/util_test.js
+++ test/reducers/util_test.js
@@ -16,9 +16,9 @@ describe('Reducer Helpers', () => {
         let s2 = nested.add('tags', s1, { id: 1, tags: [2] })
         let s3 = nested.add('tags', s2, { id: 1, tags: [3, 4] })
 
-        expect(s1).to.have.deep.property('1.tags').eql([1])
-        expect(s2).to.have.deep.property('1.tags').eql([1, 2])
-        expect(s3).to.have.deep.property('1.tags').eql([1, 2, 3, 4])
+        expect(s1).to.have.nested.property('1.tags').eql([1])
+        expect(s2).to.have.nested.property('1.tags').eql([1, 2])
+        expect(s3).to.have.nested.property('1.tags').eql([1, 2, 3, 4])
       })
 
       it('supports multiple id calls', () => {
@@ -26,20 +26,20 @@ describe('Reducer Helpers', () => {
         let s2 = nested.add('tags', s1, { id: [1, 2], tags: [2] })
         let s3 = nested.add('tags', s2, { id: [1, 2], tags: [3, 4] })
 
-        expect(s1).to.have.deep.property('1.tags').eql([1])
-        expect(s2).to.have.deep.property('1.tags').eql([1, 2])
-        expect(s3).to.have.deep.property('1.tags').eql([1, 2, 3, 4])
-        expect(s1).to.have.deep.property('2.tags').eql([1])
-        expect(s2).to.have.deep.property('2.tags').eql([1, 2])
-        expect(s3).to.have.deep.property('2.tags').eql([1, 2, 3, 4])
+        expect(s1).to.have.nested.property('1.tags').eql([1])
+        expect(s2).to.have.nested.property('1.tags').eql([1, 2])
+        expect(s3).to.have.nested.property('1.tags').eql([1, 2, 3, 4])
+        expect(s1).to.have.nested.property('2.tags').eql([1])
+        expect(s2).to.have.nested.property('2.tags').eql([1, 2])
+        expect(s3).to.have.nested.property('2.tags').eql([1, 2, 3, 4])
       })
 
       it('supports meta.idx', () => {
         let s1 = nested.add('tags', state, { id: 1, tags: [1, 2, 3] })
         let s2 = nested.add('tags', s1, { id: 1, tags: [4, 5] }, { idx: 1 })
 
-        expect(s1).to.have.deep.property('1.tags').eql([1, 2, 3])
-        expect(s2).to.have.deep.property('1.tags').eql([1, 4, 5, 2, 3])
+        expect(s1).to.have.nested.property('1.tags').eql([1, 2, 3])
+        expect(s2).to.have.nested.property('1.tags').eql([1, 4, 5, 2, 3])
       })
     })
 
@@ -53,15 +53,15 @@ describe('Reducer Helpers', () => {
         let s1 = nested.remove('tags', state, { id: 2, tags: [2] })
         let s2 = nested.remove('tags', s1, { id: 2, tags: [1, 3] })
 
-        expect(s1).to.have.deep.property('2.tags').eql([1, 3])
-        expect(s2).to.have.deep.property('2.tags').eql([])
+        expect(s1).to.have.nested.property('2.tags').eql([1, 3])
+        expect(s2).to.have.nested.property('2.tags').eql([])
       })
 
       it('supports multiple id calls', () => {
         let s1 = nested.remove('tags', state, { id: [1, 2], tags: [1, 2] })
 
-        expect(s1).to.have.deep.property('1.tags').eql([])
-        expect(s1).to.have.deep.property('2.tags').eql([3])
+        expect(s1).to.have.nested.property('1.tags').eql([])
+        expect(s1).to.have.nested.property('2.tags').eql([3])
       })
     })
   })
