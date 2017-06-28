0e419fc0ffd22f58a0e85c31e7ce1253112af182
Sylvester Keil <sylvester@keil.or.at>
2017-06-28 11:12:48 +0200

    Add creator and description fields to templates

--- db/migrate/ontology/1705231144.templates.sql
+++ db/migrate/ontology/1705231144.templates.sql
@@ -3,12 +3,15 @@ CREATE TABLE templates (
   template_id    TEXT     NOT NULL PRIMARY KEY,
   template_type  TEXT     NOT NULL DEFAULT 'item',
   name           TEXT     NOT NULL,
+  description    TEXT,
+  creator        TEXT,
   protected      BOOLEAN  NOT NULL DEFAULT FALSE,
   created        NUMERIC  NOT NULL DEFAULT CURRENT_TIMESTAMP,
   modified       NUMERIC  NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
   CHECK (template_id != ''),
   CHECK (template_type IN ('item', 'photo', 'selection'))
+  CHECK (name != '')
 );
 
 
--- db/schema/ontology.sql
+++ db/schema/ontology.sql
@@ -68,12 +68,15 @@ CREATE TABLE templates (
   template_id    TEXT     NOT NULL PRIMARY KEY,
   template_type  TEXT     NOT NULL DEFAULT 'item',
   name           TEXT     NOT NULL,
+  description    TEXT,
+  creator        TEXT,
   protected      BOOLEAN  NOT NULL DEFAULT FALSE,
   created        NUMERIC  NOT NULL DEFAULT CURRENT_TIMESTAMP,
   modified       NUMERIC  NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
   CHECK (template_id != ''),
   CHECK (template_type IN ('item', 'photo', 'selection'))
+  CHECK (name != '')
 );
 CREATE TABLE domains (
   template_id   TEXT      NOT NULL REFERENCES templates ON DELETE CASCADE,
