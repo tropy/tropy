ec5c80acee5677ba66b4889f9cca0382716bbed8
Sylvester Keil <sylvester@keil.or.at>
2017-07-06 18:20:32 +0200

    Fix type URIs

--- src/constants/type.js
+++ src/constants/type.js
@@ -4,6 +4,6 @@ module.exports = {
   DATE: 'https://tropy.org/v1/tropy#date',
   TEXT: 'http://www.w3.org/2001/XMLSchema#string',
   ITEM: 'https://tropy.org/v1/tropy#Item',
-  PHOTO: 'https://tropy.org/v1/tropy#Item',
-  SELECTION: 'https://tropy.org/v1/tropy#Item'
+  PHOTO: 'https://tropy.org/v1/tropy#Photo',
+  SELECTION: 'https://tropy.org/v1/tropy#Selection'
 }
