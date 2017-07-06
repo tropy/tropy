dbeb6831c66dcdc7073dddbc9b25fe87df5ef715
Sylvester Keil <sylvester@keil.or.at>
2017-07-06 10:46:10 +0200

    Add template constraints

--- db/migrate/ontology/1705231144.templates.sql
+++ db/migrate/ontology/1705231144.templates.sql
@@ -9,24 +9,36 @@ CREATE TABLE templates (
   modified       NUMERIC  NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
   CHECK (template_id != ''),
-  CHECK (template_type IN ('item', 'photo', 'selection'))
+  CHECK (template_type IN (
+    'https://tropy.org/v1/tropy#Item',
+    'https://tropy.org/v1/tropy#Photo',
+    'https://tropy.org/v1/tropy#Selection'
+  ))
   CHECK (name != '')
 );
 
 
 CREATE TABLE domains (
+  domain_id     INTEGER   PRIMARY KEY,
   template_id   TEXT      NOT NULL REFERENCES templates ON DELETE CASCADE,
-  class_id      TEXT      NOT NULL REFERENCES classes,
-  position      INTEGER
+  class_id      TEXT      NOT NULL,
+  position      INTEGER,
+
+  CHECK (class_id != ''),
+  UNIQUE (template_id, class_id)
 );
 
 CREATE TABLE fields (
   field_id      INTEGER   PRIMARY KEY,
   template_id   TEXT      NOT NULL REFERENCES templates ON DELETE CASCADE,
-  property_id   TEXT      NOT NULL REFERENCES properties,
-  datatype_id   TEXT      NOT NULL REFERENCES datatypes,
-  required      BOOLEAN   NOT NULL DEFAULT false,
+  property_id   TEXT      NOT NULL,
+  datatype_id   TEXT      NOT NULL,
+  required      BOOLEAN   NOT NULL DEFAULT FALSE,
   hint          TEXT,
   constant      TEXT,
-  position      INTEGER
+  position      INTEGER,
+
+  CHECK (datatype_id != ''),
+  CHECK (property_id != ''),
+  UNIQUE (template_id, property_id)
 );
