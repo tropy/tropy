2219797d9207e220d45a31f31216d86444a37489
Sylvester Keil <sylvester@keil.or.at>
2017-07-29 17:03:47 +0200

    Esper: naively animate rotation

--- src/components/esper/stage.js
+++ src/components/esper/stage.js
@@ -7,6 +7,7 @@ const { append, bounds, on, off } = require('../../dom')
 const PIXI = require('pixi.js')
 const { Sprite } = PIXI
 const { TextureCache, skipHello } = PIXI.utils
+const { PI, abs, min } = Math
 
 
 class EsperStage extends PureComponent {
@@ -52,6 +53,9 @@ class EsperStage extends PureComponent {
       this.reset(props)
     }
 
+    this.zoom(props)
+    this.rotate(props)
+
     if (props.isVisible) {
       this.pixi.start()
     } else {
@@ -65,8 +69,9 @@ class EsperStage extends PureComponent {
 
     if (props.src != null) {
       this.image = new Sprite()
-      this.load(props.src, this.image)
+      this.image.anchor.set(0.5)
 
+      this.load(props.src, this.image)
       this.pixi.stage.addChildAt(this.image, 0)
     }
 
@@ -89,25 +94,60 @@ class EsperStage extends PureComponent {
   resize = () => {
     const { width, height } = bounds(this.container)
     this.pixi.renderer.resize(width, height)
+    this.zoom()
   }
 
-  scale(width = this.pixi.view.width) {
-    return this.props.width > 0 ? (width / this.props.width) : 1
+  zoom(props = this.props) {
+    if (props.scale != null) {
+      return this.scale = props.scale
+    }
+
+    if (props.width === 0) {
+      return this.scale = 1
+    }
+
+    return this.scale = this.pixi.screen.width / props.width
   }
 
-  update = () => {
-    if (this.image != null) {
-      this.image.anchor.x = 0.5
-      this.image.anchor.y = 0.5
-      this.image.x = this.pixi.view.width / 2
-      this.image.y = this.pixi.view.height / 2
-      this.image.scale.x = this.scale()
-      this.image.scale.y = this.image.scale.x
+  rotate(props = this.props.angle) {
+    return this.rotation = (props.angle / 180) * PI
+  }
 
-      this.image.rotation = (this.props.angle / 180) * Math.PI
+  update = (delta) => {
+    if (this.isDirty()) {
+      const { image, rotation, scale } = this
+
+      image.x = this.pixi.screen.width / 2
+      image.y = this.pixi.screen.height / 2
+
+      image.scale.set(scale)
+
+
+      const diff = abs(image.rotation - rotation)
+
+      if (diff < 0.25) {
+        image.rotation = rotation
+      } else {
+        const step = min(diff, 0.25 * delta)
+
+        let next = image.rotation - step
+        if (next < 0) next = (2 * PI) - next
+
+        image.rotation = next
+      }
     }
   }
 
+
+  isDirty() {
+    const { image, scale, rotation } = this
+
+    if (image == null) return false
+    if (image.scale.x !== scale) return true
+    if (image.rotation !== rotation) return true
+
+    return false
+  }
 
   get isLoading() {
     return this.pixi.loader.loading
