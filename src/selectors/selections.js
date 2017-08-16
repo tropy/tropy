7e122d3625574c4cb7de8371706f25f3c3408323
Sylvester Keil <sylvester@keil.or.at>
2017-08-16 14:50:18 +0200

    Add getPhotoSelections

--- src/selectors/selections.js
+++ src/selectors/selections.js
@@ -1,9 +1,14 @@
 'use strict'
 
 const { createSelector: memo } = require('reselect')
+const { getSelectedPhoto } = require('./photo')
+const { pluck } = require('../common/util')
+
+const NOTHING = []
+const getSelections = ({ selections }) => selections
 
 const getActiveSelection = memo(
-  ({ selections }) => selections,
+  getSelections,
   ({ nav }) => nav.selection,
   (selections, id) => (id != null) ? selections[id] : null
 )
@@ -14,7 +19,16 @@ const getActiveSelectionData = memo(
   (metadata, id) => (id != null) ? metadata[id] : null
 )
 
+const getPhotoSelections = memo(
+  getSelectedPhoto,
+  getSelections,
+  (photo, selections) => (photo != null) ?
+    pluck(selections, photo.selections) : NOTHING
+
+)
+
 module.exports = {
   getActiveSelection,
-  getActiveSelectionData
+  getActiveSelectionData,
+  getPhotoSelections
 }
