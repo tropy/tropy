97e6917d94b620827889fa691cec4b33bc3c5da7
Sylvester Keil <sylvester@keil.or.at>
2017-09-03 17:28:18 +0200

    Sort expanded photos

--- src/selectors/ui.js
+++ src/selectors/ui.js
@@ -5,6 +5,8 @@ const { getVisiblePhotos } = require('./photos')
 const { seq, compose, map, keep } = require('transducers.js')
 const BLANK = {}
 
+const rev = (a, b) => a < b ? 1 : b < a ? -1 : 0
+
 const getActiveImageProps = memo(
   ({ ui }) => ui.image,
   ({ nav }) => nav.photo,
@@ -14,12 +16,20 @@ const getActiveImageProps = memo(
 
 const getExpandedPhotos = memo(
   ({ ui }) => ui.expand,
+  ({ ui }) => ui.panel.zoom > 0,
   getVisiblePhotos,
-  (expand, photos) =>
-    seq(photos, compose(
+  (expand, isGrid, photos) => {
+    const expanded = seq(photos, compose(
       map(photo => expand[photo.id] > 0 ? photo.id : null),
       keep()
     ))
+
+    if (isGrid) {
+      expanded.sort((a, b) => rev(expand[a], expand[b]))
+    }
+
+    return expanded
+  }
 )
 
 module.exports = {
