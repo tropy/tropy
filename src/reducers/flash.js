4428624bd20b222af2f522630252abed9e3b42e2
Sylvester Keil <sylvester@keil.or.at>
2017-10-18 18:26:55 +0200

    Add flash reducer and update action

---
+++ src/reducers/flash.js
@@ -0,0 +1,15 @@
+
+'use strict'
+
+const { FLASH } = require('../constants')
+
+module.exports = {
+  flash(state = {}, { type, payload }) {
+    switch (type) {
+      case FLASH.READY:
+        return { ...payload }
+      default:
+        state
+    }
+  }
+}
