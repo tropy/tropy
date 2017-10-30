6a5003c7f8dbf7e6b2b060b19535ac216e71472b
Kirill Stytsenko <kirill@styts.com>
2017-10-30 20:35:30 +0100

    Refactor

--- src/export/vocab.js
+++ src/export/vocab.js
@@ -5,12 +5,33 @@ const { DC, RDF, RDFS, OWL, VANN } = require('../constants')
 const { createLiteral: l } = N3.Util
 
 const prefixes = {
-  // TODO: could these be extracted from ontology.db?
+  // prefixes could be overwritten by user
   owl: 'http://www.w3.org/2002/07/owl#',
   vann: 'http://purl.org/vocab/vann/',
   rdf: 'http://www.w3.org/1999/02/22-rdf-syntax-ns#',
   rdfs: 'http://www.w3.org/2000/01/rdf-schema#',
   dc: 'http://purl.org/dc/elements/1.1/'
+}
+
+function addTriples(writer, property, collection, type) {
+  for (let id of property || []) {
+    const x = collection[id]
+    if (x.id) {
+      writer.addTriple(x.id, RDF.type, type)
+    }
+    if (x.vocabulary) {
+      writer.addTriple(x.id, RDFS.isDefinedBy, x.vocabulary)
+    }
+    if (x.comment) {
+      writer.addTriple(x.id, RDFS.comment, l(x.comment, 'en'))
+    }
+    if (x.label) {
+      writer.addTriple(x.id, RDFS.label, l(x.label, 'en'))
+    }
+    if (x.description) {
+      writer.addTriple(x.id, DC.description, l(x.description))
+    }
+  }
 }
 
 function toN3(vocab, classes, props, datatypes) {
@@ -30,35 +51,9 @@ function toN3(vocab, classes, props, datatypes) {
     writer.addTriple(vocab.id, DC.title, l(vocab.title, 'en'))
     writer.addTriple(vocab.id, DC.description, l(vocab.description))
 
-    // classes
-    for (let classId of vocab.classes || []) {
-      const c = classes[classId]
-      c.id && writer.addTriple(c.id, RDF.type, RDFS.Class)
-      c.vocabulary && writer.addTriple(c.id, RDFS.isDefinedBy, c.vocabulary)
-      c.comment && writer.addTriple(c.id, RDFS.comment, l(c.comment, 'en'))
-      c.label && writer.addTriple(c.id, RDFS.label, l(c.label, 'en'))
-      c.description && writer.addTriple(c.id, DC.description, l(c.description))
-    }
-
-    // datatypes
-    for (let typeId of vocab.datatypes || []) {
-      const d = datatypes[typeId]
-      d.id && writer.addTriple(d.id, RDF.type, RDFS.Datatype)
-      d.vocabulary && writer.addTriple(d.id, RDFS.isDefinedBy, d.vocabulary)
-      d.comment && writer.addTriple(d.id, RDFS.comment, l(d.comment, 'en'))
-      d.label && writer.addTriple(d.id, RDFS.label, l(d.label, 'en'))
-      d.description && writer.addTriple(d.id, DC.description, l(d.description))
-    }
-
-    // properties
-    for (let propId of vocab.properties || []) {
-      const p = props[propId]
-      p.id && writer.addTriple(p.id, RDF.type, RDF.Property)
-      p.vocabulary && writer.addTriple(p.id, RDFS.isDefinedBy, p.vocabulary)
-      p.comment && writer.addTriple(p.id, RDFS.comment, l(p.comment, 'en'))
-      p.label && writer.addTriple(p.id, RDFS.label, l(p.label, 'en'))
-      p.description && writer.addTriple(p.id, DC.description, l(p.description))
-    }
+    addTriples(writer, vocab.classes, classes, RDFS.Class)
+    addTriples(writer, vocab.datatypes, datatypes, RDFS.Datatype)
+    addTriples(writer, vocab.properties, props, RDF.Property)
 
     // settle the promise
     writer.end(function (error, result) {
