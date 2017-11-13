322f89f16d8b08836afdee8a19b92e44ef5c1e3a
Sylvester Keil <sylvester@keil.or.at>
2017-11-13 21:19:39 +0100

    Remove AutoResizer custom className

--- src/components/auto-resizer.js
+++ src/components/auto-resizer.js
@@ -1,19 +1,16 @@
 'use strict'
 
 const React = require('react')
-const cx = require('classnames')
 const { element, number, oneOfType, string } = require('prop-types')
 
-
-const AutoResizer = ({ children, className, content }) => (
-  <div className={cx(className, 'auto-resizer')}>
+const AutoResizer = ({ children, content }) => (
+  <div className="auto-resizer">
     <div className="content">{content}</div>
     {children}
   </div>
 )
 
 AutoResizer.propTypes = {
-  className: string,
   children: element.isRequired,
   content: oneOfType([string, number])
 }
