56e5f736e0ab729a8441dd88a44c0be623e355ff
Sylvester Keil <sylvester@keil.or.at>
2017-11-13 16:03:26 +0100

    Add className prop to AutoResizer
    Turn AutoResizer into functional component

--- src/components/auto-resizer.js
+++ src/components/auto-resizer.js
@@ -1,26 +1,22 @@
 'use strict'
 
 const React = require('react')
-const { PureComponent } = React
+const cx = require('classnames')
 const { element, number, oneOfType, string } = require('prop-types')
 
 
-class AutoResizer extends PureComponent {
-  render() {
-    return (
-      <div className="auto-resizer">
-        <div className="content">{this.props.content}</div>
-        {this.props.children}
-      </div>
-    )
-  }
+const AutoResizer = ({ children, className, content }) => (
+  <div className={cx(className, 'auto-resizer')}>
+    <div className="content">{content}</div>
+    {children}
+  </div>
+)
 
-  static propTypes = {
-    children: element.isRequired,
-    content: oneOfType([string, number])
-  }
+AutoResizer.propTypes = {
+  className: string,
+  children: element.isRequired,
+  content: oneOfType([string, number])
 }
-
 
 module.exports = {
   AutoResizer
