fd70d4ef4da334b1c7a877dfb499fe55747d4d6f
Kirill Stytsenko <kirstyts@gmail.com>
2017-11-21 20:00:09 +0100

    Add a failing test

--- test/editor/commands_test.js
+++ test/editor/commands_test.js
@@ -1,10 +1,20 @@
 'use strict'
 
-const { state } = require('../fixtures/editor')
-const content = state.doc.toJSON().content
-const p = content[1]
+const { TextSelection } = require('prosemirror-state')
+const { state, url } = require('../fixtures/editor')
+const { schema } = __require('components/editor/schema')
+const { markExtend } = __require('components/editor/selections')
+
+function selectedText(s) {
+  return s.doc.cut(
+    s.selection.from,
+    s.selection.to).textContent
+}
 
 describe('EditorState', () => {
+  const content = state.doc.toJSON().content
+  const p = content[1]
+
   it('has a paragraph with a link', () => {
     expect(p.content).to.eql([{
       text: 'paragraph with link: ',
@@ -22,5 +32,19 @@ describe('EditorState', () => {
       type: 'text'
     }
     ])
+  })
+
+  it('markExtend works correctly', () => {
+    // ensure we're over a link
+    expect(selectedText(state)).to.eql('www')
+
+    // expand mark
+    const range = markExtend(state.selection, schema.marks.link)
+    const stateExpanded = state.apply(
+      state.tr.setSelection(
+        TextSelection.create(state.doc, range.from, range.to)))
+
+    // ensure only the url is selected
+    expect(selectedText(stateExpanded)).to.eql(url)
   })
 })
--- test/fixtures/editor.js
+++ test/fixtures/editor.js
@@ -1,6 +1,6 @@
 'use strict'
 
-const { EditorState } = require('prosemirror-state')
+const { EditorState, TextSelection } = require('prosemirror-state')
 const { doc, p } = require('prosemirror-test-builder')
 const { schema } = __require('components/editor/schema')
 
@@ -14,12 +14,19 @@ let state = EditorState.create({
     p(`paragraph with link: ${url}`))
 })
 
+// add link mark for the url
 state = state.apply(
   state.tr.addMark(
     offset,
     offset + url.length,
     schema.marks.link.create({ href: url })))
 
+// select 'www'
+state = state.apply(
+  state.tr.setSelection(
+    TextSelection.create(state.doc, offset + 7, offset + 10)))
+
 module.exports = {
-  state
+  state,
+  url
 }
