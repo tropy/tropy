c9a4860ac79b9a9ed2ce84ef3c123e5c02b80f07
Kirill Stytsenko <kirstyts@gmail.com>
2017-11-24 13:16:12 +0100

    Move helpers out of markExtend

--- src/components/editor/selections.js
+++ src/components/editor/selections.js
@@ -1,40 +1,40 @@
 'use strict'
 
+const hasMark = (pos, index, markType) =>
+      markType.isInSet(pos.parent.child(index).marks)
+
+const calculate = (pos, markType) => {
+  let startIndex = pos.index()
+  let endIndex = pos.indexAfter()
+
+  if (startIndex === pos.parent.childCount) {
+    startIndex--
+  }
+  while (startIndex > 0 && hasMark(pos, startIndex, markType)) {
+    startIndex--
+  }
+  while (endIndex < pos.parent.childCount && hasMark(pos, endIndex, markType)) {
+    endIndex++
+  }
+
+  let startPos = pos.start()
+  let endPos = startPos
+
+  for (let i = 0; i < endIndex; i++) {
+    let size = pos.parent.child(i).nodeSize
+    if (i <= startIndex) startPos += size
+    endPos += size
+  }
+
+  return { from: startPos, to: endPos }
+}
+
 const markExtend = (selection, markType) => {
   if (!selection) return
 
-  const hasMark = (pos, index) =>
-    markType.isInSet(pos.parent.child(index).marks)
-
-  const calculate = (pos) => {
-    let startIndex = pos.index()
-    let endIndex = pos.indexAfter()
-
-    if (startIndex === pos.parent.childCount) {
-      startIndex--
-    }
-    while (startIndex > 0 && hasMark(pos, startIndex)) {
-      startIndex--
-    }
-    while (endIndex < pos.parent.childCount && hasMark(pos, endIndex)) {
-      endIndex++
-    }
-
-    let startPos = pos.start()
-    let endPos = startPos
-
-    for (let i = 0; i < endIndex; i++) {
-      let size = pos.parent.child(i).nodeSize
-      if (i <= startIndex) startPos += size
-      endPos += size
-    }
-
-    return { from: startPos, to: endPos }
-  }
-
   return {
-    from: calculate(selection.$from).from,
-    to: calculate(selection.$to).to
+    from: calculate(selection.$from, markType).from,
+    to: calculate(selection.$to, markType).to
   }
 }
 
