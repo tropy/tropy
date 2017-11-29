39d7015d8d457f06b3fa885c9a1ceee88b9a8208
Kirill Stytsenko <kirstyts@gmail.com>
2017-11-29 14:29:29 +0100

    Keep alignment and marks within lists as well

--- src/components/editor/commands.js
+++ src/components/editor/commands.js
@@ -84,11 +84,25 @@ module.exports = (schema) => {
         const prev = state.doc.resolve(pos - parentOffset)
         if (prev && prev.parent) {
           const direction = prev.parent.attrs.align
+          const prevMarks = state.doc.resolve(pos).marks()
           if (direction && direction !== 'left') {
-            const prevMarks = state.doc.resolve(pos).marks()
-            tr.setNodeMarkup(pos + 1, null, { align: direction })
-            tr.ensureMarks(prevMarks)
+            // if we are within a list:
+            if (prev.node(prev.depth - 1).type.name === 'list_item') {
+              // We need to dispatch the previous changes. This will update the UI.
+              dispatch(tr)
+              // The old state state does not have the new list item yet, so we cannot
+              // align the paragraph within this new list item.
+              // We build a new state, that now has this new paragraph and list item.
+              const newState = state.apply(tr)
+              // Align the paragraph within the new list item
+              tr = newState.tr.setNodeMarkup(
+                pos + 3, null, { align: direction })
+            } else {
+              // we are not within a list, align the new paragraph
+              tr.setNodeMarkup(pos + 1, null, { align: direction })
+            }
           }
+          tr.ensureMarks(prevMarks)
         }
         dispatch(tr)
       })),
