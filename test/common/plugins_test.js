21f3b42bfed4b7e8567a08025bbdb2c4c15d991d
Sylvester Keil <sylvester@keil.or.at>
2018-02-07 21:29:34 +0100

    Rename test subject

--- test/common/plugins_test.js
+++ test/common/plugins_test.js
@@ -3,48 +3,48 @@
 describe('Plugins', () => {
   const { Plugins } = __require('common/plugins')
 
-  let basic
+  let plugins
 
   beforeEach(async () => {
-    basic = new Plugins(F.plugins().path)
-    await basic.init()
+    plugins = new Plugins(F.plugins().path)
+    await plugins.init()
   })
 
   it('valid config matches', () => {
-    expect(basic.config.length).to.eql(2)
-    expect(basic.config[0]).to.have.keys(['plugin', 'name', 'options'])
-    expect(basic.config[1]).to.have.keys(['plugin'])
+    expect(plugins.config.length).to.eql(2)
+    expect(plugins.config[0]).to.have.keys(['plugin', 'name', 'options'])
+    expect(plugins.config[1]).to.have.keys(['plugin'])
   })
 
   it('hooks can be scanned', () => {
-    basic.scan()
-    expect(basic.spec[0].hooks).to.have.property('sum', true)
-    expect(basic.spec[1].hooks).to.have.property('export', false)
+    plugins.scan()
+    expect(plugins.spec[0].hooks).to.have.property('sum', true)
+    expect(plugins.spec[1].hooks).to.have.property('export', false)
   })
 
   describe('available()', () => {
-    beforeEach(() => basic.scan())
+    beforeEach(() => plugins.scan())
 
     it('registered hooks', () => {
-      expect(basic.available('sum')).to.eql([
+      expect(plugins.available('sum')).to.eql([
         { id: '0', name: 'Plugin Name' },
         { id: '1', name: 'tropy-plugin #1' }
       ])
     })
 
     it('unknown hooks', () => {
-      expect(basic.available('export')).to.be.eql([])
+      expect(plugins.available('export')).to.be.eql([])
     })
   })
 
   it('create()', () => {
-    expect(basic.create().instances).to.have.keys('0', '1')
+    expect(plugins.create().instances).to.have.keys('0', '1')
   })
 
   it('exec()', async () => {
-    basic.create()
-    const a = await basic.exec({ id: 0, action: 'sum' }, 40, 2)
-    const b = await basic.exec({ id: 1, action: 'sum' }, 40, 2)
+    plugins.create()
+    const a = await plugins.exec({ id: 0, action: 'sum' }, 40, 2)
+    const b = await plugins.exec({ id: 1, action: 'sum' }, 40, 2)
 
     expect(a).to.eql(-42)
     expect(b).to.eql(42)
