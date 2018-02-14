37d20eaa9acc04c8f5221e46b54d46b09588d0c6
Sylvester Keil <sylvester@keil.or.at>
2018-02-14 20:58:47 +0100

    Sort constants

--- src/constants/type.js
+++ src/constants/type.js
@@ -2,13 +2,11 @@
 
 module.exports = {
   DATE: 'https://tropy.org/v1/tropy#date',
-  TEXT: 'http://www.w3.org/2001/XMLSchema#string',
-  NUMBER: 'http://www.w3.org/2001/XMLSchema#integer',
   ITEM: 'https://tropy.org/v1/tropy#Item',
+  LIST: 'https://tropy.org/v1/tropy#List',
+  NOTE: 'https://tropy.org/v1/tropy#Note',
+  NUMBER: 'http://www.w3.org/2001/XMLSchema#integer',
   PHOTO: 'https://tropy.org/v1/tropy#Photo',
   SELECTION: 'https://tropy.org/v1/tropy#Selection',
-
-  // These identify the exported JSON-LD entities
-  LIST: 'https://tropy.org/v1/tropy#List',
-  NOTE: 'https://tropy.org/v1/tropy#Note'
+  TEXT: 'http://www.w3.org/2001/XMLSchema#string'
 }
