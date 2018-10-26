b7fa96dcb8a46590678a62c58da1c66f791dc2d6
Sylvester Keil <sylvester@keil.or.at>
2018-10-26 21:41:31 +0200

    Add Collapse transition component

--- src/components/fx.js
+++ src/components/fx.js
@@ -1,7 +1,8 @@
 'use strict'
 
 const React = require('react')
-const { on } = require('../dom')
+const { number } = require('prop-types')
+const { on, bounds } = require('../dom')
 
 const {
   CSSTransition,
@@ -9,9 +10,9 @@ const {
   TransitionGroup
 } = require('react-transition-group')
 
-const onTransitionEnd = (element, done) => {
-  on(element, 'transitionend', event => {
-    if (event.target === element) done()
+const onTransitionEnd = (node, done) => {
+  on(node, 'transitionend', event => {
+    if (event.target === node) done()
   })
 }
 
@@ -25,10 +26,64 @@ const Fade = (props) => (
     {...props}/>
 )
 
+class Collapse extends React.Component {
+  willCollapse = (node) => {
+    node.style.height = `${
+      this.props.height > 0 ?  this.props.height : bounds(node).height
+    }px`
+  }
+
+  collapse = (node) => {
+    node.style.height = '0px'
+  }
+
+  didCollapse = (node) => {
+    node.style.height = null
+  }
+
+  willExpand = (node) => {
+    node.style.height = '0px'
+  }
+
+  expand = (node) => {
+    node.style.height = `${this.props.height}px`
+  }
+
+  didExpand = (node) => {
+    node.style.height = null
+  }
+
+  render() {
+    // eslint-disable-next-line no-unused-vars
+    let { height, ...props } = this.props
+
+    return (
+      <CSSTransition
+        addEndListener={onTransitionEnd}
+        classNames="collapse"
+        mountOnEnter={false}
+        timeout={1000}
+        unmountOnExit
+        onEnter={this.willExpand}
+        onEntering={this.expand}
+        onEntered={this.didExpand}
+        onExit={this.willCollapse}
+        onExiting={this.collapse}
+        onExited={this.didCollapse}
+        {...props}/>
+    )
+  }
+
+  static propTypes = {
+    height: number
+  }
+}
+
 module.exports = {
   CSSTransition,
   Transition,
   TransitionGroup,
   onTransitionEnd,
+  Collapse,
   Fade
 }
