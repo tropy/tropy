9dd3fbf6c0205a07fc49f64f17fbb3ecb89ba7b3
Sylvester Keil <sylvester@keil.or.at>
2018-12-06 14:56:55 +0100

    Clear old project caches

--- src/commands/cache.js
+++ src/commands/cache.js
@@ -1,9 +1,18 @@
 'use strict'
 
-const { call, select } = require('redux-saga/effects')
+const { call, cps, select } = require('redux-saga/effects')
 const { Command } = require('./command')
 const { CACHE } = require('../constants')
 const { info } = require('../common/log')
+const { rm } = require('../common/rm')
+const { get } = require('../common/util')
+const { readdir } = require('fs')
+const { join } = require('path')
+
+const PAST = (new Date(2018, 11, 8)).getTime()
+
+const addMonths = (k = 0, d = new Date()) =>
+  new Date(d.getFullYear(), d.getMonth() + k, d.getDate())
 
 class Prune extends Command {
   static get ACTION() { return CACHE.PRUNE }
@@ -17,6 +26,22 @@ class Prune extends Command {
       info('clearing project cache...')
       files = yield call(cache.prune, state)
       info(`cleared ${files.length} file(s) from cache...`)
+    }
+
+    if (state.recent) {
+      info('checking for old project caches...')
+      let now = new Date()
+      let caches = yield cps(readdir, ARGS.cache)
+
+      for (let id of caches) {
+        let date = addMonths(6,
+          new Date(get(state.recent, [id, 'opened'], PAST)))
+
+        if (date < now) {
+          info(`removing old project cache ${id}...`)
+          yield call(rm, join(ARGS.cache, id))
+        }
+      }
     }
 
     return files
