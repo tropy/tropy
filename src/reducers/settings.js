21adc2a76d8e7ff5a8e109580ebd28aad872bd2b
Sylvester Keil <sylvester@keil.or.at>
2019-01-08 16:48:54 +0100

    Assume local time zone by default

--- src/reducers/settings.js
+++ src/reducers/settings.js
@@ -8,7 +8,7 @@ const defaults = {
   dup: 'prompt',
   layout: ITEM.LAYOUT.STACKED,
   locale: ARGS.locale,
-  localtime: false,
+  localtime: true,
   template: ITEM.TEMPLATE,
   theme: ARGS.theme,
   overlayToolbars: ARGS.frameless,
