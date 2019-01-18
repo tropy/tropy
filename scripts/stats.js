7a27dc223b646f45e57d81338c81058ec2dabc9f
Sylvester Keil <sylvester@keil.or.at>
2019-01-18 10:05:12 +0100

    Add 1.4.0 release

--- scripts/stats.js
+++ scripts/stats.js
@@ -4,8 +4,9 @@ const fetch = require('node-fetch')
 const util = require('util')
 
 const VERSIONS = [
+  '1.4.0-beta.2',
+  '1.4.0',
   '1.3.2',
-  '1.3.2-beta.2',
   '1.3.1',
   '1.3.0',
   '1.2.0',
