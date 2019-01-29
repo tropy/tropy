551b8cb016aded19422832246b4b2fc119780982
Sylvester Keil <sylvester@keil.or.at>
2019-01-29 13:22:17 +0100

    Update PhotoToolbar

--- src/components/photo/toolbar.js
+++ src/components/photo/toolbar.js
@@ -1,82 +1,66 @@
 'use strict'
 
 const React = require('react')
-const { PureComponent } = React
 const { FormattedMessage } = require('react-intl')
-const { Toolbar, ToolGroup } = require('../toolbar')
 const { Slider } = require('../slider')
 const { Button } = require('../button')
 const { number, bool, func } = require('prop-types')
+
+const {
+  Toolbar, ToolGroup, ToolbarLeft, ToolbarRight
+} = require('../toolbar')
 
 const {
   IconPhoto, IconPlus, IconListSmall, IconGridSmall
 } = require('../icons')
 
 
-class PhotoToolbar extends PureComponent {
-  handleCreate = () => {
-    this.props.onCreate()
-  }
+const PhotoToolbar = (props) => (
+  <Toolbar isDraggable={false}>
+    <ToolbarLeft>
+      <IconPhoto/>
+      <h4>
+        <FormattedMessage
+          id="panel.photo.title"
+          values={{ count: props.photos }}/>
+      </h4>
+    </ToolbarLeft>
 
-  render() {
-    const {
-      hasCreateButton,
-      isDisabled,
-      photos,
-      zoom,
-      maxZoom,
-      onZoomChange
-    } = this.props
-
-    return (
-      <Toolbar isDraggable={false}>
-        <div className="toolbar-left">
-          <IconPhoto/>
-          <h4>
-            <FormattedMessage
-              id="panel.photo.title"
-              values={{ count: photos }}/>
-          </h4>
-        </div>
-
-        <div className="toolbar-right">
-          {
-            hasCreateButton &&
-              <ToolGroup>
-                <Button
-                  icon={<IconPlus/>}
-                  isDisabled={!this.props.canCreate}
-                  title="panel.photo.create"
-                  onClick={this.handleCreate}/>
-              </ToolGroup>
-          }
+    <ToolbarRight>
+      {
+        props.hasCreateButton &&
           <ToolGroup>
-            <Slider
-              value={zoom}
-              max={maxZoom}
-              size="sm"
-              minIcon={<IconListSmall/>}
-              maxIcon={<IconGridSmall/>}
-              isDisabled={isDisabled}
-              tabIndex={-1}
-              onChange={onZoomChange}/>
+            <Button
+              icon={<IconPlus/>}
+              isDisabled={!props.canCreate}
+              title="panel.photo.create"
+              onClick={props.onCreate}/>
           </ToolGroup>
-        </div>
-      </Toolbar>
-    )
-  }
+      }
+      <ToolGroup>
+        <Slider
+          value={props.zoom}
+          max={props.maxZoom}
+          size="sm"
+          minIcon={<IconListSmall/>}
+          maxIcon={<IconGridSmall/>}
+          isDisabled={props.isDisabled}
+          tabIndex={-1}
+          onChange={props.onZoomChange}/>
+      </ToolGroup>
+    </ToolbarRight>
+  </Toolbar>
+)
 
-
-  static propTypes = {
-    canCreate: bool,
-    hasCreateButton: bool,
-    isDisabled: bool,
-    maxZoom: number.isRequired,
-    photos: number.isRequired,
-    zoom: number.isRequired,
-    onCreate: func.isRequired,
-    onZoomChange: func.isRequired
-  }
+PhotoToolbar.propTypes = {
+  canCreate: bool,
+  hasCreateButton: bool,
+  isDisabled: bool,
+  maxZoom: number.isRequired,
+  photos: number.isRequired,
+  zoom: number.isRequired,
+  onCreate: func.isRequired,
+  onZoomChange: func.isRequired
 }
 
 module.exports = {
