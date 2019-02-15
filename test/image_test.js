e1c95e787994b63bcd460c946dd5865a5610d02c
Sylvester Keil <sylvester@keil.or.at>
2019-02-15 15:20:09 +0100

    Update EXIF test

--- test/image_test.js
+++ test/image_test.js
@@ -30,6 +30,7 @@ describe('Image', () => {
       console.log(Object.keys(image.exif[0]))
       expect(image.exif[0]).to.contain.keys([
         'http://www.w3.org/2003/12/exif/ns#orientation',
+        'http://www.w3.org/2003/12/exif/ns#dateTimeOriginal',
         'http://www.w3.org/2003/12/exif/ns#modifyDate'
       ])
     })
