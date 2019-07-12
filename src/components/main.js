4c007eb0b4850607c4c3c23374718dc3622e1165
Flachware <johannes.krtek@flachware.com>
2019-07-12 10:55:01 +0200

    Fix print layout.

--- src/components/main.js
+++ src/components/main.js
@@ -30,10 +30,10 @@ class Main extends React.Component {
       <WindowContext.Provider value={this.props.window}>
         <Provider store={this.props.store}>
           <IntlProvider>
-            <div className="main-container">
+            <React.Fragment>
               {this.props.children}
               <Flash/>
-            </div>
+            </React.Fragment>
           </IntlProvider>
         </Provider>
       </WindowContext.Provider>
