8c3a56384f43c0b8c0e4650c9708950a47f94fed
Sylvester Keil <sylvester@keil.or.at>
2019-08-27 14:23:55 +0200

    Add metadata.copy command

--- src/commands/metadata.js
+++ src/commands/metadata.js
@@ -21,6 +21,67 @@ class Load extends Command {
   }
 }
 
+class Copy extends Command {
+  static get ACTION() {
+    return METADATA.COPY
+  }
+
+  *exec() {
+    let { db } = this.options
+    let { payload, meta } = this.action
+
+    let data = yield this.fetch()
+
+    yield put(act.merge(data))
+
+    yield call(db.transaction, async tx => {
+      for (let id of payload.id) {
+        await mod.update(tx, {
+          id,
+          data: data[id],
+          timestamp: meta.now
+        })
+      }
+    })
+
+    return payload
+  }
+
+  *fetch() {
+    let { payload, meta } = this.action
+    let { from, to } = payload
+
+    let copy = {}
+    let data = {}
+
+    yield select(({ metadata }) => {
+      let props = meta.cut ? [from, to] : [to]
+
+      for (let id of payload.id) {
+        let md = metadata[id] || {}
+
+        copy[id] = pick(md, props, {}, true)
+        data[id] = { [to]: md[from] }
+
+        if (meta.cut) {
+          data[id][from] = null
+        }
+      }
+    })
+
+    this.undo = act.restore(copy)
+    this.original = copy
+
+    return data
+  }
+
+  *abort() {
+    if (this.original) {
+      yield put(act.merge(this.original))
+    }
+  }
+}
+
 class Restore extends Command {
   static get ACTION() { return METADATA.RESTORE }
 
@@ -143,6 +204,7 @@ class Delete extends Command {
 
 module.exports = {
   Add,
+  Copy,
   Delete,
   Load,
   Restore,
