c91e23a17e4a42cb324f0063e45ea20e7435d383
Roberto Martinez <romama@gmail.com>
2019-08-28 09:35:37 -0400

    Adds a relative position fro dragging Metadata field.

--- src/components/metadata/drag-preview.js
+++ src/components/metadata/drag-preview.js
@@ -1,7 +1,6 @@
 'use strict'
 
 const React = require('react')
-const cx = require('classnames')
 
 const {
   arrayOf,
@@ -13,14 +12,11 @@ const {
 
 
 class FieldDragPreview extends React.PureComponent {
-  get classes() {
-    return ['metadata-field', 'drag-preview', 'copy']
-  }
 
   render() {
     const item = this.props.items[0]
     return (
-      <div className={cx(this.classes)}>
+      <div className="metadata-field drag-preview copy">
         <div>
           {item.value}
           {item.isMixed && <span>+</span>}
