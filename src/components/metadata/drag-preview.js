e3e3a8df7bdb7132b6b11a724d05cb6cf73de61b
Roberto Martinez <romama@gmail.com>
2019-08-28 17:23:19 -0400

    Converts FieldDragPreview to a functional component

--- src/components/metadata/drag-preview.js
+++ src/components/metadata/drag-preview.js
@@ -10,34 +10,27 @@ const {
   string
 } = require('prop-types')
 
+const FieldDragPreview = ({ field }) => (
+  <React.Fragment>
+    <div className="metadata-field drag-preview">
+      <div>
+        {field.value}
+        {field.isMixed && <span>+</span>}
+      </div>
+    </div>
+    {field.id.length &&
+      <div className="badge">{field.id.length}</div>
+    }
+  </React.Fragment>
+)
 
-class FieldDragPreview extends React.PureComponent {
-
-  render() {
-    const field = this.props.field
-    return (
-      <React.Fragment>
-        <div className="metadata-field drag-preview">
-          <div>
-            {field.value}
-            {field.isMixed && <span>+</span>}
-          </div>
-        </div>
-        {field.id.length &&
-          <div className="badge">{field.id.length}</div>
-        }
-      </React.Fragment>
-    )
-  }
-
-  static propTypes = {
-    field: shape({
-      id: oneOfType([number, arrayOf(number)]).isRequired,
-      value: string.isRequired
-    }).isRequired,
-  }
-
+FieldDragPreview.propTypes = {
+  field: shape({
+    id: oneOfType([number, arrayOf(number)]).isRequired,
+    value: string.isRequired
+  }).isRequired,
 }
+
 
 module.exports = {
   FieldDragPreview
