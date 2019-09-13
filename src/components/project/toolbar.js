9f15720ca3b342bc77b65fdee065016c6e0dac88
Roberto Martinez <romama@gmail.com>
2019-09-13 04:29:24 -0400

    Fixes https://github.com/tropy/tropy/issues/88 (#398)
    * Fixes https://github.com/tropy/tropy/issues/88
    
    * Refactors code
    
    * Transforms ProjectToolbar from function component to pure component
    
    * Adds stopOnMouseLeave to EsperToolbar
    
    * Creates onDrag handler and copies order between onDragStart/Stop

--- src/components/project/toolbar.js
+++ src/components/project/toolbar.js
@@ -1,6 +1,7 @@
 'use strict'
 
 const React = require('react')
+const { WindowContext } = require('../main')
 const { FormattedMessage } = require('react-intl')
 const { bool, func, number, string } = require('prop-types')
 const { Titlebar, Toolbar, ToolGroup } = require('../toolbar')
@@ -14,54 +15,61 @@ const {
   IconGrid
 } = require('../icons')
 
+class ProjectToolbar extends React.PureComponent {
 
-const ProjectToolbar = (props) => (
-  <Titlebar>
-    <Toolbar.Left>
-      <ToolGroup>
-        <Slider
-          value={props.zoom}
-          max={props.maxZoom}
-          isDisabled={props.isDisabled || props.count === 0}
-          onChange={props.onZoomChange}
-          minIcon={<IconList/>}
-          maxIcon={<IconGrid/>}
-          tabIndex={-1}/>
-      </ToolGroup>
-      <ToolGroup>
-        <Button
-          icon={<IconPlus/>}
-          isDisabled={props.isDisabled || !props.canCreateItems}
-          title="toolbar.import"
-          onClick={props.onItemCreate}/>
-      </ToolGroup>
-    </Toolbar.Left>
-    <Toolbar.Center>
-      <div className="item-count">
-        <FormattedMessage
-          id="toolbar.items"
-          values={{ count: props.count }}/>
-      </div>
-    </Toolbar.Center>
-    <Toolbar.Right>
-      <SearchField
-        query={props.query}
-        isDisabled={props.isDisabled}
-        onSearch={props.onSearch}/>
-    </Toolbar.Right>
-  </Titlebar>
-)
+  render() {
+    return (
+      <Titlebar>
+        <Toolbar.Left>
+          <ToolGroup>
+            <Slider
+              value={this.props.zoom}
+              max={this.props.maxZoom}
+              isDisabled={this.props.isDisabled || this.props.count === 0}
+              stopOnMouseLeave={this.context.state.frameless}
+              onChange={this.props.onZoomChange}
+              minIcon={<IconList/>}
+              maxIcon={<IconGrid/>}
+              tabIndex={-1}/>
+          </ToolGroup>
+          <ToolGroup>
+            <Button
+              icon={<IconPlus/>}
+              isDisabled={this.props.isDisabled || !this.props.canCreateItems}
+              title="toolbar.import"
+              onClick={this.props.onItemCreate}/>
+          </ToolGroup>
+        </Toolbar.Left>
+        <Toolbar.Center>
+          <div className="item-count">
+            <FormattedMessage
+              id="toolbar.items"
+              values={{ count: this.props.count }}/>
+          </div>
+        </Toolbar.Center>
+        <Toolbar.Right>
+          <SearchField
+            query={this.props.query}
+            isDisabled={this.props.isDisabled}
+            onSearch={this.props.onSearch}/>
+        </Toolbar.Right>
+      </Titlebar>
+    )
+  }
 
-ProjectToolbar.propTypes = {
-  canCreateItems: bool,
-  count: number.isRequired,
-  isDisabled: bool,
-  maxZoom: number.isRequired,
-  query: string.isRequired,
-  zoom: number.isRequired,
-  onItemCreate: func.isRequired,
-  onSearch: func.isRequired,
-  onZoomChange: func.isRequired
+  static contextType = WindowContext
+
+  static propTypes = {
+    canCreateItems: bool,
+    count: number.isRequired,
+    isDisabled: bool,
+    maxZoom: number.isRequired,
+    query: string.isRequired,
+    zoom: number.isRequired,
+    onItemCreate: func.isRequired,
+    onSearch: func.isRequired,
+    onZoomChange: func.isRequired
+  }
 }
 
 module.exports = {
