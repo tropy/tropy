9b1b2ced03bec38c333c0a91b44fa35f54901ee4
Sylvester Keil <sylvester@keil.or.at>
2019-10-16 16:39:53 +0200

    Eager load all images for printing

--- src/components/print/photo.js
+++ src/components/print/photo.js
@@ -32,6 +32,7 @@ const Photo = ({ canOverflow, item, hasMetadata, hasNotes, photo, cache }) => {
       <div className="photo-container">
         <img
           className={`iiif rot-${rotation.format('x')}`}
+          loading="eager"
           src={source(photo, cache)}/>
       </div>
       {hasMetadata &&
