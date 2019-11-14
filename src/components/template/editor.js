ccdc97ef7317d2fc471953520a0ddc44e34190ba
Roberto Martinez <romama@gmail.com>
2019-11-14 12:03:39 -0500

    Revert to text-right

--- src/components/template/editor.js
+++ src/components/template/editor.js
@@ -209,7 +209,7 @@ class TemplateEditor extends React.PureComponent {
               tabIndex={0} onChange={this.handleTemplateUpdate}
               size={9}/>
             {isPristine &&
-              <FormElement className="flex-row justify-content-end">
+              <FormElement className="text-right">
                 <button
                   className="btn btn-primary min-width"
                   disabled={!this.isValid}
