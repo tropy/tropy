f5bb8e9b87a5d701af93f6f2f6d58ed92013477b
Sylvester Keil <sylvester@keil.or.at>
2019-11-25 21:01:01 +0100

    Convert Panel Group test

--- test/components/panel_test.js
+++ test/components/panel_test.js
@@ -1,12 +1,19 @@
 'use strict'
 
 const React = require('react')
-const { shallow } = require('enzyme')
+const { render } = require('../support/react')
 
-describe.skip('PanelGroup', () => {
+describe('PanelGroup', () => {
   const { PanelGroup } = __require('components/panel')
 
-  it('has class panel-group', () => {
-    expect(shallow(<PanelGroup slots={[]}/>)).to.have.className('panel-group')
+  it('renders panel-group structure', () => {
+    expect(render(
+      <PanelGroup
+        slots={[]}
+        onResize={sinon.spy()}/>
+    ).element())
+      .to.have.class('panel-group')
+      .and.have.descendants('.panel-group-header, .panel-group-body')
+      .and.have.length(2)
   })
 })
