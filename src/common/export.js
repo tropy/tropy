50d0ad568edca24929f2049aceb16e950ce7908a
Sylvester Keil <sylvester@keil.or.at>
2020-01-24 17:28:53 +0100

    Move export.props to common

---
+++ src/common/export.js
@@ -0,0 +1,44 @@
+'use strict'
+
+
+
+const props = {
+  item: [
+    'template'
+  ],
+
+  image: [
+    'angle',
+    'brightness',
+    'contrast',
+    'height',
+    'hue',
+    'mirror',
+    'negative',
+    'saturation',
+    'sharpen',
+    'width'
+  ],
+
+  photo: [
+    'checksum',
+    'mimetype',
+    'orientation',
+    'path',
+    'size',
+    'template'
+  ],
+
+  selection: [
+    'template',
+    'x',
+    'y'
+  ],
+
+  note: [
+  ]
+}
+
+module.exports = {
+  props
+}
