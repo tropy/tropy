1c19203c764b2672be34a17b4ba6a0762911346b
Roberto Martinez <romama@gmail.com>
2020-02-21 16:56:16 -0500

    Add required functionalities to selectFile

--- src/components/select-file.js
+++ src/components/select-file.js
@@ -4,13 +4,7 @@ const React = require('react')
 const { noop } = require('../common/util')
 const { AutoResizer } = require('./auto-resizer')
 const { Button, ButtonGroup } = require('./button')
-
-const { StaticField } = require('./metadata/field')
-
 const { open } = require('../dialog')
-
-//const { open } = require('../browser/dialog')
-
 
 const {
   bool, func, number, object, oneOfType, oneOf, string, arrayOf
@@ -18,160 +12,104 @@ const {
 
 
 class SelectFile extends React.PureComponent {
-  input = React.createRef()
+  selectfile = React.createRef()
 
   state =  {
     hasFocus: false,
-    key: this.props.value,
-    query: '',
     value: this.props.value
-  }
-
-  static getDerivedStateFromProps(props, state) {
-    return (props.value === state.key) ? null : {
-      key: props.value,
-      query: '',
-      value: props.value
-    }
   }
 
   componentDidMount() {
     if (this.props.autofocus)
-      this.input.current.focus()
-  }
-
-
-  get isValid() {
-    return this.input.current.validity.valid
-  }
-
-  get hasChanged() {
-    return this.state.value !== this.state.key
-  }
-
-  cancel = (isForced) => {
-    this.hasBeenCancelled = true
-
-    if (this.isValid) {
-      this.props.onCancel(this.hasChanged, isForced)
-    } else {
-      this.reset()
-      this.props.onCancel(false, isForced)
-    }
-  }
-
-  commit(isForced) {
-    if (this.isValid) {
-      if (!this.hasBeenCommitted) {
-        this.hasBeenCommitted = true
-        this.props.onCommit(this.state.value, this.hasChanged, isForced)
-      }
-    } else {
-      this.cancel()
-    }
+      this.selectfile.current.focus()
   }
 
   focus = () => {
-    if (this.input.current)
-      this.input.current.focus()
+    if (this.selectfile.current)
+      this.selectfile.current.focus()
   }
 
-  reset() {
-    this.hasBeenCancelled = false
-    this.hasBeenCommitted = false
-
-    this.setState({
-      key: this.props.value,
-      query: '',
-      value: this.props.value
-    })
+  clear() {
+    this.handleChange('')
   }
 
   handleBlur = (event) => {
     let cancel = this.props.onBlur(event)
     this.setState({ hasFocus: false })
-
-    if (this.hasBeenCancelled || this.hasBeenCommitted)
-      return null
     if (cancel)
       this.cancel()
-    else
-      this.commit()
   }
 
   handleFocus = (event) => {
     this.props.onFocus(event)
-
-
-    this.hasBeenCancelled = false
-    this.hasBeenCommitted = false
-
-    this.setState({
-      hasFocus: true
-    })
-  }
-
-  handleChange = ({ target }) => {
-    this.setState({ value: target.value, query: target.value })
-    this.props.onChange(target.value)
-  }
-
-  handleCompletion = (value) => {
-    this.setState({ value }, () => this.commit(true))
-    this.props.onChange(value)
+    this.setState({ hasFocus: true })
   }
 
   handleKeyDown = (event) => {
-    if (this.props.onKeyDown != null) {
-      if (this.props.onKeyDown(event, this))
+    event.stopPropagation()
+    switch (event.key) {
+      case 'Backspace':
+        this.clear()
+        break
+      case 'Enter':
+        this.handleFileClick()
+        break
+      default:
         return null
     }
-
-    event.stopPropagation()
-
-    // Prevent default and global bindings if we handled the key!
-    event.preventDefault()
-    event.nativeEvent.stopImmediatePropagation()
   }
 
+  handleChange = (value) => {
+    if (this.state.value !== value) {
+      this.setState({ value })
+      this.props.onChange(value)
+    }
+  }
 
   handleFileClick = () => {
     const { filters, type } = this.props
-    open.custom({
-      defaultDirectory: this.props.defaultDirectory,
+    open({
+      defaultPath: this.props.defaultDirectory,
       filters: filters,
       properties: type === 'file' ? ['openFile'] : ['openDirectory']
+    }).then((res) => {
+      if (res.length) {
+        this.handleChange(res[0])
+      }
     })
   }
 
-  handleClearButtonClick = (event) => {
-    this.props.onClear(this.props.value)
-    event.stopPropagation()
+  handleClearButtonClick = () => {
+    this.clear()
   }
 
 
   render() {
-    return (
-      <div className="input-group">
-        <AutoResizer
-          content={this.state.value}
-          isDisabled={!this.props.resize}>
-
-          <span
-            onClick={this.handleFileClick}>{this.props.value}
-          </span>
-          <ButtonGroup>
-            {!this.props.isRequired && (
-            <Button
-              onClick={this.handleClearButtonClick}
-              text="select.clear" />
-            )}
-            <Button
-              onClick={this.handleFileClick}
-              text="select.browse" />
-          </ButtonGroup>
-        </AutoResizer>
-      </div>
+    return (<div
+      className="input-group"
+      onKeyDown={this.handleKeyDown}
+      onBlur={this.handleBlur}
+      onFocus={this.handleFocus}>
+      <AutoResizer
+        content={this.state.value}
+        isDisabled={!this.props.resize}>
+        <input type="text" className="form-control"
+          id={this.props.id}
+          value={this.state.value}/>
+        <ButtonGroup>
+          {!this.props.isRequired && (
+          <Button
+            className="btn-default"
+            onClick={this.handleClearButtonClick}
+            text="select.clear" />
+          )}
+          <Button
+            className="btn-default"
+            onClick={this.handleFileClick}
+            text="select.browse" />
+        </ButtonGroup>
+      </AutoResizer>
+    </div>
     )
   }
 
@@ -182,7 +120,6 @@ class SelectFile extends React.PureComponent {
     isDisabled: bool,
     isReadOnly: bool,
     isRequired: bool,
-    placeholder: string,
     resize: bool,
     tabIndex: number,
     value: oneOfType([string]).isRequired,
@@ -192,6 +129,7 @@ class SelectFile extends React.PureComponent {
     onCommit: func.isRequired,
     onFocus: func.isRequired,
     onClear: func,
+    onClick: func.isRequired,
     onKeyDown: func,
 
     type: oneOf(['file', 'directory']).isRequired,
@@ -201,6 +139,7 @@ class SelectFile extends React.PureComponent {
   }
 
   static defaultProps = {
+    defaultDirectory: '',
     tabIndex: -1,
     onBlur: noop,
     onCancel: noop,
@@ -208,7 +147,8 @@ class SelectFile extends React.PureComponent {
     onCommit: noop,
     onFocus: noop,
     onClear: noop,
-    type: 'directory',
+    onClick: noop,
+    type: 'file',
     filters: []
   }
 }
