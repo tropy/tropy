43802f991466bbfc5e01e4072196ed0280aa9539
Sylvester Keil <sylvester@keil.or.at>
2020-05-17 18:19:26 +0200

    Use save dialog when exoprting photos / selections

--- src/commands/photo/extract.js
+++ src/commands/photo/extract.js
@@ -5,31 +5,50 @@ const { call, select } = require('redux-saga/effects')
 const { PHOTO } = require('../../constants')
 const { Cache } = require('../../common/cache')
 const { Rotation } = require('../../common/iiif')
+const { warn, info } = require('../../common/log')
+const { blank } = require('../../common/util')
 const { Esper } = require('../../esper')
+const dialog = require('../../dialog')
 const sharp = require('sharp')
 
 
 class Extract extends Command {
   *exec() {
-    let { cache } = this.options
-    let { payload } = this.action
+    try {
+      let { cache } = this.options
+      let { payload } = this.action
 
-    let [photo, selection] = yield select(state => ([
-      state.photos[payload.id],
-      state.selections[payload.selection]
-    ]))
+      var file = yield call(dialog.save.image)
 
-    let image = selection || photo
-    let src = Cache.url(cache, 'full', photo)
+      if (blank(file)) return
 
-    let { buffer, ...raw } = yield call(Esper.instance.extract, src, {
-      ...image,
-      ...Rotation.addExifOrientation(image, photo).toJSON()
-    })
+      let [photo, selection] = yield select(state => ([
+        state.photos[payload.id],
+        state.selections[payload.selection]
+      ]))
 
-    let out = sharp(buffer, { raw })
+      var image = selection || photo
+      let src = Cache.url(cache, 'full', photo)
 
-    yield call([out, out.toFile], '/Users/dupin/Desktop/out.jpg')
+      let { buffer, ...raw } = yield call(Esper.instance.extract, src, {
+        ...image,
+        ...Rotation.addExifOrientation(image, photo).toJSON()
+      })
+
+      let out = sharp(buffer, { raw })
+
+      yield call([out, out.toFile], file)
+      info(`saved image #${image.id} as ${file}`)
+
+      return {
+        file,
+        size: buffer.length
+      }
+
+    } catch (e) {
+      warn({ stack: e.stack }, `failed to save image #${image.id} as ${file}`)
+      dialog.fail(e, this.action.type)
+    }
   }
 }
 
