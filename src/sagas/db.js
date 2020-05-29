7659c1df5064cf00cae920ef5df5dd4419f79d4e
Sylvester Keil <sylvester@keil.or.at>
2020-05-29 14:25:28 +0200

    Check for read-only access before project db open

--- src/sagas/db.js
+++ src/sagas/db.js
@@ -2,39 +2,47 @@
 
 const { eventChannel } = require('redux-saga')
 const { call, put, take } = require('redux-saga/effects')
-const { warn } = require('../common/log')
+const { debug, warn } = require('../common/log')
 const { fail } = require('../dialog')
 
 
 function ErrorChannel(db) {
   return eventChannel(emitter => {
-    db.on('error', error => { emitter({ error }) })
+    db.on('error', emitter)
+
     return () => {
-      db.removeEventListener('error', emitter)
+      db.removeListener('error', emitter)
     }
   })
 }
 
-function onErrorPut(action, times = 1) {
-  return function* onErrorPutAction(db) {
-    const channel = yield call(ErrorChannel, db)
-    const { error } = yield take(channel)
+function* handleDatabaseErrors(db, actions) {
+  try {
+    var channel = yield call(ErrorChannel, db)
 
-    warn({
-      msg: 'unexpected db error',
-      stack: error.stack
-    })
+    while (true) {
+      let e = yield take(channel)
 
-    yield call(fail, error, db.path)
-    yield put(action(error))
+      if (e.code in actions) {
+        yield put(actions[e.code](e))
 
-    if (times != null) {
-      if (--times <= 0) channel.close()
+      } else {
+        warn({ stack: e.stack }, 'unexpected db error')
+
+        yield call(fail, e, db.path)
+        yield put(actions.default(e))
+      }
     }
+  } catch (e) {
+    warn({ stack: e.stack }, 'unexpected error in *handleDatabaseErrors')
+
+  } finally {
+    channel.close()
+    debug('*handleDatabaseErrors terminated')
   }
 }
 
 module.exports = {
   ErrorChannel,
-  onErrorPut
+  handleDatabaseErrors
 }
