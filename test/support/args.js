49bf2d21c28ee0c8abf0b6c82a532bb2091c59b1
Sylvester Keil <sylvester@keil.or.at>
2020-08-27 18:09:15 +0200

    Update test usage of ARGS and win

--- test/support/args.js
+++ test/support/args.js
@@ -1,7 +1,9 @@
 import { update } from '../../src/args'
 
-update({
-  locale: 'en',
-  env: process.env.NODE_ENV || 'test',
-  debug: process.env.TROPY_DEBUG || process.env.DEBUG
-})
+if (process.type === 'renderer') {
+  update({
+    locale: 'en',
+    env: process.env.NODE_ENV || 'test',
+    debug: process.env.TROPY_DEBUG || process.env.DEBUG
+  })
+}
