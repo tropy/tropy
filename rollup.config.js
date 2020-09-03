4e2f98f1870fa1b53dd91a1ea648631c2eee7068
Sylvester Keil <sylvester@keil.or.at>
2020-09-03 18:16:13 +0200

    Initial working rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -2,29 +2,78 @@ import babel from '@rollup/plugin-babel'
 import builtins from 'builtin-modules'
 import commonjs from '@rollup/plugin-commonjs'
 import json from '@rollup/plugin-json'
+import natives from 'rollup-plugin-natives'
+import replace from '@rollup/plugin-replace'
 import resolve from '@rollup/plugin-node-resolve'
 
-export default {
-  input: [
-    './src/browser/main.js',
-    './src/common/api.js'
-  ],
-  output: {
-    dir: './lib',
-    format: 'cjs'
+export default [
+  {
+    input: [
+      'src/browser/main.js',
+      'src/common/api.js'
+    ],
+    output: {
+      dir: 'lib/browser',
+      format: 'cjs',
+      sourcemap: false
+    },
+    preserveEntrySignatures: 'strict',
+    plugins: [
+      resolve(),
+      babel({ babelHelpers: 'bundled' }),
+      json(),
+      commonjs({
+        ignore: [
+          'pino-pretty'
+        ]
+      })
+    ],
+    external: [
+      'electron',
+      ...builtins
+    ]
   },
-  plugins: [
-    resolve(),
-    json(),
-    babel({ babelHelpers: 'bundled' }),
-    commonjs({
-      ignore: [
-        'pino-pretty'
-      ]
-    })
-  ],
-  external: [
-    'electron',
-    ...builtins
-  ]
-}
+
+  {
+    input: {
+      'bootstrap': 'src/bootstrap.js',
+      'image': 'src/image/index.js',
+      'db': 'src/common/db.js',
+      'views/about': 'src/views/about.js',
+      'views/prefs': 'src/views/prefs.js',
+      'views/project': 'src/views/project.js',
+      'views/print': 'src/views/print.js',
+      'views/wizard': 'src/views/wizard.js'
+    },
+    output: {
+      dir: 'lib',
+      format: 'cjs',
+      sourcemap: true
+    },
+    preserveEntrySignatures: 'strict',
+    plugins: [
+      natives({
+        copyTo: 'lib/node/lib',
+        destDir: './node/lib'
+      }),
+      resolve(),
+      replace({
+        'process.env.NODE_ENV': '"production"',
+        'readable-stream': 'stream'
+      }),
+      json(),
+      babel({ babelHelpers: 'bundled' }),
+      commonjs({
+        ignore: [
+          'pino-pretty',
+          'request',
+          'rdf-canonize-native'
+        ]
+      })
+    ],
+    external: [
+      'electron',
+      ...builtins
+    ]
+  }
+]
