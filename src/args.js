66e7a95e5d989095ae3554d5b0e4a0a05215e85d
Sylvester Keil <sylvester@keil.or.at>
2020-09-08 10:38:38 +0200

    Remove NODE_ENV assignment in renderer

--- src/args.js
+++ src/args.js
@@ -1,19 +1,18 @@
 const decode = decodeURIComponent
 const encode = encodeURIComponent
 
-let ARGS = {}
+const ARGS = {}
+
 export default ARGS
 
 export function parse() {
   let hash = window.location.hash.slice(1)
-
   Object.assign(ARGS, JSON.parse(decode(hash)))
-  // process.env.NODE_ENV = ARGS.env
-
   return ARGS
 }
 
 export function update(hash) {
   Object.assign(ARGS, hash)
   window.location.hash = encode(JSON.stringify(ARGS))
+  return ARGS
 }
