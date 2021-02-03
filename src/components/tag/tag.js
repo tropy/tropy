5c7b813423576109c65102af7b6208049a2708c4
Flachware <mail@flachware.com>
2021-02-03 19:05:17 +0100

    Display plus icon only in focus state.

--- src/components/tag/tag.js
+++ src/components/tag/tag.js
@@ -109,7 +109,7 @@ class Tag extends React.PureComponent {
             onCancel={this.props.onEditCancel}
             onChange={this.handleChange}/>
         </div>
-        {hasFocusIcon && !isEditing &&
+        {hasFocusIcon && !isEditing && this.props.tag.mixed &&
           <IconPlusCircles/>}
       </li>
     )
