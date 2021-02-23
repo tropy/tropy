3694f08da9fe4d4d658ce879bc4dbc37971295da
Sylvester Keil <sylvester@keil.or.at>
2021-02-23 14:57:20 +0100

    Fix typo

--- src/window.js
+++ src/window.js
@@ -55,7 +55,7 @@ export class Window extends EventEmitter {
       'scrollbars',
       'theme',
       'dark',
-      'constrast',
+      'contrast',
       'maximizable',
       'minimizable',
       'zoom'
