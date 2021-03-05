268322e7e08701f43883116e16e05797dab83e40
Sylvester Keil <sylvester@keil.or.at>
2021-03-05 14:47:40 +0100

    Disable spellchecker by default

--- src/browser/wm.js
+++ src/browser/wm.js
@@ -44,6 +44,7 @@ export class WindowManager extends EventEmitter {
         enableRemoteModule: false,
         nodeIntegration: true,
         preload: join(paths.lib, 'bootstrap.js'),
+        spellcheck: false,
         v8CacheOptions: 'bypassHeatCheck',
         ...defaults.webPreferences
       }
