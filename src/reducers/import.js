de34a2f2a30fee7cc728bc56f381ebf91a3ac0ed
Sylvester Keil <sylvester@keil.or.at>
2021-04-23 12:02:44 +0200

    Merge last import sets if they conclude in rapid succession

--- src/reducers/import.js
+++ src/reducers/import.js
@@ -1,14 +1,24 @@
 import { IMPORTS, ITEM } from '../constants'
 
+const THRESHOLD = 1000 * 60 * 2
+
+function merge(state, items, time = Date.now()) {
+  let lastImportTime = state[0]?.time
+
+  if ((time - lastImportTime) < THRESHOLD)
+    return [{ time, items: [...state[0].items, ...items] }]
+  else
+    return [{ time, items }]
+}
+
 export function imports(state = [], { type, payload, error, meta }) {
   switch (type) {
     case IMPORTS.RESTORE:
       return (payload != null) ? [...payload] : []
     case ITEM.IMPORT:
-      return (!meta.done || error || !payload.length) ? state : [{
-        time: Date.now(),
-        items: payload
-      }]
+      return (!meta.done || error || !payload.length) ?
+        state :
+        merge(state, payload)
     default:
       return state
   }
