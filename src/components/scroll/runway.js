be69b7303115174bad7ac7a2c3db4ce4ad99f14c
Sylvester Keil <sylvester@keil.or.at>
2021-07-26 11:59:41 +0200

    Do not set min-width on runway in js
    Disregard scrollbar width for table min-width

--- src/components/scroll/runway.js
+++ src/components/scroll/runway.js
@@ -2,8 +2,8 @@ import React from 'react'
 import cx from 'classnames'
 import { element, number, object, oneOfType, string } from 'prop-types'
 
-export const Runway = ({ children, className, height, width }) => (
-  <div className={cx('runway', className)} style={{ height, minWidth: width }}>
+export const Runway = ({ children, className, height }) => (
+  <div className={cx('runway', className)} style={{ height }}>
     {children}
   </div>
 )
@@ -11,6 +11,5 @@ export const Runway = ({ children, className, height, width }) => (
 Runway.propTypes = {
   children: element,
   className: oneOfType(string, object),
-  width: number,
   height: number.isRequired
 }
