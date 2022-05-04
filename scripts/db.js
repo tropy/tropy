61a9c0bee3cf5d56c6cb0f302043ed69314e76da
Sylvester Keil <sylvester@keil.or.at>
2022-05-04 15:30:06 +0200

    Fix db script

--- scripts/db.js
+++ scripts/db.js
@@ -4,11 +4,9 @@ require('shelljs/make')
 
 const BABEL_CONFIG = {
   presets: [
-    '@babel/preset-react'
+    ['@babel/preset-react', { runtime: 'automatic' }]
   ],
   plugins: [
-    '@babel/plugin-syntax-class-properties',
-    '@babel/plugin-proposal-export-namespace-from',
     'babel-plugin-dynamic-import-node',
     '@babel/plugin-transform-modules-commonjs'
   ]
