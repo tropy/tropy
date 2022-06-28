ec3e12af6620684a00e416872af179524bb1e372
Sylvester Keil <sylvester@keil.or.at>
2022-06-28 18:25:11 +0200

    Remove Bluebird

--- test/support/mode.js
+++ test/support/mode.js
@@ -1,7 +1,6 @@
 import process from 'node:process'
 
 process.env.NODE_ENV = 'test'
-process.env.BLUEBIRD_DEBUG = 'true'
 
 if (process.type === 'renderer') {
   window.__REACT_DEVTOOLS_GLOBAL_HOOK__ = { isDisabled: true }
