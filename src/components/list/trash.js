27bf1a9c7f17b0030c6e86050e697f2d1731bda0
Sylvester Keil <sylvester@keil.or.at>
2022-07-27 14:35:11 +0200

    Use DND hook API

--- src/components/list/trash.js
+++ src/components/list/trash.js
@@ -1,68 +1,56 @@
 import React from 'react'
-import cx from 'classnames'
-import { IconTrash } from '../icons'
 import { FormattedMessage } from 'react-intl'
-import { DND, DropTarget } from '../dnd'
+import cx from 'classnames'
 import { bool, func } from 'prop-types'
+import { IconTrash } from '../icons.js'
+import { DND, useDrop } from '../dnd.js'
 
 
-class TrashListNode extends React.PureComponent {
-  get classes() {
-    return {
-      active: this.props.isSelected,
-      over: this.props.isOver
-    }
-  }
+export const TrashListNode = React.memo(({
+  isSelected,
+  onClick,
+  onContextMenu,
+  onDropItems
+}) => {
 
-  handleContextMenu = (event) => {
-    this.props.onContextMenu(event, 'trash', {})
-  }
+  let [{ isOver }, drop] = useDrop({
+    accept: [DND.ITEMS],
 
-  render() {
-    const { dt, isSelected, onClick } = this.props
+    drop(item) {
+      onDropItems(item.items)
+    },
 
-    return dt(
-      <li
-        className={cx(this.classes)}
-        onContextMenu={this.handleContextMenu}
-        onClick={isSelected ? null : onClick}>
-        <div className="list-node-container">
-          <IconTrash/>
-          <div className="name">
-            <div className="truncate">
-              <FormattedMessage id="sidebar.trash"/>
-            </div>
+    collect: (monitor) => ({
+      isOver: monitor.isOver()
+    })
+  })
+
+  return (
+    <li
+      ref={drop}
+      className={cx({
+        active: isSelected,
+        over: isOver
+      })}
+      onClick={onClick}
+      onContextMenu={(event) => {
+        onContextMenu(event, 'trash', {})
+      }}>
+      <div className="list-node-container">
+        <IconTrash/>
+        <div className="name">
+          <div className="truncate">
+            <FormattedMessage id="sidebar.trash"/>
           </div>
         </div>
-      </li>
-    )
-  }
-
-  static propTypes = {
-    isOver: bool,
-    isSelected: bool,
-    dt: func.isRequired,
-    onClick: func.isRequired,
-    onContextMenu: func.isRequired,
-    onDropItems: func.isRequired
-  }
-}
-
-const spec = {
-  drop({ onDropItems }, monitor) {
-    onDropItems(monitor.getItem().items)
-  }
-}
-
-const collect = (connect, monitor) => ({
-  dt: connect.dropTarget(),
-  isOver: monitor.isOver()
+      </div>
+    </li>
+  )
 })
 
-
-const TrashListNodeContainer =
-  DropTarget(DND.ITEMS, spec, collect)(TrashListNode)
-
-export {
-  TrashListNodeContainer as TrashListNode
+TrashListNode.propTypes = {
+  isSelected: bool,
+  onClick: func.isRequired,
+  onContextMenu: func.isRequired,
+  onDropItems: func.isRequired
 }
