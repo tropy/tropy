e755cf8b4b664e72922c32fee5ebf78cd66aac9d
Sylvester Keil <sylvester@keil.or.at>
2022-07-29 18:51:06 +0200

    Update tests to use new mkdbtmp helper

--- test/models/tag_test.js
+++ test/models/tag_test.js
@@ -1,242 +1,235 @@
-import { mkdbtmp } from '../support/db'
+import { mkdbtmp } from '../support/db.js'
+import * as mod from '../../src/models/index.js'
+import { delay } from '../../src/common/util.js'
 
-import { project as projectModel, item as itemModel, tag as tagModel } from '../../src/models'
 
+describe('tag model', () => {
+  let db = mkdbtmp('test.tpy', mod.project.create, { name: 'Test Project' })
 
-describe('models', async () => {
-  describe('tag', () => {
-    let db
+  describe('create', () => {
+    it('creates a new tag', async () => {
+      await mod.tag.create(db.current, { name: 'testTag' })
 
-    mkdbtmp(x => db = x,
-      'db_test.sqlite', projectModel.create, { name: 'Test Project' })
-    // required to set up database schema
-    // TODO is there a better way to do this?
+      await expect(db.current.empty()).to.eventually.be.false
 
-    describe('create', () => {
-      it('creates a new tag', async () => {
-        await tagModel.create(db, { name: 'testTag' })
+      await expect(db.current.all('select * from tags'))
+        .to.eventually.have.lengthOf(1)
+    })
 
-        await expect(db.empty()).to.eventually.be.false
+    it('creates a tag with color null by default', () =>
+      expect(mod.tag.create(db.current, { name: 'test' }))
+        .to.eventually.have.property('color', null))
 
-        await expect(db.all('select * from tags'))
-          .to.eventually.have.lengthOf(1)
+    it('creates a tag with color if specified', () =>
+      expect(
+        mod.tag.create(db.current, { name: 'test', color: 'blue' })
+      ).to.eventually.have.property('color', 'blue'))
+
+    it('throws if try to create tag with duplicate name', async () => {
+      let name = 'duplicate'
+
+      await expect(mod.tag.create(db.current, { name }))
+        .to.eventually.be.fulfilled
+
+      await expect(mod.tag.create(db.current, { name }))
+        .to.eventually.be.rejectedWith('UNIQUE constraint failed')
+    })
+  })
+
+  describe('load', async () => {
+    it('returns the tag object if found', async () => {
+      let name = 'testTag1'
+      let { id } = await mod.tag.create(db.current, { name })
+
+      let tag = (await mod.tag.load(db.current, [id]))[id]
+
+      expect(tag)
+        .to.include({ name, color: null })
+        .and.have.a.property('created')
+        .that.matches(/^[0-9:\s-]+$/)
+
+      expect(tag).to.have.a.property('modified')
+        .that.does.equal(tag.created)
+    })
+
+    it('returns multiple tags if passed multiple IDs', async () => {
+      let tag1 = await mod.tag.create(db.current, {
+        name: 'tag1'
       })
 
-      it('creates a tag with color null by default', async () => {
-        const { color } = await tagModel.create(db, { name: 'testTag' })
-        expect(color).to.be.null
+      let tag2 = await mod.tag.create(db.current, {
+        name: 'tag2',
+        color: 'green'
       })
 
-      it('creates a tag with color if specified', async () => {
-        const { color } = await tagModel.create(
-          db,
-          { name: 'testTag', color: 'blue' }
-        )
-        expect(color).not.to.be.null
-      })
+      let tags = await mod.tag.load(db.current, [tag1.id, tag2.id])
 
-      it('throws if try to create tag with duplicate name', async () => {
-        let threw = false
-        let errorMessage = ''
-        const tagName = 'testDuplicate'
-        await tagModel.create(db, { name: tagName })
+      expect(Object.keys(tags)).to.have.length(2)
+      expect(tags[tag1.id]).to.deep.equal(tag1)
+      expect(tags[tag2.id]).to.deep.equal(tag2)
+    })
 
-        try {
-          await tagModel.create(db, { name: tagName })
-        } catch (e) {
-          errorMessage = e.message
-          threw = true
-        }
+    it('returns empty object if tag with id not found', () =>
+      expect(mod.tag.load(db.current, [1000])).to.eventually.be.empty)
 
-        expect(threw).to.be.true
-        expect(errorMessage).to.contain(
-          'SQLITE_CONSTRAINT: UNIQUE constraint failed'
-        )
+    it('returns only data for found tags', async () => {
+      let tag1 = await mod.tag.create(db.current, { name: 'tag1' })
+      let tags = await mod.tag.load(db.current, [1, 1200])
 
+      expect(tags[tag1.id]).to.deep.equal(tag1)
+      expect(tags[1200]).to.be.undefined
+    })
+  })
+
+  describe('save', async () => {
+    let original
+
+    beforeEach(async () => {
+      original = await mod.tag.create(db.current, {
+        name: 'test', color: 'red'
       })
     })
 
-    describe('load', async () => {
-      it('returns the tag object if found', async () => {
-        const tagName = 'testTag1'
-        const { id } = await tagModel.create(db, { name: tagName })
+    it('updates data and modified timestamp', async () => {
+      let id = original.id
+      await delay(1000) // delay 1 sec to ensure modified timestamp differs!
+      await mod.tag.save(db.current, { id, color: 'blue' })
+      let tag = (await mod.tag.load(db.current, [id]))[id]
 
-        const tag = (await tagModel.load(db, [id]))[id]
+      expect(tag.name).to.equal(original.name)
+      expect(tag.color).not.to.equal(original.color)
+      expect(tag.created).not.to.equal(tag.modified)
+      expect(tag.modified).not.to.equal(original.modified)
+    })
+  })
 
-        expect(tag)
-          .to.include({ name: tagName, color: null })
-          .and.have.a.property('created')
-          .that.does.match(
-            /^[0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2}$/
-          ) // YYYY-MM-DD HH:MM:SS
-        expect(tag)
-          .to.have.a.property('modified').that.does.equal(tag.created)
-      })
-      it('returns multiple tags if passed multiple IDs', async () => {
-        const tag1 = await tagModel.create(db, { name: 'tag1' })
-        const tag2 = await tagModel.create(db, { name: 'tag2', color: 'green' })
+  describe('delete', async () => {
 
-        const tags = await tagModel.load(db, [tag1.id, tag2.id])
+    it('deletes a single tag', async () => {
+      let { id } = await mod.tag.create(db.current, { name: 'tag' })
 
-        expect(Object.keys(tags)).to.have.length(2)
-        expect(tags[tag1.id]).to.deep.equal(tag1)
-        expect(tags[tag2.id]).to.deep.equal(tag2)
-      })
+      await expect(db.current.get('select count(*) as count from tags'))
+        .to.eventually.have.property('count', 1)
 
-      it('returns empty object if tag with id not found', async () => {
-        const tags = await tagModel.load(db, [1000])
+      await mod.tag.delete(db.current, [id])
 
-        expect(tags).to.be.empty
-      })
-
-      it('returns only data for found tags', async () => {
-        const tag1 = await tagModel.create(db, { name: 'tag1' })
-
-        const tags = await tagModel.load(db, [1, 1200])
-
-        expect(tags[tag1.id]).to.deep.equal(tag1)
-        expect(tags[1200]).to.be.undefined
-      })
+      await expect(db.current.get('select count(*) as count from tags'))
+        .to.eventually.have.property('count', 0)
     })
 
-    describe('save', async () => {
+    it('deletes multiple tags', async () => {
+      let tag1 = await mod.tag.create(db.current, { name: 'tag1' })
+      let tag2 = await mod.tag.create(db.current, { name: 'tag2' })
 
-      it('updates data', async () => {
-        const tagName = 'testTag1'
-        const { id, color } = await tagModel.create(
-          db,
-          { name: tagName, color: 'red' }
-        )
+      await expect(db.current.get('select count(*) as count from tags'))
+        .to.eventually.have.property('count', 2)
 
-        await tagModel.save(db, { id, color: 'blue' })
+      await mod.tag.delete(db.current, [tag1.id, tag2.id])
 
-        const newTag = (await tagModel.load(db, [id]))[id]
-        expect(newTag)
-          .to.include({ name: tagName })
-          .and.to.have.a.property('color').that.does.not.equal(color)
-      })
+      await expect(db.current.get('select count(*) as count from tags'))
+        .to.eventually.have.property('count', 0)
     })
 
-    describe('delete', async () => {
+    it('deletes only specified tags', async () => {
+      let tag1 = await mod.tag.create(db.current, { name: 'tag1' })
+      let tag2 = await mod.tag.create(db.current, { name: 'tag2' })
 
-      it('deletes a single tag', async () => {
-        const { id } = await tagModel.create(db, { name: 'tag' })
-        await expect(db.all('select * from tags'))
-          .to.eventually.have.lengthOf(1)
+      await mod.tag.delete(db.current, [tag1.id])
 
-        await tagModel.delete(db, [id])
+      let remainingTags = await mod.tag.load(db.current, [tag1.id, tag2.id])
 
-        await expect(db.all('select * from tags'))
-          .to.eventually.have.lengthOf(0)
-        const deletedTag = (await tagModel.load(db, [id]))[id]
-        expect(deletedTag).to.be.undefined
-
-      })
-      it('deletes multiple tags', async () => {
-        const tag1 = await tagModel.create(db, { name: 'tag' })
-        const tag2 = await tagModel.create(db, { name: 'tag2' })
-
-        await tagModel.delete(db, [tag1.id, tag2.id])
-
-        await expect(db.all('select * from tags'))
-          .to.eventually.have.lengthOf(0)
-        const deletedTags = await tagModel.load(db, [tag1.id, tag2.id])
-        expect(deletedTags).to.be.empty
-      })
-      it('deletes only specified tags', async () => {
-        const tag1 = await tagModel.create(db, { name: 'tag' })
-        const tag2 = await tagModel.create(db, { name: 'tag2' })
-
-        await tagModel.delete(db, [tag1.id])
-
-        const remainingTags = await tagModel.load(db, [tag1.id, tag2.id])
-        expect(remainingTags[tag2.id]).to.deep.equal(tag2)
-        expect(remainingTags[tag1.id]).to.be.undefined
-      })
-      it.todo('also deletes taggings for a deleted tag?')
+      expect(remainingTags[tag2.id]).to.deep.equal(tag2)
+      expect(remainingTags[tag1.id]).to.be.undefined
     })
 
-    describe('merge', async () => {
-      const [tag1id, tag2id, tag3id] = [1, 2, 3]
-      let tag1, tag2, tag3
-      let item1, item2
+    it.todo('also deletes taggings for a deleted tag?')
+  })
 
-      beforeEach(async function () {
-        tag1 = await tagModel.create(
-          db, { name: 'tag1', color: 'red', id: tag1id }
-        )
-        tag2 = await tagModel.create(
-          db, { name: 'tag2', color: 'blue', id: tag2id }
-        )
-        tag3 = await tagModel.create(
-          db, { name: 'tag3', color: 'green', id: tag3id }
-        )
+  describe('merge', async () => {
+    let [tag1id, tag2id, tag3id] = [1, 2, 3]
+    let tag1, tag2, tag3
+    let item1, item2
 
-        item1 = await itemModel.create(db, 'https://tropy.org/v1/templates/generic', {
+    beforeEach(async function () {
+      tag1 = await mod.tag.create(
+        db.current, { name: 'tag1', color: 'red', id: tag1id }
+      )
+      tag2 = await mod.tag.create(
+        db.current, { name: 'tag2', color: 'blue', id: tag2id }
+      )
+      tag3 = await mod.tag.create(
+        db.current, { name: 'tag3', color: 'green', id: tag3id }
+      )
+
+      item1 = await mod.item.create(db.current,
+        'https://tropy.org/v1/templates/generic',
+        {
           'http://purl.org/dc/elements/1.1/title': {
             text: 'item1',
             type: 'http://www.w3.org/2001/XMLSchema#string'
           }
         })
-        item2 = await itemModel.create(db, 'https://tropy.org/v1/templates/generic', {
+      item2 = await mod.item.create(db.current,
+        'https://tropy.org/v1/templates/generic',
+        {
           'http://purl.org/dc/elements/1.1/title': {
             text: 'item2',
             type: 'http://www.w3.org/2001/XMLSchema#string'
           }
         })
-        await itemModel.tags.add(db, { id: [item1.id], tag: tag1.id })
-        await itemModel.tags.add(db, { id: [item2.id], tag: tag2.id })
 
-
-      })
-
-
-      it('merges other tags into first tag', async () => {
-        await tagModel.merge(db, [tag1.id, tag2.id])
-
-        const res = await tagModel.load(db, [tag1.id, tag2.id])
-
-        expect(res)
-          .to.have.property(tag1.id).which.deep.equals(tag1)
-          .to.not.have.property(tag2.id)
-      })
-
-      it('merges all other tags into first tag provided', async () => {
-        await tagModel.merge(db, [tag3.id, tag2.id, tag1.id])
-
-        const res = await tagModel.load(db, [tag1.id, tag2.id, tag3.id])
-
-        expect(res)
-          .to.have.property(tag3.id).which.deep.equals(tag3)
-          .to.not.have.property(tag2.id)
-        expect(res)
-          .to.not.have.property(tag1.id)
-      })
-
-      it('updates taggings to first of merged tags', async () => {
-        await tagModel.merge(db, [tag1.id, tag2.id])
-
-        const taggings = await db.all('select * from taggings')
-
-        expect(taggings[0].tag_id).to.deep.equal(tag1.id)
-        expect(taggings[1].tag_id).to.deep.equal(tag1.id)
-
-      })
-
-      it('succeeds if item has >1 of tags to be merged', async () => {
-        await itemModel.tags.add(db, { id: [item2.id], tag: tag1.id })
-        await itemModel.tags.add(db, { id: [item1.id], tag: tag2.id })
-
-        await tagModel.merge(db, [tag1.id, tag2.id])
-
-        const taggings = await db.all('select * from taggings')
-        expect(taggings[0].tag_id).to.deep.equal(tag1.id)
-        expect(taggings[1].tag_id).to.deep.equal(tag1.id)
-        expect(Object.keys(taggings).length).to.equal(2)
-      })
-      it.todo('maintains unaffected taggings')
+      await mod.item.tags.add(db.current, { id: [item1.id], tag: tag1.id })
+      await mod.item.tags.add(db.current, { id: [item2.id], tag: tag2.id })
     })
-    describe.todo('items')
-  })
-})
 
+
+    it('merges other tags into first tag', async () => {
+      await mod.tag.merge(db.current, [tag1.id, tag2.id])
+
+      let res = await mod.tag.load(db.current, [tag1.id, tag2.id])
+
+      expect(res)
+        .to.have.property(tag1.id).which.deep.equals(tag1)
+        .to.not.have.property(tag2.id)
+    })
+
+    it('merges all other tags into first tag provided', async () => {
+      await mod.tag.merge(db.current, [tag3.id, tag2.id, tag1.id])
+
+      let res = await mod.tag.load(db.current, [tag1.id, tag2.id, tag3.id])
+
+      expect(res)
+        .to.have.property(tag3.id).which.deep.equals(tag3)
+        .to.not.have.property(tag2.id)
+      expect(res)
+        .to.not.have.property(tag1.id)
+    })
+
+    it('updates taggings to first of merged tags', async () => {
+      await mod.tag.merge(db.current, [tag1.id, tag2.id])
+
+      let taggings = await db.current.all('select * from taggings')
+
+      expect(taggings[0].tag_id).to.deep.equal(tag1.id)
+      expect(taggings[1].tag_id).to.deep.equal(tag1.id)
+
+    })
+
+    it('succeeds if item has >1 of tags to be merged', async () => {
+      await mod.item.tags.add(db.current, { id: [item2.id], tag: tag1.id })
+      await mod.item.tags.add(db.current, { id: [item1.id], tag: tag2.id })
+
+      await mod.tag.merge(db.current, [tag1.id, tag2.id])
+
+      let taggings = await db.current.all('select * from taggings')
+      expect(taggings[0].tag_id).to.deep.equal(tag1.id)
+      expect(taggings[1].tag_id).to.deep.equal(tag1.id)
+      expect(Object.keys(taggings).length).to.equal(2)
+    })
+
+    it.todo('maintains unaffected taggings')
+
+  })
+
+  describe.skip('items')
+})
