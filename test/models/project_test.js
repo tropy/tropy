c6290f36ced2416ee22ea67032c390a05ff7a001
Sylvester Keil <sylvester@keil.or.at>
2022-07-29 15:39:47 +0200

    Simplify mkdbtmp helper

--- test/models/project_test.js
+++ test/models/project_test.js
@@ -1,28 +1,29 @@
-import { mkdbtmp } from '../support/db'
-import { project as projectModel } from '../../src/models'
+import { mkdbtmp } from '../support/db.js'
+import projectModel from '../../src/models/project.js'
 
 describe('models', () => {
   describe('project', () => {
-    let db
-
-    mkdbtmp(x => db = x,
-      'a.tpy', projectModel.create, { name: 'Ariadne' })
+    let db = mkdbtmp(':memory:', projectModel.create, { name: 'Ariadne' })
 
     describe('create', () => {
       it('creates a fresh project', async () => {
-        await expect(db.empty()).to.eventually.be.false
 
-        await expect(db.all('select * from project'))
+        await expect(db.current.empty()).to.eventually.be.false
+
+        await expect(db.current.all('select * from project'))
           .to.eventually.have.lengthOf(1)
 
-        await expect(db.version())
+        await expect(db.current.get('select name from project'))
+          .to.eventually.have.property('name', 'Ariadne')
+
+        await expect(db.current.version())
           .to.eventually.to.be.a('number').above(1601010000)
       })
     })
 
     describe('load', () => {
       it('returns the project object', async () => {
-        let project = await projectModel.load(db)
+        let project = await projectModel.load(db.current)
 
         expect(project)
           .to.include({ name: 'Ariadne', items: 0 })
--- test/support/db.js
+++ test/support/db.js
@@ -1,21 +1,40 @@
-import { mktmp } from './tmp'
-import { unlink } from 'fs/promises'
-import { Database } from '../../src/common/db'
+import { unlink } from 'node:fs/promises'
+import { mktmp } from './tmp.js'
+import { Database } from '../../src/common/db.js'
 
-export function mkdbtmp(callback, filename = 'db_test.sqlite', ...args) {
-  let db
-  let file = mktmp(filename)
+// Creates a temporary database.
+//
+// Returns a React-style ref object whose 'current' field will be set
+// to point to the current database connection in a `beforeEach` hook
+// and that will be removed in `afterEach`.
+//
+// If this is called with a `name` other than ':memory:'
+// a temporary file of that name will be created.
+// The file will be removed again by the `afterEach` hook.
+//
+// Use the `init` argument to pass an optional initialization script;
+// the current database connection will be passed to this script
+// along with any additional arguments.
+
+export function mkdbtmp(name = ':memory:', init, ...args) {
+  let db = { current: null }
+  let isInMemory = name === ':memory:'
+  let file = isInMemory ? name : mktmp(name)
 
   beforeEach(async () => {
-    if (args.length > 0)
-      await Database.create(file, ...args)
+    db.current = new Database(file)
 
-    db = new Database(file)
-    return callback(db)
+    if (init)
+      await init(db.current, ...args)
   })
 
   afterEach(async () => {
-    await db.close()
-    await unlink(file)
+    await db.current.close()
+    db.current = null
+
+    if (!isInMemory)
+      await unlink(file)
   })
+
+  return db
 }
