8308604df5a911e07ed836bade4fb92ad451bc0f
Sylvester Keil <sylvester@keil.or.at>
2022-08-12 17:58:33 +0200

    Use and reload project files from store

--- src/components/project/file.js
+++ src/components/project/file.js
@@ -1,75 +1,10 @@
 import { basename } from 'node:path'
 import React from 'react'
-import { arrayOf,  func, number, string } from 'prop-types'
+import { arrayOf,  func, number, shape, string } from 'prop-types'
 import { FormattedMessage } from 'react-intl'
 import { Button } from '../button.js'
 import { IconMaze, IconWarningSm, IconXMedium } from '../icons.js'
 import { RelativeDate } from '../date.js'
-
-
-export const ProjectFileList = ({
-  files,
-  onRemove,
-  onSelect
-}) => (
-  <ol className="project-files">
-    {files.map(path =>
-      <ProjectFile
-        key={path}
-        onClick={onSelect}
-        onRemove={onRemove}
-        path={path}/>
-    )}
-  </ol>
-)
-
-ProjectFileList.propTypes = {
-  files: arrayOf(string).isRequired,
-  onRemove: func,
-  onSelect: func
-}
-
-export const ProjectFile = ({
-  path,
-  onConsolidate,
-  onClick,
-  onRemove
-}) => (
-  <li
-    className="project-file"
-    onClick={() => onClick?.(path)}
-    title={path}>
-    <IconMaze/>
-    <div className="flex-col">
-      <div className="name">
-        <div className="truncate">{basename(path)}</div>
-        {
-          Math.random() > 0.75 && (
-            <Button
-              icon={<IconWarningSm/>}
-              onClick={() => onConsolidate?.(path)}
-              title="project.new.find-missing"/>
-          )
-        }
-      </div>
-      <ProjectFileStats
-        lastModified={Date.now() - Math.random() * 100000}
-        items={Math.round(1167 * Math.random())}
-        photos={Math.round(4959 * Math.random())}
-        notes={Math.round(182 * Math.random())}/>
-    </div>
-    <Button
-      icon={<IconXMedium/>}
-      onClick={() => onRemove?.(path)}/>
-  </li>
-)
-
-ProjectFile.propTypes = {
-  path: string.isRequired,
-  onConsolidate: func,
-  onClick: func,
-  onRemove: func
-}
 
 export const ProjectFileStats = ({ lastModified, ...values }) =>
   (lastModified != null) && (
@@ -94,4 +29,69 @@ ProjectFileStats.propTypes = {
   lastModified: number,
   notes: number,
   photos: number
+}
+
+export const ProjectFileList = ({
+  files,
+  onRemove,
+  onSelect
+}) => (
+  <ol className="project-files">
+    {files.map(({ path, ...stats }) =>
+      <ProjectFile
+        key={path}
+        onClick={onSelect}
+        onRemove={onRemove}
+        path={path}
+        stats={stats}/>
+    )}
+  </ol>
+)
+
+ProjectFileList.propTypes = {
+  files: arrayOf(shape({
+    path: string.isRequired
+  })).isRequired,
+  onRemove: func,
+  onSelect: func
+}
+
+export const ProjectFile = ({
+  path,
+  onConsolidate,
+  onClick,
+  onRemove,
+  stats
+}) => (
+  <li
+    className="project-file"
+    onClick={() => onClick?.(path)}
+    title={path}>
+    <IconMaze/>
+    <div className="flex-col">
+      <div className="name">
+        <div className="truncate">{stats?.name || basename(path)}</div>
+        {
+          !stats?.lastModified && (
+            <Button
+              icon={<IconWarningSm/>}
+              onClick={() => onConsolidate?.(path)}
+              title="project.new.find-missing"/>
+          )
+        }
+      </div>
+      <ProjectFileStats {...stats}/>
+    </div>
+    <Button
+      icon={<IconXMedium/>}
+      onClick={() => onRemove?.(path)}/>
+  </li>
+)
+
+ProjectFile.propTypes = {
+  path: string.isRequired,
+  stats: shape(ProjectFileStats.propsTypes),
+  onConsolidate: func,
+  onClick: func,
+  onRemove: func
 }
