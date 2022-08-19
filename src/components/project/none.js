50019e438b894ec8a98836a16d3795ae0274d0e2
Sylvester Keil <sylvester@keil.or.at>
2022-08-19 14:54:49 +0200

    Explore resizing window for NoProject view

--- src/components/project/none.js
+++ src/components/project/none.js
@@ -1,9 +1,28 @@
+import { useEffect } from 'react'
 import { useArgs } from '../../hooks/use-args.js'
+import { useWindow } from '../../hooks/use-window.js'
 import { NewProject } from './new.js'
 import { RecentProjects } from './recent.js'
 
 export const NoProject = () => {
   let recent = useArgs('recent')
+  let win = useWindow()
+
+  useEffect(() => {
+    let { width, height } = win
+
+    return () => {
+      win.resize(width, height, true)
+    }
+  }, [win])
+
+  useEffect(() => {
+    if (recent.length)
+      win.resize(880, 580, true)
+    else
+      win.resize(440, 580, true)
+
+  }, [win, recent])
 
   return (
     <div className="no-project">
