b3ccc82783908fa810fb59930fd2c06ea2421937
Sylvester Keil <sylvester@keil.or.at>
2022-08-21 13:53:20 +0200

    Fix restoring minimzed windows

--- src/browser/wm.js
+++ src/browser/wm.js
@@ -286,7 +286,7 @@ export class WindowManager extends EventEmitter {
         break
       case 'minimize':
         if (win.isMinimized())
-          win.unminimize()
+          win.restore()
         else
           win.minimize()
         break
