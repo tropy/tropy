613f45441b697a735f1ab08d219a4ced709dbd3d
Sylvester Keil <sylvester@keil.or.at>
2022-08-26 14:16:40 +0200

    Hook up new project form

--- src/components/project/none.js
+++ src/components/project/none.js
@@ -1,23 +1,34 @@
+import { useDispatch } from 'react-redux'
 import { number } from 'prop-types'
 import { useArgs } from '../../hooks/use-args.js'
+import { useEvent } from '../../hooks/use-event.js'
 import { useWindowSize } from '../../hooks/use-window-size.js'
 import { NewProject } from './new.js'
 import { RecentProjects } from './recent.js'
 import { Fade } from '../fx.js'
+import project from '../../actions/project.js'
 
 export const NoProject = ({
   height,
   width
 }) => {
+  let dispatch = useDispatch()
   let recent = useArgs('recent')
 
   useWindowSize(width * (recent.length ? 2 : 1), height)
 
+  let handleProjectOpen = useEvent(path => {
+    dispatch(project.open(path))
+  })
+
   return (
     <Fade appear in>
       <div className="no-project">
-        <RecentProjects files={recent}/>
-        <NewProject/>
+        <RecentProjects
+          files={recent}
+          onSelect={handleProjectOpen}/>
+        <NewProject
+          onCreated={handleProjectOpen}/>
       </div>
     </Fade>
   )
