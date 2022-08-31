28ff2e4b5118254398c2fbe15eb4b22e39cd37ee
Sylvester Keil <sylvester@keil.or.at>
2022-08-31 15:15:10 +0200

    Add usePasteEvent to handle global paste

---
+++ src/hooks/use-paste-event.js
@@ -0,0 +1,22 @@
+
+import { useEventHandler } from './use-event-handler.js'
+import { getClipboardData } from '../clipboard.js'
+import { isInput } from '../dom.js'
+
+export function usePasteEvent(format, handler) {
+  useEventHandler(window, 'paste', (event) => {
+    if (isInput(event))
+      return
+
+    let data = getClipboardData(event, format)
+
+    if (data == null)
+      return
+
+    event.preventDefault()
+    event.stopPropagation()
+
+    handler(data)
+
+  }, false, true)
+}
