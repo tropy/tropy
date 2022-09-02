9bfa6f6b52331eb5fa9c001cdc98d9e2b216fd49
Sylvester Keil <sylvester@keil.or.at>
2022-09-02 19:11:41 +0200

    Clear on edit on save

--- src/reducers/edit.js
+++ src/reducers/edit.js
@@ -1,12 +1,18 @@
-import { EDIT, METADATA } from '../constants'
+import { EDIT, METADATA, PROJECT, TAG } from '../constants'
 
-export function edit(state = {}, { type, payload }) {
+export function edit(state = {}, { type, payload, meta }) {
   switch (type) {
     case EDIT.START:
       return { ...payload }
+
     case METADATA.SAVE:
+    case PROJECT.SAVE:
+    case TAG.CREATE:
+    case TAG.SAVE:
+      return (meta.done) ? {} : state
     case EDIT.CANCEL:
       return {}
+
     default:
       return state
   }
