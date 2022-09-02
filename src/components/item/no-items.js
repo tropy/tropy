05caa231638e79ea84799fae4cc374a4adeaacf4
Sylvester Keil <sylvester@keil.or.at>
2022-09-02 19:20:39 +0200

    Rewrite ProjectView
    WIP

--- src/components/item/no-items.js
+++ src/components/item/no-items.js
@@ -1,26 +1,18 @@
 import React from 'react'
 import cx from 'classnames'
 import { FormattedMessage } from 'react-intl'
-import { bool, func } from 'prop-types'
+import { bool } from 'prop-types'
 
-export const NoItems = ({ connectDropTarget, isOver, isReadOnly }) => {
-  if (isReadOnly || !connectDropTarget)
-    return (
-      <div className="no-items">
-        <figure className="no-items-illustration"/>
-      </div>
-    )
-  else
-    return connectDropTarget(
-      <div className={cx('no-items', { over: isOver })}>
-        <figure className="no-items-illustration"/>
-        <h1><FormattedMessage id="project.empty"/></h1>
-      </div>
-    )
-}
+export const NoItems = React.forwardRef(
+  ({ isOver, isReadOnly }, ref) => (
+    <div ref={ref} className={cx('no-items', { over: isOver })}>
+      <figure className="no-items-illustration"/>
+      {!isReadOnly &&
+        <h1><FormattedMessage id="project.empty"/></h1>}
+    </div>
+  ))
 
 NoItems.propTypes = {
-  connectDropTarget: func,
   isOver: bool,
   isReadOnly: bool
 }
