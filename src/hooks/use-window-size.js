03617ac91a7a00bb26cfab05d80e2ed0c3d70989
Sylvester Keil <sylvester@keil.or.at>
2022-09-06 14:59:13 +0200

    Handle restoring of window size internally

--- src/hooks/use-window-size.js
+++ src/hooks/use-window-size.js
@@ -2,13 +2,15 @@ import { useEffect } from 'react'
 import { useWindow } from './use-window.js'
 
 // Resizes the window to the given dimensions while the component is mounted.
-export function useWindowSize(width, height, delay = 0) {
+export function useWindowSize(width, height) {
   let win = useWindow()
+  let delay = win.isResizeAnimated() ? 150 : 0
 
   useEffect(() => {
     win.setFixedSize(true)
 
     return () => {
+      // Using a timeout because we want this to fire after unmount!
       setTimeout(() => { win?.setFixedSize(false) }, delay)
     }
 
