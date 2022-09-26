31b0d6266a8581a088aef4c7cd06d3c8f7adb9fc
Sylvester Keil <sylvester@keil.or.at>
2022-09-26 18:00:22 +0200

    Remove reducer side-effects
    Use project.lastAccess instead of item count

--- src/reducers/project.js
+++ src/reducers/project.js
@@ -1,15 +1,6 @@
 import { PROJECT, ITEM } from '../constants/index.js'
-import ARGS, { update } from '../args.js'
 
-const init = { name: '', items: 0 }
-
-function dec(state, by = 1) {
-  return { ...state, items: Math.max(0, state.items - by) }
-}
-
-function inc(state, by = 1) {
-  return { ...state, items: state.items + by }
-}
+const init = { name: '' }
 
 // eslint-disable-next-line complexity
 export function project(state = init, { type, payload, meta, error }) {
@@ -21,21 +12,13 @@ export function project(state = init, { type, payload, meta, error }) {
     case PROJECT.OPEN:
       return {
         ...init,
-        file: payload,
+        path: payload,
         isReadOnly: meta.isReadOnly
       }
     case PROJECT.OPENED:
-      // Update window's global ARGS to allow reloading the project!
-      if (payload.file !== ARGS.file) {
-        update({ file: payload.file })
-      }
       return { ...payload }
 
     case PROJECT.CLOSE:
-      // Clear project file if project was closed by user.
-      if (payload === 'user') {
-        update({ file: null })
-      }
       return {
         ...state,
         closedBy: payload,
@@ -50,17 +33,9 @@ export function project(state = init, { type, payload, meta, error }) {
       }
 
     case ITEM.INSERT:
-      return inc(state)
-    case ITEM.RESTORE:
-      return (!error && meta.done) ? inc(state, payload.length) : state
-    case ITEM.DELETE:
-      return (!error && meta.done) ? dec(state, payload.length) : state
-    case ITEM.MERGE:
-    case ITEM.IMPLODE:
-      return (!error && meta.done) ? dec(state, meta.dec) : state
-    case ITEM.EXPLODE:
-    case ITEM.SPLIT:
-      return (!error && meta.done) ? inc(state, meta.inc) : state
+      return (state.lastAccess == null) ?
+        state.lastAccess = Date.now() : state
+
     default:
       return state
   }
