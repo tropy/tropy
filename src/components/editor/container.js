d6e16194da945bccb0a4bfb034a449a906d75c1c
Sylvester Keil <sylvester@keil.or.at>
2022-11-16 16:31:59 +0100

    Make Editor functional component

--- src/components/editor/container.js
+++ src/components/editor/container.js
@@ -1,139 +1,137 @@
-import React from 'react'
+import { useMemo, useRef, useState } from 'react'
 import cx from 'classnames'
 import { func, bool, object, number, string } from 'prop-types'
 import { EditorToolbar } from './toolbar.js'
 import { EditorView } from './view.js'
+import { useEvent } from '../../hooks/use-event.js'
 import { commands, toEditorState } from '../../editor/index.js'
 import { match } from '../../keymap.js'
 
 
-export class Editor extends React.Component {
-  toolbar = React.createRef()
-  container = React.createRef()
-  view = React.createRef()
+export function Editor({
+  hasTitlebar,
+  isDisabled,
+  isReadOnly,
+  keymap,
+  mode,
+  numbers,
+  onBlur,
+  onChange,
+  onContextMenu,
+  placeholder,
+  state,
+  tabIndex,
+  wrap
+}) {
+  let container = useRef()
+  let toolbar = useRef()
+  let view = useRef()
 
-  state = {
-    hasViewFocus: false
-  }
+  let [hasViewFocus, setViewFocus] = useState(false)
 
-  get classes() {
-    return ['editor', {
-      'is-blurred': !this.state.hasViewFocus
-    }]
-  }
+  state = useMemo(() => toEditorState(state), [state])
 
-  focus = () => {
-    this.view.current?.focus()
-  }
+  let exec = useEvent((action, ...args) =>
+    commands[action]?.(
+      view.current.state, view.current.dispatch, ...args
+    ))
 
-  exec(action, ...args) {
-    return commands[action]?.(
-      this.view.current.state, this.view.current.dispatch, ...args
-    )
-  }
+  let handleCommand = useEvent((...args) => {
+    exec(...args)
 
-  handleKeyDown = (_, event) => {
-    if (this.props.isDisabled)
+    if (!view.current.hasFocus())
+      view.current.focus()
+  })
+
+  let handleKeyDown = useEvent((_, event) => {
+    if (isDisabled)
       return false
 
-    const action = match(this.props.keymap, event)
+    let action = match(keymap, event)
 
     switch (action) {
       case null:
         return
       case 'addLink':
-        if (this.toolbar.current) {
-          this.toolbar.current.handleLinkButtonClick()
-        }
+        toolbar.current.handleLinkButtonClick()
         break
       case 'lift':
-        if (this.exec('liftListItem')) break
+        if (exec('liftListItem')) break
         // eslint-disable-next-line no-fallthrough
       default:
-        if (!this.exec(action)) return
+        if (!exec(action)) return
     }
 
     event.stopPropagation()
     return true
-  }
+  })
 
-  handleFocus = (event) => {
-    if (event.target === this.container.current) {
-      this.focus()
+  let handleContainerFocus = useEvent((event) => {
+    if (event.target === container.current) {
+      view.current.focus()
     }
-  }
+  })
 
-  handleCommand = (...args) => {
-    if (this.exec(...args)) {
-      if (!this.state.hasViewFocus) this.focus()
-    }
-  }
+  let handleViewFocus = useEvent(() => {
+    setViewFocus(true)
+  })
 
-  handleViewFocus = () => {
-    this.setState({ hasViewFocus: true })
-  }
+  let handleViewBlur = useEvent((event) => {
+    setViewFocus(false)
+    onBlur?.(event)
+  })
 
-  handleViewBlur = () => {
-    this.setState({ hasViewFocus: false })
-    this.props.onBlur()
-  }
+  return (
+    <div
+      ref={container}
+      className={cx('editor', { 'is-blurred': !hasViewFocus })}
+      tabIndex={isDisabled ? -1 : tabIndex}
+      onContextMenu={onContextMenu}
+      onFocus={handleContainerFocus}>
+      <EditorToolbar
+        isDisabled={isDisabled}
+        isReadOnly={isReadOnly}
+        isTitlebar={hasTitlebar}
+        state={state}
+        ref={toolbar}
+        onCommand={handleCommand}/>
+      <EditorView
+        ref={view}
+        state={state}
+        isDisabled={isDisabled}
+        isReadOnly={isReadOnly}
+        mode={mode}
+        numbers={numbers}
+        placeholder={placeholder}
+        wrap={wrap}
+        onFocus={handleViewFocus}
+        onBlur={handleViewBlur}
+        onChange={onChange}
+        onContextMenu={onContextMenu}
+        onKeyDown={handleKeyDown}/>
+    </div>
+  )
+}
 
-  render() {
-    let state = toEditorState(this.props.state)
-    let tabIndex = this.props.isDisabled ? -1 : this.props.tabIndex
+Editor.propTypes = {
+  hasTitlebar: bool,
+  isDisabled: bool,
+  isReadOnly: bool,
+  keymap: object.isRequired,
+  mode: string.isRequired,
+  numbers: bool,
+  onBlur: func.isRequired,
+  onChange: func.isRequired,
+  onContextMenu: func,
+  placeholder: string,
+  state: object,
+  tabIndex: number.isRequired,
+  wrap: bool
+}
 
-    return (
-      <div
-        ref={this.container}
-        className={cx(this.classes)}
-        tabIndex={tabIndex}
-        onContextMenu={this.props.onContextMenu}
-        onFocus={this.handleFocus}>
-        <EditorToolbar
-          isDisabled={this.props.isDisabled}
-          isReadOnly={this.props.isReadOnly}
-          isTitlebar={this.props.hasTitlebar}
-          state={state}
-          ref={this.toolbar}
-          onCommand={this.handleCommand}/>
-        <EditorView
-          ref={this.view}
-          state={state}
-          isDisabled={this.props.isDisabled}
-          isReadOnly={this.props.isReadOnly}
-          mode={this.props.mode}
-          numbers={this.props.numbers}
-          placeholder={this.props.placeholder}
-          wrap={this.props.wrap}
-          onFocus={this.handleViewFocus}
-          onBlur={this.handleViewBlur}
-          onChange={this.props.onChange}
-          onContextMenu={this.props.onContextMenu}
-          onKeyDown={this.handleKeyDown}/>
-      </div>
-    )
-  }
-
-  static propTypes = {
-    hasTitlebar: bool,
-    isDisabled: bool,
-    isReadOnly: bool,
-    keymap: object.isRequired,
-    mode: string.isRequired,
-    numbers: bool,
-    onBlur: func.isRequired,
-    onChange: func.isRequired,
-    onContextMenu: func,
-    placeholder: string,
-    state: object,
-    tabIndex: number.isRequired,
-    wrap: bool
-  }
-
-  static defaultProps = {
-    mode: 'horizontal',
-    numbers: false,
-    tabIndex: -1,
-    wrap: true
-  }
+Editor.defaultProps = {
+  mode: 'horizontal',
+  numbers: false,
+  tabIndex: -1,
+  wrap: true
 }
