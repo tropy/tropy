1706b8b4f7af44fb907349f068563438360d4cb9
Sylvester Keil <sylvester@keil.or.at>
2022-12-24 13:00:52 +0100

    Re-implement note deletion

--- src/selectors/notes.js
+++ src/selectors/notes.js
@@ -1,6 +1,6 @@
 import { createSelector as memo } from 'reselect'
 import { seq, compose, cat, map, keep } from 'transducers.js'
-import { getVisiblePhotos, getSelectedPhoto } from './photos'
+import { getVisiblePhotos } from './photos'
 import { getVisibleSelections } from './selections'
 
 const getNotes = ({ notes }) => notes
@@ -8,21 +8,6 @@ const getSelectedNoteId = ({ nav }) => nav.note
 
 export const getSelectedNote = memo(
   getNotes, getSelectedNoteId, (notes, id) => notes[id]
-)
-
-export const getSelectableNoteId = memo(
-  getSelectedPhoto, getSelectedNoteId, (photo, id) => {
-    if (!photo) return null
-    if (!photo.notes.length) return null
-    if (!id) return photo.notes[0]
-
-    const idx = photo.notes.indexOf(id)
-
-    if (idx < 0) return photo.notes[0]
-    if (idx > 0) return photo.notes[idx - 1]
-
-    return photo.notes[idx + 1]
-  }
 )
 
 export const getVisibleNotes = memo(
@@ -40,3 +25,51 @@ export const getVisibleNotes = memo(
       keep()
     ))
 )
+
+export const getNextNoteSelection = memo(
+  getSelectedNoteId,
+  getVisibleNotes,
+  (id, notes) => {
+    let idx = notes.findIndex(note => note.id === id)
+
+    if (idx === -1 || notes.length <= 1)
+      return null
+
+    let prev = notes[idx - 1]
+    let cursor = notes[idx]
+    let next = notes[idx + 1]
+
+    if (prev == null)
+      return next
+    if (next == null)
+      return prev
+
+    if (next.photo === cursor.photo && next.selection === cursor.selection)
+      return next
+    if (prev.photo === cursor.photo && prev.selection === prev.selection)
+      return prev
+
+    return next
+  }
+)
+
+
+export const getNotesMap = (state, props) =>
+  props.notes.reduce((map, noteId) => {
+    let { photo, selection } = state.notes[noteId]
+
+    let id = (selection != null) ? selection : photo
+    let type = (selection != null) ? 'selection' : 'photo'
+
+    if (map.has(id))
+      map.get(id).notes.push(noteId)
+    else
+      map.set(id, {
+        id,
+        type,
+        notes: [noteId]
+      })
+
+    return map
+
+  }, new Map)
