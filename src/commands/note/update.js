17382956e2527852136b0ee810f083135ca2c492
Sylvester Keil <sylvester@keil.or.at>
2023-01-18 17:12:41 +0100

    Improve undo/redo of newly created notes

--- src/commands/note/update.js
+++ src/commands/note/update.js
@@ -4,17 +4,28 @@ import NOTE from '../../constants/note.js'
 import { pick } from '../../common/util.js'
 import actions from '../../actions/note.js'
 
+const wasPreviouslyCreated = (id, past) =>
+  past?.undo.type === NOTE.DELETE &&
+    past.undo.payload?.[0] === id &&
+    past.undo.payload.length === 1
 
 class Update extends Command {
   *exec() {
     let note = this.action.payload
 
-    let original = yield select(({ notes }) => (
-      pick(notes[note.id], Object.keys(note))
-    ))
+    let [original, past] = yield select(({ notes, history }) => ([
+      pick(notes[note.id], Object.keys(note)),
+      history.past[0]
+    ]))
 
-    this.undo = actions.update(original)
-    this.redo = actions.update(note)
+    // Subtle: when a note is updated immediately after creation,
+    // we do not add updates to the history stack.
+    // This way, `undo` after the initial session will remove the note,
+    // skipping the initial state (typically a single character).
+    if (!wasPreviouslyCreated(note.id, past)) {
+      this.undo = actions.update(original)
+      this.redo = actions.update(note)
+    }
 
     return note
   }
