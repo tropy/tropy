271609ac4afc7d2d03c5629be22e2687a5b11bdc
Sylvester Keil <sylvester@keil.or.at>
2023-04-25 10:00:53 +0200

    Add useDropText

---
+++ src/hooks/use-drop-text.js
@@ -0,0 +1,16 @@
+import { useDrop } from 'react-dnd'
+import { NativeTypes } from 'react-dnd-html5-backend'
+
+export function useDropText({ onDrop }) {
+  return useDrop(() => ({
+    accept: [NativeTypes.TEXT, NativeTypes.URL],
+
+    drop(item) {
+      onDrop({ text: item.text || item.urls[0] })
+    },
+
+    collect: (monitor) => ({
+      isOver: monitor.isOver()
+    })
+  }), [onDrop])
+}
