4960a8df629126a0a60550872d3a772088505215
Sylvester Keil <sylvester@keil.or.at>
2023-06-26 17:18:37 +0200

    Add useWindowArgs hook

--- src/hooks/use-window.js
+++ src/hooks/use-window.js
@@ -1,6 +1,19 @@
-import { useContext } from 'react'
+import { useContext, useState } from 'react'
 import { WindowContext } from '../components/window.js'
+import { useEventHandler } from './use-event-handler.js'
+import { clone } from '../args.js'
 
 export function useWindow() {
   return useContext(WindowContext)
+}
+
+export function useWindowArgs() {
+  let win = useWindow()
+  let [args, setArgs] = useState(clone)
+
+  useEventHandler(win, 'settings.update', () => {
+    setArgs(clone())
+  })
+
+  return args
 }
