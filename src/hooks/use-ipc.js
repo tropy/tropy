b5d18c27203be4daff87e17f89ebd05129941eaf
Sylvester Keil <sylvester@keil.or.at>
2023-06-26 17:18:57 +0200

    Add useIpc

--- src/hooks/use-ipc.js
+++ src/hooks/use-ipc.js
@@ -1,6 +1,10 @@
 import { useCallback } from 'react'
 import { ipcRenderer } from 'electron'
 
+export function useIpc() {
+  return ipcRenderer
+}
+
 export function useIpcSend(dependencies = []) {
   return useCallback((...args) => {
     ipcRenderer.send(...dependencies, ...args)
