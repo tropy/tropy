6e76a5066b9c942fbd7fe7cdb689c9b3f94c34f2
Sylvester Keil <sylvester@keil.or.at>
2023-07-06 13:45:40 +0200

    Add EXIF windows properties

---
+++ db/migrate/ontology/2307061337.add-exif-win-props.sql
@@ -0,0 +1,11 @@
+INSERT INTO properties VALUES('http://www.w3.org/2003/12/exif/ns#xPAuthor','http://www.w3.org/2003/12/exif/ns#',NULL,NULL,NULL,NULL,'Author tag used by Windows');
+INSERT INTO properties VALUES('http://www.w3.org/2003/12/exif/ns#xPComment','http://www.w3.org/2003/12/exif/ns#',NULL,NULL,NULL,NULL,'Comment tag used by Windows');
+INSERT INTO properties VALUES('http://www.w3.org/2003/12/exif/ns#xPKeywords','http://www.w3.org/2003/12/exif/ns#',NULL,NULL,NULL,NULL,'Keywords tag used by Windows');
+INSERT INTO properties VALUES('http://www.w3.org/2003/12/exif/ns#xPSubject','http://www.w3.org/2003/12/exif/ns#',NULL,NULL,NULL,NULL,'Subject tag used by Windows');
+INSERT INTO properties VALUES('http://www.w3.org/2003/12/exif/ns#xPTitle','http://www.w3.org/2003/12/exif/ns#',NULL,NULL,NULL,NULL,'Title tag used by Windows');
+
+INSERT INTO labels VALUES('http://www.w3.org/2003/12/exif/ns#xPAuthor','en','Author');
+INSERT INTO labels VALUES('http://www.w3.org/2003/12/exif/ns#xPComment','en','Comment');
+INSERT INTO labels VALUES('http://www.w3.org/2003/12/exif/ns#xPKeywords','en','Keywords');
+INSERT INTO labels VALUES('http://www.w3.org/2003/12/exif/ns#xPSubject','en','Subject');
+INSERT INTO labels VALUES('http://www.w3.org/2003/12/exif/ns#xPTitle','en','Title');
