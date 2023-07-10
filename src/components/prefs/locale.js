77b291b8e2c714a51bd5808b69e00bdceed4ad42
Sylvester Keil <sylvester@keil.or.at>
2023-07-10 21:11:31 +0200

    Add locale settings component

---
+++ src/components/prefs/locale.js
@@ -0,0 +1,45 @@
+import { arrayOf, string } from 'prop-types'
+import { FormSelect } from '../form.js'
+import { useIpcEvent } from '../../hooks/use-ipc.js'
+import { useWindowArgs } from '../../hooks/use-window.js'
+
+export function LocaleSettings({ options }) {
+  let { locale } = useWindowArgs()
+
+  let handleLocaleChange = useIpcEvent(event => (
+    event.locale
+  ), ['cmd', 'app:switch-locale'])
+
+  return (
+    <>
+      <FormSelect
+        id="prefs.app.locale.locale"
+        name="locale"
+        isRequired
+        isSelectionHidden
+        value={locale}
+        options={options}
+        tabIndex={0}
+        onChange={handleLocaleChange}/>
+    </>
+  )
+}
+
+LocaleSettings.propTypes = {
+  options: arrayOf(string).isRequired
+}
+
+LocaleSettings.defaultProps = {
+  options: [
+    'cn',
+    'de',
+    'en',
+    'es',
+    'fr',
+    'it',
+    'ja',
+    'pt',
+    'pt-BR',
+    'uk'
+  ]
+}
