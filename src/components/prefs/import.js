1238c4202b755bc31e8aef112aa24a785b485b28
Sylvester Keil <sylvester@keil.or.at>
2023-07-11 18:53:33 +0200

    Add import settings component

---
+++ src/components/prefs/import.js
@@ -0,0 +1,45 @@
+import { func, object } from 'prop-types'
+import { TitleSettings } from './title.js'
+import { FormField, FormToggle, FormToggleGroup } from '../form.js'
+import { IMAGE } from '../../constants/index.js'
+
+const DUP_OPTS = ['skip', 'import', 'prompt']
+
+export function ImportSettings({ config, onChange }) {
+  return (
+    <>
+      <FormToggleGroup
+        id="prefs.app.dup"
+        name="dup"
+        isCompact
+        value={config.dup}
+        options={DUP_OPTS}
+        onChange={onChange}/>
+      <FormToggle
+        id="prefs.app.localtime"
+        name="localtime"
+        value={config.localtime}
+        onChange={onChange}/>
+      <TitleSettings
+        config={config.title}
+        onChange={onChange}/>
+      <FormField
+        id="prefs.app.density.label"
+        isCompact
+        isRequired
+        max={IMAGE.MAX_DENSITY}
+        min={IMAGE.MIN_DENSITY}
+        name="density"
+        onChange={onChange}
+        title="prefs.app.density.title"
+        tabIndex={0}
+        type="number"
+        value={config.density}/>
+    </>
+  )
+}
+
+ImportSettings.propTypes = {
+  config: object.isRequired,
+  onChange: func.isRequired
+}
