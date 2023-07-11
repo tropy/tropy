e752739b7a8685beda763a9dea56edb874603cfe
Sylvester Keil <sylvester@keil.or.at>
2023-07-11 18:56:46 +0200

    Use new settings components

--- src/components/prefs/app.js
+++ src/components/prefs/app.js
@@ -1,166 +1,33 @@
-import React, { } from 'react'
-import { ipcRenderer as ipc } from 'electron'
-import ARGS from '../../args.js'
+import React from 'react'
+import { Form } from '../form.js'
+import { DeveloperSettings } from './dev.js'
 import { ExportSettings } from './export.js'
 import { PrintSettings } from './print.js'
 import { InterfaceSettings } from './interface.js'
+import { ImportSettings } from './import.js'
+import { LocaleSettings } from './locale.js'
 import { StyleSettings } from './style.js'
-import { TemplateSelect } from '../template/select.js'
-import { ResourceSelect } from '../resource/select.js'
+import { TemplateSettings } from './template.js'
 import { ScrollContainer } from '../scroll/container.js'
-
-import {
-  IMAGE
-} from '../../constants/index.js'
-
-import {
-  IconItemSmall,
-  IconPhoto,
-  IconSelection
-} from '../icons.js'
-
-import {
-  array, arrayOf, bool, func, object, shape, string, number
-} from 'prop-types'
-
-import {
-  Form,
-  FormElement,
-  FormField,
-  FormSelect,
-  FormToggle,
-  FormToggleGroup
-} from '../form.js'
+import { func, object, shape, string } from 'prop-types'
 
 
 export class AppPrefs extends React.PureComponent {
-  handleDebugChange() {
-    ipc.send('cmd', 'app:toggle-debug-flag')
-  }
-
-  handleLocaleChange = ({ locale }) => {
-    ipc.send('cmd', 'app:switch-locale', locale)
-  }
-
-  handleLocalTimeChange = ({ localtime }) => {
-    this.props.onSettingsUpdate({ localtime })
-  }
-
-  handleTemplateChange = (values, hasChanged) => {
-    if (hasChanged) {
-      let [type, template] = Object.entries(values)[0]
-      this.props.onSettingsUpdate({
-        templates: {
-          [type]: template.id
-        }
-      })
-    }
-  }
-
-  handleTitleChange = (values, hasChanged) => {
-    if (hasChanged) {
-      let [name, value] = Object.entries(values)[0]
-      if (value && value.id) value = value.id
-      this.props.onSettingsUpdate({
-        title: {
-          [name]: value
-        }
-      })
-    }
-  }
-
   render() {
     return (
       <ScrollContainer>
         <Form>
-          <FormElement id="prefs.app.templates.label">
-            <TemplateSelect
-              icon={<IconItemSmall/>}
-              id="prefs.app.templates.label"
-              isRequired
-              name="item"
-              options={this.props.templates.item}
-              value={this.props.settings.templates.item}
-              tabIndex={0}
-              onChange={this.handleTemplateChange}/>
-            <TemplateSelect
-              icon={<IconPhoto/>}
-              isRequired
-              name="photo"
-              options={this.props.templates.photo}
-              value={this.props.settings.templates.photo}
-              tabIndex={0}
-              onChange={this.handleTemplateChange}/>
-            <TemplateSelect
-              icon={<IconSelection/>}
-              isRequired
-              name="selection"
-              options={this.props.templates.selection}
-              value={this.props.settings.templates.selection}
-              tabIndex={0}
-              onChange={this.handleTemplateChange}/>
-          </FormElement>
-          <hr/>
-          <FormToggleGroup
-            id="prefs.app.dup"
-            name="dup"
-            isCompact
-            value={this.props.settings.dup}
-            options={this.props.dupOptions}
+          <TemplateSettings
+            config={this.props.settings.templates}
             onChange={this.props.onSettingsUpdate}/>
-          <FormToggle
-            id="prefs.app.localtime"
-            name="localtime"
-            value={this.props.settings.localtime}
-            onChange={this.handleLocalTimeChange}/>
-          <FormElement
-            id="prefs.app.title.label"
-            isCompact>
-            <ResourceSelect
-              icon={<IconItemSmall/>}
-              id="prefs.app.title.label"
-              options={this.props.properties}
-              name="item"
-              value={this.props.settings.title.item}
-              tabIndex={0}
-              onChange={this.handleTitleChange}/>
-            <ResourceSelect
-              icon={<IconPhoto/>}
-              options={this.props.properties}
-              name="photo"
-              value={this.props.settings.title.photo}
-              tabIndex={0}
-              onChange={this.handleTitleChange}/>
-          </FormElement>
-          <FormToggle
-            id="prefs.app.title.force"
-            name="force"
-            value={this.props.settings.title.force}
-            onChange={this.handleTitleChange}/>
-          <FormField
-            id="prefs.app.density.label"
-            isCompact
-            isRequired
-            max={this.props.importMax}
-            min={this.props.importMin}
-            name="density"
-            onChange={this.props.onSettingsUpdate}
-            title="prefs.app.density.title"
-            tabIndex={0}
-            type="number"
-            value={this.props.settings.density}/>
+          <hr/>
+          <ImportSettings
+            config={this.props.settings}
+            onChange={this.props.onSettingsUpdate}/>
           <hr/>
           <StyleSettings/>
           <hr/>
-          <FormSelect
-            id="prefs.app.locale.locale"
-            name="locale"
-            isRequired
-            isSelectionHidden
-            value={this.props.settings.locale}
-            options={this.props.locales}
-            tabIndex={0}
-            onChange={this.handleLocaleChange}/>
+          <LocaleSettings/>
           <hr/>
           <InterfaceSettings
             config={this.props.settings}
@@ -174,41 +41,17 @@ export class AppPrefs extends React.PureComponent {
             config={this.props.settings.print}
             onChange={this.props.onSettingsUpdate}/>
           <hr/>
-          <FormToggle
-            id="prefs.app.debug"
-            name="debug"
-            isDisabled={ARGS.dev}
-            value={this.props.settings.debug || ARGS.dev}
-            onChange={this.handleDebugChange}/>
+          <DeveloperSettings/>
         </Form>
       </ScrollContainer>
     )
   }
 
   static propTypes = {
-    properties: array.isRequired,
-    templates: shape({
-      item: array.isRequired,
-      photo: array.isRequired,
-      selection: array.isRequired
-    }).isRequired,
     settings: shape({
-      debug: bool.isRequired,
       layout: string.isRequired,
-      locale: string.isRequired,
       templates: object.isRequired
     }).isRequired,
-    locales: arrayOf(string).isRequired,
-    importMin: number.isRequired,
-    importMax: number.isRequired,
-    dupOptions: arrayOf(string).isRequired,
     onSettingsUpdate: func.isRequired
-  }
-
-  static defaultProps = {
-    locales: ['cn', 'de', 'en', 'es', 'fr', 'it', 'ja', 'pt', 'pt-BR', 'uk'],
-    dupOptions: ['skip', 'import', 'prompt'],
-    importMin: IMAGE.MIN_DENSITY,
-    importMax: IMAGE.MAX_DENSITY
   }
 }
