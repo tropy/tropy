b815faf95d8b5b5a35536079f9a774f5901d99e3
Sylvester Keil <sylvester@keil.or.at>
2023-09-08 12:58:03 +0200

    Add simple LeafNode list component

---
+++ src/components/list/leaf-node.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import cx from 'classnames'
+import * as icons from '../icons.js'
+import { FormattedMessage } from 'react-intl'
+import { bool, func, string } from 'prop-types'
+
+
+export const ListLeafNode = ({ icon, name, isSelected, onClick }) => (
+  <li
+    className={cx({ active: isSelected })}
+    onClick={isSelected ? null : onClick}>
+    <div className="list-node-container">
+      {icons[`Icon${icon}`]}
+      <div className="name">
+        <div className="truncate">
+          <FormattedMessage id={name}/>
+        </div>
+      </div>
+    </div>
+  </li>
+)
+
+ListLeafNode.propTypes = {
+  icon: string.isRequired,
+  isSelected: bool,
+  name: string.isRequired,
+  onClick: func.isRequired
+}
