62da7bcdb6e0e6aac13ac0b484b6d5aebc07dbdd
Sylvester Keil <sylvester@keil.or.at>
2023-09-08 13:15:23 +0200

    Fix leaf node icons

--- src/components/list/leaf-node.js
+++ src/components/list/leaf-node.js
@@ -4,28 +4,28 @@ import * as icons from '../icons.js'
 import { FormattedMessage } from 'react-intl'
 import { bool, func, string } from 'prop-types'
 
-export const ListLeafNode = ({ icon, name, isSelected, onClick }) => {
-  const Icon = icons[`Icon${icon}`]
-
-  return (
-    <li
-      className={cx({ active: isSelected })}
-      onClick={isSelected ? null : onClick}>
-      <div className="list-node-container">
-        <Icon/>
-        <div className="name">
-          <div className="truncate">
-            <FormattedMessage id={name}/>
-          </div>
+export const ListLeafNode = ({ icon, name, isSelected, onClick }) => (
+  <li
+    className={cx({ active: isSelected })}
+    onClick={isSelected ? null : onClick}>
+    <div className="list-node-container">
+      {React.createElement(icons[`Icon${icon}`])}
+      <div className="name">
+        <div className="truncate">
+          <FormattedMessage id={name}/>
         </div>
       </div>
-    </li>
-  )
-}
+    </div>
+  </li>
+)
 
 ListLeafNode.propTypes = {
   icon: string.isRequired,
   isSelected: bool,
   name: string.isRequired,
   onClick: func.isRequired
+}
+
+ListLeafNode.defaultProps = {
+  icon: 'Folder'
 }
