55058254c8ee01edd619e815deb7e092d725bb3f
Sylvester Keil <sylvester@keil.or.at>
2023-09-22 16:06:26 +0200

    Fix icon test

--- test/components/icons_test.js
+++ test/components/icons_test.js
@@ -1,10 +1,10 @@
 import { render, inWindowContext } from '../support/react'
-import { IconTropy } from '../../src/components/icons'
+import { IconLock } from '../../src/components/icons'
 
-describe('IconTropy', () => {
-  it('renders the Tropy icon SVG', () => {
+describe('IconLock', () => {
+  it('renders the lock icon SVG', () => {
     expect(
-      render(<IconTropy/>, inWindowContext).element())
+      render(<IconLock/>, inWindowContext).element())
         .to.have.class('icon')
         .and.class('icon-tropy')
         .and.have.descendant('svg')
