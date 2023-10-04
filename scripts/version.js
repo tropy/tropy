25b989e24febf47a9803f745add9365bc180b747
Sylvester Keil <sylvester@keil.or.at>
2023-10-04 10:28:45 +0200

    Add version script

---
+++ scripts/version.js
@@ -0,0 +1,26 @@
+const process = require('node:process')
+const semver = require('semver')
+const shx = require('shelljs')
+
+const VERSION = process.env.npm_package_version
+
+const METAINFO = 'res/linux/org.tropy.Tropy.metainfo.xml'
+const RELEASES = '  <releases>'
+
+function datestring(date = new Date) {
+  return date.toISOString().slice(0, 10)
+}
+
+if (!semver.valid(VERSION)) {
+  console.error('no valid npm version given')
+  process.exit(1)
+}
+
+if (!semver.prerelease(VERSION)) {
+  const release = [
+    `    <release version="${VERSION}" date="${datestring()}">`,
+    `      <url>https://github.com/tropy/tropy/releases/tag/v${VERSION}</url>`,
+    '    </release>'
+  ]
+  shx.sed('-i', RELEASES, [RELEASES, ...release].join('\n'), METAINFO)
+}
