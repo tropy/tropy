0b290b8addd0ed9909fca27703453d4287ee28f6
Sylvester Keil <sylvester@keil.or.at>
2023-10-11 10:42:56 +0200

    Fix #818
    Electron's nativeImage bitmap expects data in BGR color space.
    We now convert the RGB data to PNG first using sharp.

--- src/commands/photo/extract.js
+++ src/commands/photo/extract.js
@@ -7,7 +7,7 @@ import { addOrientation } from '../../common/iiif.js'
 import { warn, info } from '../../common/log.js'
 import Esper from '../../esper/index.js'
 import { fail, save } from '../../dialog.js'
-import { toFile } from '../../image/index.js'
+import { toBuffer, toFile } from '../../image/index.js'
 
 
 export class Extract extends Command {
@@ -36,9 +36,11 @@ export class Extract extends Command {
       })
 
       switch (target) {
-        case ':clipboard:':
-          clipboard.writeImage(nativeImage.createFromBitmap(buffer, raw))
+        case ':clipboard:': {
+          let png = yield call(toBuffer, 'png', buffer, { raw })
+          clipboard.writeImage(nativeImage.createFromBuffer(png))
           break
+        }
         default:
           yield call(toFile, target, buffer, { raw })
       }
