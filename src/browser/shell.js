41e5c9b5125d274aa39b735d95cc68aa5f65b782
Sylvester Keil <sylvester@keil.or.at>
2024-02-13 10:55:08 +0100

    Add shell open/show service

---
+++ src/browser/shell.js
@@ -0,0 +1,66 @@
+import { URL } from 'node:url'
+import { ipcMain, shell } from 'electron'
+import { error } from '../common/log.js'
+
+export async function open(target) {
+  let { path, protocol } = parse(target)
+
+  if (protocol === 'file') {
+    let err = await shell.openPath(path)
+    if (err) throw new Error(`failed to open ${path}: ${err}`)
+  } else {
+    await shell.openExternal(`${protocol}://${path}`)
+  }
+}
+
+export async function show(target) {
+  let { path, protocol } = parse(target)
+
+  if (protocol === 'file') {
+    shell.showItemInFolder(path)
+  } else {
+    await shell.openExternal(`${protocol}://${path}`)
+  }
+}
+
+function parse(target) {
+  let path, protocol
+
+  if (!(target instanceof URL) && target.path) {
+    protocol = target.protocol
+    path = target.path
+  } else {
+    [protocol, path] = String(target).split('://', 2)
+  }
+  protocol = protocol || 'file'
+
+  return { path, protocol }
+}
+
+export async function trash(path) {
+  await shell.trashItem(path)
+}
+
+export function start() {
+  ipcMain.on('shell', handler)
+}
+
+export function stop() {
+  ipcMain.removeListener('shell', handler)
+}
+
+async function handler(event, cmd, ...args) {
+  switch (cmd) {
+    case 'open':
+      await open(...args)
+      break
+    case 'show':
+      await show(...args)
+      break
+    case 'trash':
+      await trash(...args)
+      break
+    default:
+      error(`unknown shell request ${cmd} received`)
+  }
+}
