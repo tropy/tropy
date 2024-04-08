67aa8421f1617160c206e40fb419a0f8607b9ed8
Sylvester Keil <sylvester@keil.or.at>
2024-04-08 18:58:09 +0200

    Add create and restore reducers

--- src/slices/transcriptions.js
+++ src/slices/transcriptions.js
@@ -1,26 +1,34 @@
 import { createSlice } from '@reduxjs/toolkit'
+import { cmdReducer } from './util.js'
+
 
 const transcriptions = createSlice({
   name: 'transcriptions',
   initialState: {},
 
   reducers: {
-    load: {
-      prepare: (payload, meta = {}) => ({
-        payload,
-        meta: { cmd: 'project', ...meta }
-      }),
-      reducer(state, { payload, meta, error }) {
-        if (meta.done && !error)
-          Object.assign(state, payload)
-      }
-    }
+    create: cmdReducer((state, { payload, meta, error }) => {
+      if (meta.done && !error)
+        Object.assign(state, payload)
+    }),
+
+    load: cmdReducer((state, { payload, meta, error }) => {
+      if (meta.done && !error)
+        Object.assign(state, payload)
+    }),
+
+    restore: cmdReducer((state, { payload, meta, error }) => {
+      if (meta.done && !error)
+        Object.assign(state, payload)
+    })
   }
 })
 
 
 export const {
-  load
+  create,
+  load,
+  restore
 } = transcriptions.actions
 
 export default transcriptions.reducer
