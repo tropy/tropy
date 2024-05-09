42e7fe71aa07a038505b77c8dab4b3d8f8f3a72c
Sylvester Keil <sylvester@keil.or.at>
2024-05-09 12:54:12 +0200

    Add test for ontology db creation

---
+++ test/models/ontology_test.js
@@ -0,0 +1,18 @@
+import { join } from 'node:path'
+import { Database } from '../../src/common/db.js'
+import { mkdtmp } from '../support/tmp.js'
+import mod from '../../src/models/ontology.js'
+
+describe('models.ontology', () => {
+  let tmp = mkdtmp()
+
+  describe('create', () => {
+    it('creates a new ontology db from the schema', async () => {
+      let db = new Database(join(tmp, 'o.db'), 'w+')
+      await mod.create(db)
+
+      let { vocab } = await db.get('SELECT count(*) as vocab FROM vocabularies')
+      expect(vocab).to.be.greaterThan(10)
+    })
+  })
+})
