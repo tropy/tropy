25026a9cc6aa630a75c95bac8395890987f91e22
Sylvester Keil <sylvester@keil.or.at>
2024-07-29 12:34:47 +0200

    Update babel configuration

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,27 @@
-{
-  "presets": [
-    ["@babel/preset-react", { "runtime": "automatic" }]
-  ],
-  "plugins": [
+import process from 'node:process'
+
+export default function (api) {
+  api.cache(true)
+
+  let presets = [
+    ['@babel/preset-react', { runtime: 'automatic' }]
   ]
+
+  let plugins = [
+    ['@babel/plugin-syntax-import-attributes']
+  ]
+
+  let targets = { node: '20.15' }
+
+  if (process.env.COVERAGE) {
+    plugins.push(['istanbul', {
+      include: ['src/**/*.js']
+    }])
+  }
+
+  return {
+    presets,
+    plugins,
+    targets
+  }
 }
