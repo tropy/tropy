c3f96b080425e74415ffababc8418600ff14c7be
Sylvester Keil <sylvester@keil.or.at>
2024-08-05 17:40:27 +0200

    Turn Flash into functional component

--- src/components/flash.js
+++ src/components/flash.js
@@ -1,60 +1,52 @@
-import React from 'react'
-import { connect } from 'react-redux'
+import React, { useCallback } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import { FormattedMessage } from 'react-intl'
-import * as act from '../actions/index.js'
+import { flash } from '../actions/index.js'
 import { IconXLarge } from './icons.js'
 import { Button } from './button.js'
 
+const FlashMessage = ({
+  id,
+  onHide,
+  values
+}) => (
+  <li className="flash-message">
+    <span className="text-container">
+      <FormattedMessage id={`flash.${id}.message`} values={values}/>
+    </span>
 
-class FlashMessage extends React.PureComponent {
-  handleConfirm = () => {
-    this.props.onHide({ id: this.props.id, confirm: true })
-  }
+    <button className="btn btn-primary" onClick={() => {
+      onHide({ id, confirm: true })
+    }}>
+      <FormattedMessage id={`flash.${id}.confirm`}/>
+    </button>
 
-  handleDismiss = () => {
-    this.props.onHide({ id: this.props.id, dismiss: true })
-  }
-
-  render() {
-    return (
-      <li className="flash-message">
-        <span className="text-container">
-          <FormattedMessage
-            id={`flash.${this.props.id}.message`}
-            values={this.props.values}/>
-        </span>
-
-        <button className="btn btn-primary" onClick={this.handleConfirm}>
-          <FormattedMessage id={`flash.${this.props.id}.confirm`}/>
-        </button>
-
-        <Button icon={<IconXLarge/>} onClick={this.handleDismiss}/>
-      </li>
-    )
-  }
-}
-
-
-const Flash = ({ messages, onHide }) => (
-  messages.length > 0 && (
-    <ul className="flash">
-      {messages.map(({ id, values }) =>
-        <FlashMessage key={id} id={id} values={values} onHide={onHide}/>)}
-    </ul>
-  )
+    <Button icon={<IconXLarge/>} onClick={() => {
+      onHide({ id, dismiss: true })
+    }}/>
+  </li>
 )
 
-const FlashContainer = connect(
-  state => ({
-    messages: state.flash
-  }),
-  dispatch => ({
-    onHide(...args) {
-      dispatch(act.flash.hide(...args))
-    }
-  })
-)(Flash)
 
-export {
-  FlashContainer as Flash
+export const Flash = () => {
+  let dispatch = useDispatch()
+  let messages = useSelector(state => state.flash)
+
+  let onHide = useCallback((...args) => {
+    dispatch(flash.hide(...args))
+  })
+
+  if (!(messages?.length > 0))
+    return null
+
+  return (
+    <ul className="flash">
+      {messages.map(({ id, values }) =>
+        <FlashMessage
+          key={id}
+          id={id}
+          values={values}
+          onHide={onHide}/>)}
+    </ul>
+  )
 }
