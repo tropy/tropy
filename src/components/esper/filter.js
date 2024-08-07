bc0e34d0e9f02eb8d764478133c0b0e31c0b54e1
Sylvester Keil <sylvester@keil.or.at>
2024-08-07 17:16:40 +0200

    Refactor EsperPanel

---
+++ src/components/esper/filter.js
@@ -0,0 +1,120 @@
+import React from 'react'
+import { FormattedMessage } from 'react-intl'
+import { FormToggle } from '../form.js'
+import { Slider } from '../slider.js'
+import { Icon } from '../icons.js'
+import { useEvent } from '../../hooks/use-event.js'
+import { TABS } from '../../constants/index.js'
+
+export const FilterControls = ({
+  brightness,
+  contrast,
+  hue,
+  isDisabled,
+  negative,
+  onChange,
+  saturation,
+  sharpen
+}) => (
+  <ul className="filter-controls">
+    <RangeFilter
+      icon="Sun"
+      isDisabled={isDisabled}
+      name="brightness"
+      onChange={onChange}
+      value={brightness}/>
+    <RangeFilter
+      icon="Contrast"
+      isDisabled={isDisabled}
+      name="contrast"
+      onChange={onChange}
+      value={contrast}/>
+    <RangeFilter
+      icon="Hue"
+      isDisabled={isDisabled}
+      max={180}
+      min={-180}
+      name="hue"
+      onChange={onChange}
+      value={hue}/>
+    <RangeFilter
+      icon="Drop"
+      isDisabled={isDisabled}
+      name="saturation"
+      onChange={onChange}
+      value={saturation}/>
+    <RangeFilter
+      icon="Sharpen"
+      isDisabled={isDisabled}
+      max={200}
+      min={0}
+      name="sharpen"
+      onChange={onChange}
+      origin={null}
+      value={sharpen}/>
+    <ToggleFilter
+      name="negative"
+      isDisabled={isDisabled}
+      onChange={onChange}
+      value={negative}/>
+  </ul>
+)
+
+export const RangeFilter = ({
+  max = 100,
+  min = -100,
+  name,
+  icon,
+  isDisabled = false,
+  onChange,
+  origin = 0,
+  tabIndex = TABS.EsperPanel,
+  value
+}) => {
+  let handleChange = useEvent((next) => {
+    onChange({ [name]: next })
+  })
+
+  return (
+    <li className="filter-control color-slider">
+      <div className="flex-row center">
+        <Icon name={icon}/>
+        <div className="title">
+          <FormattedMessage
+            id={`esper.panel.${name}.label`}/>
+        </div>
+        <div className="value">
+          <FormattedMessage
+            id={`esper.panel.${name}.value`}
+            values={{ value }}/>
+        </div>
+      </div>
+      <Slider
+        isDisabled={isDisabled}
+        max={max}
+        min={min}
+        origin={origin}
+        tabIndex={tabIndex}
+        value={value}
+        onChange={handleChange}/>
+    </li>
+  )
+}
+
+export const ToggleFilter = ({
+  name,
+  isDisabled,
+  onChange,
+  tabIndex = TABS.EsperPanel,
+  value
+}) => (
+  <li className="filter-control">
+    <FormToggle
+      id={`esper.panel.${name}`}
+      name={name}
+      isDisabled={isDisabled}
+      tabIndex={tabIndex}
+      value={value}
+      onChange={onChange}/>
+  </li>
+)
--- src/components/esper/panel.js
+++ src/components/esper/panel.js
@@ -1,136 +1,35 @@
 import React from 'react'
-import { Slider } from '../slider.js'
-import { Button } from '../button.js'
-import { FormattedMessage } from 'react-intl'
-import { FormToggle } from '../form.js'
+import { FilterControls } from './filter.js'
+import { Button, ButtonContainer } from '../button.js'
 import { shallow } from '../../common/util.js'
-import { TABS } from '../../constants/index.js'
 import { FILTERS } from '../../esper/photo.js'
+import { TABS } from '../../constants/index.js'
 
-import {
-  IconSun,
-  IconContrast,
-  IconHue,
-  IconDrop,
-  IconSharpen
-} from '../icons.js'
-
-
-export class ColorSlider extends React.PureComponent {
-  handleChange = (value) => {
-    this.props.onChange({ [this.props.type]: value })
-  }
-
-  get tabIndex() {
-    return this.props.isDisabled ? null : this.props.tabIndex
-  }
-
-  renderLabel() {
-    return (
-      <div className="flex-row center">
-        {this.props.icon}
-        <div className="title">
-          <FormattedMessage
-            id={`esper.panel.${this.props.type}.label`}/>
-        </div>
-        <div className="value">
-          <FormattedMessage
-            id={`esper.panel.${this.props.type}.value`}
-            values={{ value: this.props.value }}/>
-        </div>
-      </div>
-    )
-  }
-
-  render() {
-    return (
-      <li className="adjustment color-slider">
-        {this.renderLabel()}
-        <Slider
-          isDisabled={this.props.isDisabled}
-          max={this.props.max}
-          min={this.props.min}
-          origin={this.props.origin}
-          tabIndex={this.tabIndex}
-          value={this.props.value}
-          onChange={this.handleChange}/>
-      </li>
-    )
-  }
-
-  static defaultProps = {
-    max: 100,
-    min: -100,
-    origin: 0
-  }
-}
-
-
-export const EsperPanel = (props) => {
-  let isDefault = shallow(FILTERS, props)
+export const EsperPanel = ({
+  isDisabled,
+  isVisible,
+  onChange,
+  onRevert,
+  ...props
+}) => {
+  let isPristine = shallow(FILTERS, props)
 
   return (
     <div className="esper-panel">
-      <ul className="adjustments">
-        <ColorSlider
-          icon={<IconSun/>}
-          isDisabled={props.isDisabled || !props.isVisible}
-          tabIndex={TABS.EsperPanel}
-          type="brightness"
-          value={props.brightness}
-          onChange={props.onChange}/>
-        <ColorSlider
-          icon={<IconContrast/>}
-          isDisabled={props.isDisabled || !props.isVisible}
-          tabIndex={TABS.EsperPanel}
-          type="contrast"
-          value={props.contrast}
-          onChange={props.onChange}/>
-        <ColorSlider
-          icon={<IconHue/>}
-          isDisabled={props.isDisabled || !props.isVisible}
-          tabIndex={TABS.EsperPanel}
-          min={-180}
-          max={180}
-          type="hue"
-          value={props.hue}
-          onChange={props.onChange}/>
-        <ColorSlider
-          icon={<IconDrop/>}
-          isDisabled={props.isDisabled || !props.isVisible}
-          tabIndex={TABS.EsperPanel}
-          type="saturation"
-          value={props.saturation}
-          onChange={props.onChange}/>
-        <ColorSlider
-          icon={<IconSharpen/>}
-          isDisabled={props.isDisabled || !props.isVisible}
-          tabIndex={TABS.EsperPanel}
-          type="sharpen"
-          min={0}
-          max={200}
-          origin={null}
-          value={props.sharpen}
-          onChange={props.onChange}/>
-        <li className="adjustment">
-          <FormToggle
-            id="esper.panel.negative"
-            name="negative"
-            isDisabled={props.isDisabled || !props.isVisible}
-            tabIndex={TABS.EsperPanel}
-            value={props.negative}
-            onChange={props.onChange}/>
-        </li>
-      </ul>
-      <div className="revert-btn-container">
+      <FilterControls
+        isDisabled={isDisabled || !isVisible}
+        {...props}
+        onChange={onChange}/>
+
+      <ButtonContainer className="revert">
         <Button
           isBlock
           isDefault
-          isDisabled={isDefault || props.isDisabled || !props.isVisible}
+          isDisabled={isDisabled || isPristine || !isVisible}
           tabIndex={TABS.EsperPanel}
           text="esper.panel.revert"
-          onClick={props.onRevert}/>
-      </div>
+          onClick={onRevert}/>
+      </ButtonContainer>
     </div>
   )
 }
