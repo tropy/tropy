90d61cc5b0396bdf8688239236bd968f83cabf39
Sylvester Keil <sylvester@keil.or.at>
2024-08-09 14:26:39 +0200

    Use destructured props

--- src/components/esper/error.js
+++ src/components/esper/error.js
@@ -2,13 +2,13 @@ import { FormattedMessage } from 'react-intl'
 import { Button } from '../button.js'
 import { IconWarningLarge } from '../icons.js'
 
-export const EsperPhotoError = (props) => (
+export const EsperPhotoError = ({ onConsolidate }) => (
   <div className="esper-error">
     <IconWarningLarge/>
     <p><FormattedMessage id="photo.error"/></p>
     <Button
       isDefault
       text="photo.consolidate"
-      onClick={props.onConsolidate}/>
+      onClick={onConsolidate}/>
   </div>
 )
