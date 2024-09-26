4df52c7e0907eb876f076bd3f9d0c3f5694f2242
Sylvester Keil <sylvester@keil.or.at>
2024-09-26 11:28:02 +0200

    Set Resizable edge based on layout

--- src/components/esper/overlay.js
+++ src/components/esper/overlay.js
@@ -2,12 +2,13 @@ import { useDispatch, useSelector } from 'react-redux'
 import cx from 'classnames'
 import { useEvent } from '../../hooks/use-event.js'
 import { Resizable } from '../resizable.js'
-import { ESPER } from '../../constants/index.js'
+import { ESPER, ITEM } from '../../constants/index.js'
 import ui from '../../actions/ui.js'
 
 export const EsperOverlay = ({ children, mode }) => {
   let dispatch = useDispatch()
   let height = useSelector(state => state.ui.esper.split)
+  let layout = useSelector(state => state.settings.layout)
 
   let handleResize = useEvent((split) => {
     dispatch(ui.update({ esper: { split } }))
@@ -24,7 +25,7 @@ export const EsperOverlay = ({ children, mode }) => {
 
   return (
     <Resizable
-      edge="top"
+      edge={layout === ITEM.LAYOUT.SIDE_BY_SIDE ? 'left' : 'top'}
       isRelative
       isBuffered
       onChange={handleResize}
