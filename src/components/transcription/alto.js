131cdb24d8948e478564dfc16886689ad4c5051e
Sylvester Keil <sylvester@keil.or.at>
2024-10-26 14:58:27 +0200

    Add mouse selection handler props

--- src/components/transcription/alto.js
+++ src/components/transcription/alto.js
@@ -1,13 +1,31 @@
 import React from 'react'
 import cx from 'classnames'
+import { useEvent } from '../../hooks/use-event.js'
 
 export const Alto = React.memo(({
   document,
-  outline = 'none' // eslint-disable-line no-unused-vars
+  outline = 'none'
 }) => {
 
+  let handleMouseDown = useEvent(() => {
+    // start live selection mode
+    // cancel if mouse outside window
+  })
+
+  let handleMouseUp = useEvent(() => {
+    // stop live selection mode
+    // save selection if not empty
+  })
+
+  let handleMouseEnter = useEvent(() => {
+    // update live selection
+  })
+
   return (
-    <section className="cx('alto-document', `outline-${outline}`)">
+    <section
+      className={cx('alto-document', `outline-${outline}`)}
+      onMouseDown={handleMouseDown}
+      onMouseUp={handleMouseUp}>
       {document.blocks().map((block, bidx) => (
         <TextBlock key={bidx}>
           {block.lines().map((line, lidx) => (
@@ -16,7 +34,8 @@ export const Alto = React.memo(({
                 <String
                   key={sidx}
                   isSelected={false}
-                  value={string.CONTENT}/>
+                  onMouseEnter={handleMouseEnter}
+                  value={string}/>
               ))}
             </Line>
           ))}
@@ -38,8 +57,14 @@ export const Line = ({ children }) => (
   </div>
 )
 
-export const String = ({ isSelected = false, value }) => (
-  <span className={cx('string', { selected: isSelected })}>
-    {value}
+export const String = ({
+  isSelected = false,
+  onMouseEnter,
+  value
+}) => (
+  <span
+    className={cx('string', { selected: isSelected })}
+    onMouseEnter={() => { onMouseEnter(value) }}>
+    {value.CONTENT}
   </span>
 )
