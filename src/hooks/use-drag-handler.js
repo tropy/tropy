380f1e439f636b7f480a82d3427e3a2e2b0ea000
Sylvester Keil <sylvester@keil.or.at>
2024-10-26 20:05:21 +0200

    Add useDragHandler hook

---
+++ src/hooks/use-drag-handler.js
@@ -0,0 +1,69 @@
+import { useEvent } from './use-event.js'
+import { on, off, toggle } from '../dom.js'
+
+export function useDragHandler({
+  onDrag,
+  onDragStart,
+  onDragStop,
+  stopOnMouseLeave = true
+}) {
+  let handleKeyDown = useEvent((event) => {
+    switch (event.key) {
+      case 'Escape':
+        event.stopPropagation()
+        event.stopImmediatePropagation()
+        handleDragStop(event)
+        break
+    }
+  })
+
+  let handleDrag = useEvent((event) => {
+    if (event.buttons === 0) {
+      handleDragStop(event)
+    } else {
+      onDrag?.(event)
+    }
+  })
+
+  let handleDragStop = useEvent((event) => {
+    off(document, 'mousemove', handleDrag)
+    off(document, 'mouseup', handleDragStop, { capture: true })
+    off(window, 'blur', handleDragStop)
+
+    off(document.body, 'mouseleave', handleDragStop)
+    off(document.body, 'keydown', handleKeyDown)
+
+    let wasCancelled = !(
+      event?.type === 'mouseup' || event?.type === 'mousemove'
+    )
+
+    onDragStop?.(event, wasCancelled)
+
+    toggle(document.documentElement, 'dragging', false)
+  })
+
+  return useEvent((event, ...args) => {
+    handleDragStop()
+
+    // Handle only clicks with the left/primary button!
+    if (event.button !== 0)
+      return
+
+    toggle(document.documentElement, 'dragging', true)
+
+    on(document, 'mousemove', handleDrag)
+    on(document, 'mouseup', handleDragStop, { capture: true })
+    on(window, 'blur', handleDragStop)
+
+    if (stopOnMouseLeave) {
+      on(document.body, 'mouseleave', handleDragStop)
+    }
+
+    // Register on body because global bindings are bound
+    // on document and we need to stop the propagation in
+    // case we handle it here!
+    on(document.body, 'keydown', handleKeyDown)
+
+    onDragStart?.(event, ...args)
+  })
+}
