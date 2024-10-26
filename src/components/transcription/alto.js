18181a58b5b26dd354ab8629d808d6c0e66cbc78
Sylvester Keil <sylvester@keil.or.at>
2024-10-26 20:08:16 +0200

    Hook up useDragHandler WIP

--- src/components/transcription/alto.js
+++ src/components/transcription/alto.js
@@ -1,31 +1,38 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 import cx from 'classnames'
 import { useEvent } from '../../hooks/use-event.js'
+import { useDragHandler } from '../../hooks/use-drag-handler.js'
+
+function expand(a, b) {
+  if (!a) return b
+  if (!b) return a
+  // todo
+  return a
+}
 
 export const Alto = React.memo(({
   document,
   outline = 'none'
 }) => {
+  let anchor = useRef()
+  let [selection, setSelection] = useState()
 
-  let handleMouseDown = useEvent(() => {
-    // start live selection mode
-    // cancel if mouse outside window
+  let handleDragStart = useDragHandler({
+    onDragStart(event, string) {
+      anchor.current = string.bounds()
+    },
+    onDragEnd() {
+      anchor.current = null
+      setSelection(null)
+    }
   })
 
-  let handleMouseUp = useEvent(() => {
-    // stop live selection mode
-    // save selection if not empty
-  })
-
-  let handleMouseEnter = useEvent(() => {
-    // update live selection
+  let handleMouseEnter = useEvent((string) => {
+    setSelection(expand(anchor.current, string.bounds()))
   })
 
   return (
-    <section
-      className={cx('alto-document', `outline-${outline}`)}
-      onMouseDown={handleMouseDown}
-      onMouseUp={handleMouseUp}>
+    <section className={cx('alto-document', `outline-${outline}`)}>
       {document.blocks().map((block, bidx) => (
         <TextBlock key={bidx}>
           {block.lines().map((line, lidx) => (
@@ -33,7 +40,8 @@ export const Alto = React.memo(({
               {line.strings().map((string, sidx) => (
                 <String
                   key={sidx}
-                  isSelected={false}
+                  isSelected={selection && string.intersects(selection)}
+                  onMouseDown={handleDragStart}
                   onMouseEnter={handleMouseEnter}
                   value={string}/>
               ))}
@@ -59,11 +67,13 @@ export const Line = ({ children }) => (
 
 export const String = ({
   isSelected = false,
+  onMouseDown,
   onMouseEnter,
   value
 }) => (
   <span
     className={cx('string', { selected: isSelected })}
+    onMouseDown={(event) => { onMouseDown(event, value) }}
     onMouseEnter={() => { onMouseEnter(value) }}>
     {value.CONTENT}
   </span>
