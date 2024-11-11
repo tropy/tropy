375cd2836450d82ae154256c528c8474f21a9589
Sylvester Keil <sylvester@keil.or.at>
2024-11-11 16:53:55 +0100

    Convert sharpen shader to WGSL

--- src/esper/filter/sharpen.js
+++ src/esper/filter/sharpen.js
@@ -1,5 +1,5 @@
-import { Filter, GlProgram } from 'pixi.js'
-import { vertex } from 'pixi-filters'
+import { Filter, GlProgram, GpuProgram } from 'pixi.js'
+import { vertex, wgslVertex } from 'pixi-filters'
 import { restrict } from '../../common/util.js'
 import { Shader } from '../../res.js'
 
@@ -7,13 +7,23 @@ import { Shader } from '../../res.js'
 export class SharpenFilter extends Filter {
   constructor(intensity = 0, width = 200, height = 200) {
     super({
+      gpuProgram: GpuProgram.from({
+        fragment: {
+          source: Shader.load('sharpen.wgsl'),
+          entryPoint: 'mainFragment'
+        },
+        vertex: {
+          source: wgslVertex,
+          entryPoint: 'mainVertex'
+        }
+      }),
       glProgram: GlProgram.from({
         fragment: Shader.load('sharpen.frag'),
         vertex,
         name: 'sharpen-filter'
       }),
       resources: {
-        sharpen: {
+        sharpenUniforms: {
           size: {
             value: new Float32Array(2),
             type: 'vec2<f32>'
@@ -31,27 +41,31 @@ export class SharpenFilter extends Filter {
     this.height = height
   }
 
+  get uniforms() {
+    return this.resources.sharpenUniforms.uniforms
+  }
+
   get intensity() {
-    return this.resources.sharpen.uniforms.intensity
+    return this.uniforms.intensity
   }
 
   set intensity(intensity) {
-    this.resources.sharpen.uniforms.intensity = restrict(intensity / 100, 0, 10)
+    this.uniforms.intensity = restrict(intensity / 100, 0, 10)
   }
 
   get width() {
-    return 1 / this.resources.sharpen.uniforms.size[0]
+    return 1 / this.uniforms.size[0]
   }
 
   set width(value) {
-    this.resources.sharpen.uniforms.size[0] = 1 / value
+    this.uniforms.size[0] = 1 / value
   }
 
   get height() {
-    return 1 / this.resources.sharpen.uniforms.size[1]
+    return 1 / this.uniforms.size[1]
   }
 
   set height(value) {
-    this.resources.sharpen.uniforms.size[1] = 1 / value
+    this.uniforms.size[1] = 1 / value
   }
 }
