a94eeae4b3d7c2142d1cbf4862a2864df58a58f2
Sylvester Keil <sylvester@keil.or.at>
2024-11-11 11:30:49 +0100

    Upadte color syntax for new Graphics API

--- src/constants/esper.js
+++ src/constants/esper.js
@@ -25,21 +25,21 @@ export default {
 
   COLOR: {
     mask: {
-      line: [0xffffff, 1],
-      fill: [0x000000, 0.4]
+      line: { color: 0xffffff, alpha: 1 },
+      fill: { color: 0x000000, alpha: 0.4 }
     },
     selection: {
       default: {
-        line: [0x5c93e5, 1],
-        fill: [0xcedef7, 0.4]
+        line: { color: 0x5c93e5, alpha: 1 },
+        fill: { color: 0xcedef7, alpha: 0.4 }
       },
       active: {
-        line: [0x5c93e5, 1],
-        fill: [0xcedef7, 0.8]
+        line: { color: 0x5c93e5, alpha: 1 },
+        fill: { color: 0xcedef7, alpha: 0.8 }
       },
       live: {
-        line: [0x5c93e5, 1],
-        fill: [0xcedef7, 0.8]
+        line: { color: 0x5c93e5, alpha: 1 },
+        fill: { color: 0xcedef7, alpha: 0.8 }
       }
     }
   }
