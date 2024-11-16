5e51aa02bc6fea7387245ecb00e8cb933ef8c622
Sylvester Keil <sylvester@keil.or.at>
2024-11-16 13:51:12 +0100

    Draw string text boxes

--- src/esper/text.js
+++ src/esper/text.js
@@ -5,53 +5,96 @@ import { normalizeRectangle } from './util.js'
 
 
 export class TextLayer extends Container {
-
   constructor() {
     super()
-    this.selection = new TextSelection()
-    this.addChild(this.selection)
+    this.liveSelection = new TextSelection()
+    this.addChild(this.liveSelection)
+  }
+
+  clear() {
+    if (this.children.length > 1) {
+      for (let block of this.removeChildren(1)) {
+        block.destroy()
+      }
+    }
   }
 
   destroy() {
     super.destroy({ children: true })
   }
 
-  sync(props) {
-    // drop/create text objects
+  sync(props, state) {
+    console.log('TextLayer#sync')
+
+    // this.visible
+    // this.eventMode
+
+    let document = state.alto
+    this.clear()
+
+    if (document) {
+      for (let string of document.strings()) {
+        this.addChild(new TextBox(string))
+      }
+    }
   }
 
-  update(props) {
+  update({ selection } = {}) {
+    let scale = 1 / this.parent.scale.y
+
+    if (selection)
+      selection = normalizeRectangle(selection)
+
     for (let child of this.children) {
-      child.update(props)
+      child.update(scale, selection)
     }
   }
 }
 
 export class TextSelection extends Graphics {
-  destroy() {
-    super.destroy({ children: true })
-  }
+  update(scale = 1, { x, y, width, height } = {}) {
+    this.clear()
 
-  update() {
-    // if there's a text selection draw it. otherwise hide.
-  }
-}
+    if (!width || !height) return
 
-
-export class TextBlock extends Container {
-  destroy() {
-    super.destroy({ children: true })
+    this
+      .rect(x, y, width, height)
+      .fill({ color: 0x000000, alpha: 0.5 })
+      .stroke({ width: scale, color: 0xffffff, alpha: 1 })
   }
 }
 
-export class TextLine extends Container {
-  destroy() {
-    super.destroy({ children: true })
-  }
-}
 
-export class Word extends Graphics {
+export class TextBox extends Graphics {
+  fillStyle = { color: 0x000000, alpha: 0.125 }
+  strokeStyle = ({ color: 0xffffff, alpha: 1 })
+
+  constructor(node) {
+    super()
+    this.sync(node)
+  }
+
   destroy() {
-    super.destroy({ children: true })
+    super.destroy(true)
+  }
+
+  sync(node) {
+    this.data = normalizeRectangle(node.bounds())
+
+  }
+
+  update(scale) {
+    let { x, y, width, height } = this.data
+
+    this.clear()
+    if (!width || !height) return
+
+    // hover
+    // selected
+
+    this
+      .rect(x, y, width, height)
+      .fill(this.fillStyle)
+      .stroke({ width: scale, ...this.strokeStyle })
   }
 }
