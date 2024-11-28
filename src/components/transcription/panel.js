911b57527abe6260f29e073296c2580f3c973ce7
Sylvester Keil <sylvester@keil.or.at>
2024-11-28 11:23:28 +0100

    Add transcription versions

--- src/components/transcription/panel.js
+++ src/components/transcription/panel.js
@@ -1,8 +1,24 @@
+import { useSelector } from 'react-redux'
+import { TranscriptionMetadata } from './metadata.js'
+import { getTranscriptions } from '../../selectors/index.js'
 
-export const TranscriptionPanel = () => {
+export const TranscriptionPanel = ({
+  active,
+  id,
+}) => {
+  let transcriptions = useSelector(state => getTranscriptions(state, { id }))
 
   return (
     <div className="transcription-panel">
+      <ol className="transcription-versions">
+        {transcriptions.map(tr => (
+          <li key={tr.id}>
+            <TranscriptionMetadata
+              created={tr.created}
+              isActive={tr.id === active}/>
+          </li>
+        ))}
+      </ol>
     </div>
   )
 }
