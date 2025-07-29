46f25b0ea60a463c16a3e9894f5a8bdb9e3b1b07
Sylvester Keil <sylvester@keil.or.at>
2025-07-29 19:24:05 +0200

    Add api stop / close connections

--- src/main/api.js
+++ src/main/api.js
@@ -1,6 +1,8 @@
-import { info, logger } from '../common/log.js'
+import { debug, info, logger } from '../common/log.js'
 
 export class Server {
+  #server
+
   constructor(app) {
     this.app = app
   }
@@ -14,23 +16,41 @@ export class Server {
   )
 
   async start() {
-    if (this.app.state.api || this.app.opts.port) {
-      let api = await import('../common/api')
+    if (this.#server != null)
+      throw new Error('api already started')
 
-      this.koa = api.create({
-        dispatch: this.dispatch,
-        log: logger.child({ name: 'api' }),
-        rsvp: this.rsvp,
-        version: this.app.version
-      })
+    if (this.app.state.api || this.app.opts.port) {
+      if (this.koa == null) {
+        let api = await import('../common/api')
+
+        this.koa = api.create({
+          dispatch: this.dispatch,
+          log: logger.child({ name: 'api' }),
+          rsvp: this.rsvp,
+          version: this.app.version
+        })
+      }
 
       let port = this.app.opts.port || this.app.state.port
 
-      info(`api.start on port ${port}`)
-      this.koa.listen(port)
+      info(`api.start listening on port ${port}...`)
+      this.#server = this.koa.listen(port)
     }
   }
 
   stop() {
+    return new Promise((resolve) => {
+      if (this.#server != null) {
+        info('api.stop closing connections...')
+        this.#server.close(() => {
+          this.#server = null
+          debug('api.stop all connections closed')
+          resolve()
+        })
+        this.#server.closeAllConnections()
+      } else {
+        resolve()
+      }
+    })
   }
 }
