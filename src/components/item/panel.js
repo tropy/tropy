a3299bad393b15bb82bb3737ec449219e0c3f53c
Sylvester Keil <sylvester@keil.or.at>
2025-09-26 13:51:40 +0200

    Skip focus on edit cancel

--- src/components/item/panel.js
+++ src/components/item/panel.js
@@ -65,7 +65,6 @@ export class ItemPanelGroup extends React.PureComponent {
 
   handleEditCancel = () => {
     this.props.onEditCancel()
-    this.panel.current.focus()
   }
 
   handleTagAddCancel = () => {
