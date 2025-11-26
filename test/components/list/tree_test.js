2797ad65e160f61b356c722281124d9fbb8e889f
Sylvester Keil <sylvester@keil.or.at>
2025-11-26 12:10:58 +0100

    Update list tree tests

--- test/components/list/tree_test.js
+++ test/components/list/tree_test.js
@@ -2,31 +2,19 @@ import { render, inWindowContext } from '../../support/react.js'
 import { ListTree } from '#internal/components/list/tree.js'
 
 describe('ListTree', () => {
-  it('renders no list nodes by default', () => {
-    expect(
-      render(
-        <ListTree
-          parent={F.lists.root}
-          lists={F.lists}/>,
-        inWindowContext).element())
-      .not.to.have.descendants('.list-node')
-  })
-
   it('renders no list for empty parent', () => {
     expect(
       render(
         <ListTree
-          isExpanded
           parent={F.lists.empty}
           lists={F.lists}/>,
         inWindowContext).element())
       .not.to.have.descendants('.list-node')
   })
 
-  it('renders list nodes when expanded', () => {
+  it('renders list nodes', () => {
     const { element, getByText } = render((
       <ListTree
-        isExpanded
         parent={F.lists.root}
         lists={F.lists}/>
     ), inWindowContext)
