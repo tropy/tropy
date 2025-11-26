65de4a8b52a15d436464520b5271834f0cb7b610
Sylvester Keil <sylvester@keil.or.at>
2025-11-26 15:16:18 +0100

    Remove unused import

--- src/components/photo/thumbnail.js
+++ src/components/photo/thumbnail.js
@@ -1,4 +1,3 @@
-import React from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { Image } from '../image.js'
 import { selectCachePrefix } from '../../selectors/project.js'
