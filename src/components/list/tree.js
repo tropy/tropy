789758d4e273eee7b969cc4123193a94a7cff743
Sylvester Keil <sylvester@keil.or.at>
2025-11-26 10:57:31 +0100

    Make ListTree a functional component

--- src/components/list/tree.js
+++ src/components/list/tree.js
@@ -1,62 +1,55 @@
-import React from 'react'
 import { ListNode, NewListNode } from './node.js'
 import { Collapse } from '../fx.js'
 
+const hasNewListNode = (edit, parent) =>
+  edit && edit.id == null && edit.parent === parent
 
-export class ListTree extends React.Component {
-  hasNewListNode(parent = this.props.parent.id) {
-    let { edit } = this.props
-    return edit && edit.id == null && edit.parent === parent
-  }
+export const ListTree = ({
+  depth = 0,
+  expand = {},
+  hold = {},
+  lists = {},
+  minDropDepth = 0,
+  ...props
+}) => {
+  return (
+    <ol className="list-tree">
+      {props.isExpanded && props.parent.children.map((id, idx, all) => {
+        if (id in lists) {
+          let list = lists[id]
+          let newListNode = hasNewListNode(props.edit, id)
+          let isExpandable = newListNode || list.children.length > 0
+          let isExpanded = newListNode || expand[id]
 
-  isEditing(id) {
-    return this.props.edit?.id === id
-  }
-
-  mapChildren(fn, props = this.props) {
-    return props.isExpanded && props.parent.children.map((id, idx, all) => {
-      if (id in props.lists) {
-        let list = props.lists[id]
-        let hasNewListNode = this.hasNewListNode(id)
-        let isExpandable = hasNewListNode || list.children.length > 0
-        let isExpanded = hasNewListNode || props.expand[id]
-        let isLast = (idx === all.length - 1)
-
-        return fn(id, {
-          ...props,
-          list,
-          isSelected: props.selection === id,
-          isExpandable,
-          isExpanded: isExpandable && isExpanded,
-          isEditing: this.isEditing(id),
-          isHolding: props.hold[id],
-          isLast,
-          position: idx
-        })
-      }
-    })
-  }
-
-  render() {
-    return (
-      <ol className="list-tree" ref={this.setContainer}>
-        {this.mapChildren((key, props) =>
-          <ListNode key={key} {...props}/>)}
-        <Collapse in={this.hasNewListNode()} className="list-node" tagName="li">
-          <NewListNode
-            parent={this.props.edit?.parent}
-            onCancel={this.props.onEditCancel}
-            onSave={this.props.onSave}/>
-        </Collapse>
-      </ol>
-    )
-  }
-
-  static defaultProps = {
-    depth: 0,
-    expand: {},
-    hold: {},
-    lists: {},
-    minDropDepth: 0
-  }
+          return (
+            <ListNode
+              {...props}
+              key={id}
+              list={list}
+              lists={lists}
+              hold={hold}
+              depth={depth}
+              minDropDepth={minDropDepth}
+              expand={expand}
+              isSelected={props.selection === id}
+              isExpandable={isExpandable}
+              isExpanded={isExpandable && isExpanded}
+              isEditing={props.edit?.id === id}
+              isHolding={hold[id]}
+              isLast={idx === all.length - 1}
+              position={idx}/>
+          )
+        }
+      })}
+      <Collapse
+        tagName="li"
+        className="list-node"
+        in={hasNewListNode(props.edit, props.parent.id)}>
+        <NewListNode
+          parent={props.edit?.parent}
+          onCancel={props.onEditCancel}
+          onSave={props.onSave}/>
+      </Collapse>
+    </ol>
+  )
 }
