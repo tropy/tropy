92cc611eed604514e6799d83dddbd23ba4738b2c
Sylvester Keil <sylvester@keil.or.at>
2025-12-17 18:23:53 +0100

    Automatically tag citation file

--- scripts/version.js
+++ scripts/version.js
@@ -6,10 +6,13 @@ const VERSION = process.env.npm_package_version
 
 const METAINFO = 'res/linux/org.tropy.Tropy.metainfo.xml'
 const RELEASES = '  <releases>'
+const CITATION = 'CITATION.cff'
 
 function datestring(date = new Date) {
   return date.toISOString().slice(0, 10)
 }
+
+const DATE = datestring()
 
 if (!semver.valid(VERSION)) {
   console.error('no valid npm version given')
@@ -18,9 +21,12 @@ if (!semver.valid(VERSION)) {
 
 if (!semver.prerelease(VERSION)) {
   const release = [
-    `    <release version="${VERSION}" date="${datestring()}">`,
+    `    <release version="${VERSION}" date="${DATE}">`,
     `      <url>https://github.com/tropy/tropy/releases/tag/v${VERSION}</url>`,
     '    </release>'
   ]
   shx.sed('-i', RELEASES, [RELEASES, ...release].join('\n'), METAINFO)
+
+  shx.sed('-i', /^version: [\d+.]+$/, `version: ${VERSION}`, CITATION)
+  shx.sed('-i', /^date-released: '[\d+-]+'$/, `date-released: '${DATE}'`, CITATION)
 }
