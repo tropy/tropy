0a20e3c49776849b316aa036aa941ba8808a9526
Martim Passos <martim.passos@alumni.usp.br>
2026-03-23 15:06:15 -0300

    Use new selector, return selection when not prompting

--- src/commands/list/export.js
+++ src/commands/list/export.js
@@ -1,30 +1,34 @@
 import { writeFile as write } from 'node:fs'
-import { clipboard } from 'electron'
 import { call, cps, select } from 'redux-saga/effects'
 import { Command } from '../command.js'
 import { LIST } from '../../constants/index.js'
+import { copy } from '../../clipboard.js'
 import { join } from '../../common/csv.js'
 import { save } from '../../dialog.js'
-import { getAllLists } from '../../selectors/index.js'
+import { getLists } from '../../selectors/index.js'
 
 
 export class Export extends Command {
   *exec() {
-    let { target } = this.action.meta
+    let { meta, payload } = this.action
+    let { target } = meta
 
     if (!target) {
+      if (!meta.prompt) {
+        return yield select(getLists, payload)
+      }
+
       this.isInteractive = true
       target = yield call(save.csv, { defaultPath: 'lists.csv' })
+      if (!target) return
     }
 
-    if (!target) return
-
-    let lists = yield select(getAllLists)
-    let text = join(lists)
+    let lists = yield select(getLists, payload)
+    let text = join(lists, '\n')
 
     switch (target) {
       case ':clipboard:':
-        yield call(clipboard.write, { text })
+        copy({ text })
         break
       default:
         yield cps(write, target, text)
