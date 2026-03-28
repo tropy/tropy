7c721d2db2a938f49bcbf78695d85bb991d4123f
Sylvester Keil <sylvester@keil.or.at>
2026-03-28 09:10:15 +0100

    Update babel config for production bundle

--- babel.config.js
+++ babel.config.js
@@ -1,27 +1,15 @@
-import process from 'node:process'
-
 export default function (api) {
   api.cache(true)
 
-  let presets = [
-    ['@babel/preset-react', { runtime: 'automatic' }]
-  ]
-
-  let plugins = [
-    ['@babel/plugin-syntax-import-attributes']
-  ]
-
-  let targets = { node: '20.15' }
-
-  if (process.env.COVERAGE) {
-    plugins.push(['istanbul', {
-      include: ['src/**/*.js']
-    }])
-  }
-
   return {
-    presets,
-    plugins,
-    targets
+    presets: [
+      ['@babel/preset-react', { runtime: 'automatic' }]
+    ],
+    plugins: [
+      ['@babel/plugin-syntax-import-attributes']
+    ],
+    targets: {
+      node: '24.14'
+    }
   }
 }
