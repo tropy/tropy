deb25e291109c48665e3b59a266a6736ea97964e
Sylvester Keil <sylvester@keil.or.at>
2026-03-28 08:21:09 +0100

    Use ESM if possible

--- test/support/args.js
+++ test/support/args.js
@@ -1,6 +1,8 @@
-const { resolve } = require('node:path')
+import { resolve } from 'node:path'
+import { createRequire } from 'node:module'
 
-const root = resolve(__dirname, '../..')
+const require = createRequire(import.meta.url)
+const root = resolve(import.meta.dirname, '../..')
 
 if (process.type === 'browser') {
   let { Resource } = require('#tropy/main/res.js')
