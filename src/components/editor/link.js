3b747a2d1e4b2bb120cd79df4142e7908a546f2d
Sylvester Keil <sylvester@keil.or.at>
2026-03-31 22:52:19 +0200

    Convert LinkContext to functional component

--- src/components/editor/link.js
+++ src/components/editor/link.js
@@ -1,65 +1,60 @@
-import React from 'react'
-import { injectIntl } from 'react-intl'
+import { useEffect, useRef } from 'react'
+import { useIntl } from 'react-intl'
+import { useEvent } from '../../hooks/use-event.js'
 import { Toolbar } from '../toolbar.js'
 import { Input } from '../input.js'
 import { ensure } from '../../dom.js'
 
-class LinkContext extends React.PureComponent {
-  input = React.createRef()
-  container = React.createRef()
+export const LinkContext = ({
+  href = '',
+  isActive = false,
+  isReadOnly,
+  onBlur = () => (true), // cancel on blur!
+  onCancel,
+  onCommit
+}) => {
+  let input = useRef()
+  let container = useRef()
+  let intl = useIntl()
 
-  componentDidUpdate ({ isActive: wasActive }) {
-    if (!wasActive && this.props.isActive) {
+  let placeholder = intl.formatMessage({
+    id: 'editor.commands.link.target'
+  })
+
+  useEffect(() => {
+    if (isActive) {
       ensure(
-        this.container.current,
+        container.current,
         'transitionend',
-        this.input.current.focus,
+        input.current.focus,
         650)
     }
-  }
+  }, [isActive])
 
-  getLabelFor (name) {
-    return this.props.intl.formatMessage({
-      id: `editor.commands.link.${name}`
-    })
-  }
 
-  handleTargetChange = (href) => {
-    this.props.onCommit({ href })
-  }
+  let handleTargetChange = useEvent((value) => {
+    onCommit({ href: value })
+  })
 
-  render () {
-    return (
-      <Toolbar.Context
-        className="link"
-        ref={this.container}
-        isActive={this.props.isActive}>
-        <Toolbar.Left className="form-inline">
-          {this.props.isActive && (
-            <Input
-              ref={this.input}
-              className="form-control link-target"
-              isReadOnly={this.props.isReadOnly}
-              isRequired
-              placeholder={this.getLabelFor('target')}
-              value={this.props.href}
-              onBlur={this.props.onBlur}
-              onCancel={this.props.onCancel}
-              onCommit={this.handleTargetChange}/>
-          )}
-        </Toolbar.Left>
-      </Toolbar.Context>
-    )
-  }
-
-  static defaultProps = {
-    href: '',
-    onBlur: () => true // cancel on blur!
-  }
-}
-
-const LinkContextContainer = injectIntl(LinkContext)
-
-export {
-  LinkContextContainer as LinkContext
+  return (
+    <Toolbar.Context
+      className="link"
+      ref={container}
+      isActive={isActive}>
+      <Toolbar.Left className="form-inline">
+        {isActive && (
+          <Input
+            ref={input}
+            className="form-control link-target"
+            isReadOnly={isReadOnly}
+            isRequired
+            placeholder={placeholder}
+            value={href}
+            onBlur={onBlur}
+            onCancel={onCancel}
+            onCommit={handleTargetChange}/>
+        )}
+      </Toolbar.Left>
+    </Toolbar.Context>
+  )
 }
