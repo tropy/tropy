803c1954996ca790b3656699d293c09a3b0772e0
Sylvester Keil <sylvester@keil.or.at>
2026-05-04 11:29:23 +0200

    Add more Link props

--- src/components/link.js
+++ src/components/link.js
@@ -1,15 +1,27 @@
 import { useIntl } from 'react-intl'
 import { useIpcEvent } from '../hooks/use-ipc.js'
 
-export const Link = ({ children, id, ...opts }) => {
+export const Link = ({
+  className,
+  children: text,
+  id,
+  title,
+  url,
+  ...opts
+}) => {
   let intl = useIntl()
 
-  let url = intl.formatMessage({ id: `${id}.url` }, opts)
-  let text = children ?? intl.formatMessage({ id: `${id}.title` }, opts)
+  if (id != null) {
+    url ??= intl.formatMessage({ id: `${id}.url` }, opts)
+    text ??= intl.formatMessage({ id: `${id}.title` }, opts)
+    title ??= intl.formatMessage({ id: `${id}.hint` }, opts)
+  }
 
   let handleClick = useIpcEvent(null, ['shell', 'open', url])
 
   return (
-    <a onClick={handleClick}>{text}</a>
+    <a className={className} onClick={handleClick} title={title}>
+      {text}
+    </a>
   )
 }
