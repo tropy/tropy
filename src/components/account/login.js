75acbeb6780a069fa7607d95fbdd201a4e35cb5d
Sylvester Keil <sylvester@keil.or.at>
2026-05-04 12:02:22 +0200

    Use FormField for Login form

--- src/components/account/login.js
+++ src/components/account/login.js
@@ -1,8 +1,8 @@
-import { useCallback, useRef } from 'react'
+import { useCallback } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { FormattedMessage } from 'react-intl'
 import { Button } from '../button.js'
-import { Form, FormElement } from '../form.js'
+import { Form, FormElement, FormField } from '../form.js'
 import { link } from '../../slices/account.js'
 
 
@@ -10,13 +10,11 @@ export function Login ({ ref }) {
   let dispatch = useDispatch()
   let account = useSelector(state => state.account)
 
-  let usernameRef = useRef(null)
-  let passwordRef = useRef(null)
-
   let handleSubmit = useCallback((event) => {
     event.preventDefault()
-    let username = usernameRef.current?.value?.trim()
-    let password = passwordRef.current?.value
+    let form = event.target
+    let username = form.username?.value?.trim()
+    let password = form.password?.value
     if (!username || !password) return
 
     dispatch(link({ username, password }))
@@ -27,41 +25,26 @@ export function Login ({ ref }) {
   return (
     <Form ref={ref} onSubmit={handleSubmit}>
       <figure className="app-icon"/>
-      <FormElement size={8}>
+      <legend>
         <p className="form-description">
           <FormattedMessage id="prefs.account.description"/>
         </p>
-      </FormElement>
-      <div className="form-group">
-        <label htmlFor="prefs.account.username" className="control-label col-4">
-          <FormattedMessage id="prefs.account.username"/>
-        </label>
-        <div className="col-8">
-          <input
-            ref={usernameRef}
-            id="prefs.account.username"
-            className="form-control"
-            type="text"
-            autoComplete="username"
-            disabled={isPending}
-            required/>
-        </div>
-      </div>
-      <div className="form-group">
-        <label htmlFor="prefs.account.password" className="control-label col-4">
-          <FormattedMessage id="prefs.account.password"/>
-        </label>
-        <div className="col-8">
-          <input
-            ref={passwordRef}
-            id="prefs.account.password"
-            className="form-control"
-            type="password"
-            autoComplete="current-password"
-            disabled={isPending}
-            required/>
-        </div>
-      </div>
+      </legend>
+      <FormField
+        id="prefs.account.username"
+        name="username"
+        isCompact
+        isRequired
+        disabled={isPending}
+        tabIndex={0}/>
+      <FormField
+        id="prefs.account.password"
+        name="password"
+        isCompact
+        isRequired
+        disabled={isPending}
+        tabIndex={0}
+        type="password"/>
       <FormElement size={8}>
         <Button
           isDefault
