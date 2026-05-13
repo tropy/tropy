5393fa3546328e7f2369f51781747f1c96769bf7
Martim Passos <martim.passos@alumni.usp.br>
2026-05-13 16:52:49 -0300

    Project URL prefs

--- src/components/project/settings.js
+++ src/components/project/settings.js
@@ -7,6 +7,7 @@ import { Button } from '../button.js'
 import { ProjectTypeField } from './type-field.js'
 import { notify } from '../../dialog.js'
 import { BASES, TYPES } from '../../common/project.js'
+import { sanitizeSlug } from '../../common/slug.js'
 import { convert, optimize } from '../../slices/project-files.js'
 import { useDispatch } from 'react-redux'
 import { useIntl } from 'react-intl'
@@ -76,6 +77,16 @@ export const ProjectSettings = React.memo(({
         isRequired
         tabIndex={0}
         value={project.name}
+        onChange={onChange}/>
+      <FormField
+        id="prefs.project.url"
+        name="slug"
+        isCompact
+        isReadOnly={project.isReadOnly}
+        type="url"
+        prefix="tropy://project/"
+        placeholder={sanitizeSlug(project.name || '')}
+        value={project.slug || ''}
         onChange={onChange}/>
       <FormField
         id="prefs.project.id"
