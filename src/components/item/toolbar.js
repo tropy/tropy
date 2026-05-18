31a08304107a27aab44e464e20f02ca2b8cb6aa7
Sylvester Keil <sylvester@keil.or.at>
2026-05-18 17:56:13 +0200

    Add offline indicator

--- src/components/item/toolbar.js
+++ src/components/item/toolbar.js
@@ -1,14 +1,19 @@
 import { useRef } from 'react'
 import { useDispatch } from 'react-redux'
 import { useEvent } from '../../hooks/use-event.js'
+import { useOnline } from '../../hooks/use-online.js'
 import { Titlebar, Toolbar, ToolGroup } from '../toolbar.js'
 import { Button } from '../button.js'
+import { Icon } from '../icons.js'
 import { Fade } from '../fx.js'
 import { mode } from '../../actions/nav.js'
 
 export const ItemToolbar = ({ isItemMode }) => {
   let dispatch = useDispatch()
-  let ref = useRef(null)
+  let online = useOnline()
+
+  let bref = useRef(null)
+  let oref = useRef(null)
 
   let toggleProjectMode = useEvent(() => {
     dispatch(mode.project())
@@ -18,15 +23,23 @@ export const ItemToolbar = ({ isItemMode }) => {
     <Titlebar>
       <Toolbar.Left>
         <ToolGroup>
-          <Fade nodeRef={ref} in={isItemMode}>
+          <Fade nodeRef={bref} in={isItemMode}>
             <Button
-              ref={ref}
+              ref={bref}
               icon="IconChevron16"
               noFocus
               onClick={toggleProjectMode}/>
           </Fade>
         </ToolGroup>
       </Toolbar.Left>
+      <Toolbar.Right>
+        <Fade nodeRef={bref} in={!online}>
+          <Icon
+            ref={oref}
+            name="Ghost"
+            title="toolbar.offline"/>
+        </Fade>
+      </Toolbar.Right>
     </Titlebar>
   )
 }
