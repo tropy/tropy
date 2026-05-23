4f7652a2e31cf53df3be9406e7fc96d62842b0c3
Sylvester Keil <sylvester@keil.or.at>
2026-05-23 19:14:26 +0200

    Make account service respond to invoke and send

--- src/main/ipc.js
+++ src/main/ipc.js
@@ -1,16 +1,16 @@
 import { ipcMain } from 'electron'
 import { trace, warn } from '../common/log.js'
 
-export function ipcActionHandler (channel, handler) {
-  ipcMain.handle(channel, async (event, command, ...args) => {
+export function ipcServiceHandler (channel, handler) {
+  let onInvoke = async (event, command, ...args) => {
     try {
-      trace(`handling ipc action ${channel}.${command}`)
+      trace(`handling ipc ${channel}.${command}`)
 
       return {
         payload: await handler(event, command, ...args)
       }
     } catch (err) {
-      warn({ err }, `ipc action handler ${channel}.${command} failed`)
+      warn({ err }, `ipc ${channel}.${command} failed`)
 
       return {
         error: true,
@@ -23,9 +23,20 @@ export function ipcActionHandler (channel, handler) {
         }
       }
     }
-  })
+  }
+
+  let onMessage = async (event, command, ...args) => {
+    let result = await onInvoke(event, command, ...args)
+    if (result.error && !event.sender.isDestroyed()) {
+      event.sender.send(`${channel}/error`, result.payload)
+    }
+  }
+
+  ipcMain.handle(channel, onInvoke)
+  ipcMain.on(channel, onMessage)
 
   return () => {
     ipcMain.removeHandler(channel)
+    ipcMain.removeListener(channel, onMessage)
   }
 }
