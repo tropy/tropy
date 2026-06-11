c0d83e766de0f8b88d4913e96b3e0657a61ca0b5
Sylvester Keil <sylvester@keil.or.at>
2026-06-11 12:31:41 +0200

    Add utility for making auth requests with retry

--- src/account.js
+++ src/account.js
@@ -2,16 +2,34 @@ import assert from 'node:assert/strict'
 import { ipcRenderer } from 'electron'
 import ARGS from './args.js'
 import { TokenSet } from './common/token-set.js'
+import { retry } from './common/net.js'
 
 let tokenSet
 
-export async function getAccessToken () {
+export async function getAccessToken (forceRefresh = false) {
   assert(ARGS.account.linked, 'no linked account')
 
-  if (!tokenSet?.fresh) {
+  if (forceRefresh || !tokenSet?.fresh) {
     let values = await ipcRenderer.invoke('account', 'getAccessToken')
     tokenSet = new TokenSet(values)
   }
 
   return tokenSet
+}
+
+export async function request (pathname, options = {}, forceRefresh = false) {
+  let { accessToken } = await getAccessToken(forceRefresh)
+  let res = await retry(new URL(pathname, ARGS.account.url), {
+    ...options,
+    headers: {
+      ...options.headers,
+      Authorization: `Bearer ${accessToken}`
+    }
+  })
+
+  if (!forceRefresh && (res.status === 401 || res.status === 403)) {
+    return request(pathname, options, true)
+  }
+
+  return res
 }
