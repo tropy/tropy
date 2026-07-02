6f2e64a7b699054b37b6a41cc5ea70c6f7bb6f2b
Martim Passos <martim.passos@alumni.usp.br>
2026-07-02 09:52:24 -0300

    Drop selectProject, useMemo in component

--- src/selectors/project.js
+++ src/selectors/project.js
@@ -1,14 +1,8 @@
 import { join } from 'node:path'
 import { createSelector } from 'reselect'
 import ARGS from '../args.js'
-import { protocolURL } from '../common/url.js'
 
 export const selectCachePrefix = createSelector(
   (state) => state.project?.id,
   (id) => (ARGS.cache && id) ? join(ARGS.cache, id) : null
-)
-
-export const selectProject = createSelector(
-  (state) => state.project,
-  (project) => ({ ...project, url: protocolURL(project.path) })
 )
