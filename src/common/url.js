ba4e3ebf759e0739bb9f2c5464f31d3b5df3deed
Martim Passos <martim.passos@alumni.usp.br>
2026-08-19 09:09:30 -0300

    url-encode project filename as id

--- src/common/url.js
+++ src/common/url.js
@@ -1,16 +1,9 @@
 import { basename, extname } from 'node:path'
 
-const sanitize = (input) =>
-  String(input ?? '')
-    .normalize('NFKD')
-    .replace(/\p{M}+/gu, '')
-    .toLowerCase()
-    .replace(/[^a-z0-9]+/g, '-')
-    .replace(/^-|-$/g, '')
-
 export function urlId (path) {
   let file = String(path ?? '')
-  return sanitize(basename(file, extname(file))) || 'project'
+  return encodeURIComponent(
+    basename(file, extname(file)).toWellFormed().normalize())
 }
 
 export function protocolURL (path, { item, photo } = {}) {
